0d6d224c3fd591485f3560be9057c41b04d4efde
ErkanTurut <erkamturut@gmail.com>
2023-07-22 23:58:56 +0200

    edit logo auto light mode

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -9,7 +9,7 @@ const NavBar = async () => {
     <div className="fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-2">
       <div className="container max-w-7xl h-full mx-auto flex items-center justify-between gap-2">
         <Link href="/" className="flex gap-2 items-center">
-          <Icons.logo size="m" color="dark" />
+          <Icons.logo size="m" />
           <p className="hidden text-zinc-700 text-sm font-bold md:block">
             Capy
           </p>
