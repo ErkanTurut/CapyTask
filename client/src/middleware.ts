6ed55047526dc49d699905e8386456e90356b387
Erkan Turut <erkamturut@gmail.com>
2023-09-17 00:09:16 +0200

    dashboard page rules

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -2,10 +2,15 @@ import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { Database } from "./types/supabase.types";
+import { authMiddleware, clerkClient } from "@clerk/nextjs";
 
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next();
-  const supabase = createMiddlewareClient<Database>({ req, res });
+  const supabase = createMiddlewareClient({ req, res });
   await supabase.auth.getSession();
+  if (req.nextUrl.pathname === "/dashboard") {
+    return NextResponse.redirect(new URL("/dashboard/account", req.url));
+  }
+
   return res;
 }
