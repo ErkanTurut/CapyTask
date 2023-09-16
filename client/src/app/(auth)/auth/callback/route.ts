7f65031e95ade410425bfd1a320922de6e37c6f1
Erkan Turut <erkamturut@gmail.com>
2023-09-16 21:14:39 +0200

    add callback route

--- client/src/app/(auth)/auth/callback/route.ts
+++ client/src/app/(auth)/auth/callback/route.ts
@@ -5,29 +5,17 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import type { Database } from "@/types/supabase.types";
 
-import { otpCodeSchema } from "@/lib/validations/auth";
-import { z } from "zod";
-
 export const dynamic = "force-dynamic";
 
-export async function POST(request: NextRequest) {
-  try {
-    const res = await request.json();
-    const { code } = otpCodeSchema.parse(res);
-    console.log("code", code);
-    if (code) {
-      console.log("code", code);
-      const supabase = createRouteHandlerClient<Database>({ cookies });
-      await supabase.auth.verifyOtp({
-        email: "erkamturut@gmail.com",
-        token: code,
-        type: "email",
-      });
-    }
+export async function GET(request: NextRequest) {
+  const requestUrl = new URL(request.url);
+  const code = requestUrl.searchParams.get("code");
 
-    // // URL to redirect to after sign in process completes
-    return NextResponse.redirect(request.nextUrl.origin);
-  } catch (error) {
-    console.log(error);
+  if (code) {
+    const supabase = createRouteHandlerClient<Database>({ cookies });
+    await supabase.auth.exchangeCodeForSession(code);
   }
+
+  // URL to redirect to after sign in process completes
+  return NextResponse.redirect(requestUrl.origin);
 }
