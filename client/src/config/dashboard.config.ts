35cbd209e6bae90f82e41d3ac48bc0cfdd016fa6
Erkan Turut <erkamturut@gmail.com>
2023-09-18 01:00:13 +0200

    edit dashboard config map

--- client/src/config/dashboard.config.ts
+++ client/src/config/dashboard.config.ts
@@ -7,26 +7,66 @@ export interface DashboardConfig {
 export const dashboardConfig: DashboardConfig = {
   sidebarNav: [
     {
+      title: "Organisations",
+      icon: "home",
+      items: [
+        {
+          title: "ACME",
+          href: "/dashboard/ACME",
+          items: [],
+        },
+      ],
+    },
+    {
+      title: "Projects",
+      icon: "backpack",
+      items: [
+        {
+          title: "All Projects",
+          href: "/dashboard/projects",
+          items: [],
+        },
+      ],
+    },
+
+    {
       title: "Account",
       href: "/dashboard/account",
       icon: "user",
-      items: [],
-    },
-    {
-      title: "Stores",
-      items: [],
+      items: [
+        {
+          title: "Preferences",
+          href: "/dashboard/account/preferences",
+          items: [],
+        },
+        {
+          title: "Settings",
+          href: "/dashboard/account/settings",
+          items: [],
+        },
+        {
+          title: "Notifications",
+          href: "/dashboard/account/notifications",
+          items: [],
+        },
+      ],
     },
     {
       title: "Billing",
-      href: "/dashboard/billing",
+      icon: "banknote",
       disabled: true,
-      items: [],
-    },
-    {
-      title: "Purchases",
-      href: "/dashboard/purchases",
-
-      items: [],
+      items: [
+        {
+          title: "Invoices",
+          href: "/dashboard/billing/invoices",
+          items: [],
+        },
+        {
+          title: "Payments",
+          href: "/dashboard/billing/payments",
+          items: [],
+        },
+      ],
     },
   ],
 };
