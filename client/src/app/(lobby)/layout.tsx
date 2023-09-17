4bb5eba30801762637faca0632b959190c4b21a4
Erkan Turut <erkamturut@gmail.com>
2023-09-18 01:02:22 +0200

    products page for test

--- client/src/app/(lobby)/layout.tsx
+++ client/src/app/(lobby)/layout.tsx
@@ -1,12 +1,17 @@
-import { currentUser } from "@clerk/nextjs";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import type { User } from "@prisma/client";
+
 import NavBar from "@/components/layouts/navbar";
+import { cookies } from "next/headers";
+// import { useUser } from "@/components/providers/supabaseUserProvider";
 
 interface LobbyLayoutProps {
   children: React.ReactNode;
 }
 
 export default async function LobbyLayout({ children }: LobbyLayoutProps) {
-  const user = null;
+  const supabase = createServerComponentClient({ cookies });
+  const { data: user, error } = await supabase.from("User").select().single();
 
   return (
     <div className="relative flex min-h-screen flex-col">
--- client/src/app/(lobby)/products/page.tsx
+++ client/src/app/(lobby)/products/page.tsx
@@ -1,15 +1,11 @@
 "use client";
-import { Button } from "@/components/ui/button";
+
 import { Shell } from "@/components/shells/shell";
-import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
-const supabase = createClientComponentClient();
+
 export default async function Home() {
-  const { data, error } = await supabase.auth.getUser();
-  console.log(data, error);
   return (
     <Shell as="div" className="gap-12">
       <section>Products</section>
-      <Button onClick={() => console.log(data)}>Button</Button>
     </Shell>
   );
 }
