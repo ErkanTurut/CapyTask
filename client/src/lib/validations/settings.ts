bcdba6beb0f21b119739aa23582c9a4a57e1c385
Erkan Turut <erkamturut@gmail.com>
2023-10-05 01:07:49 +0200

    settings validators

--- client/src/lib/validations/settings.ts
+++ client/src/lib/validations/settings.ts
@@ -1,12 +1,33 @@
 import * as z from "zod";
 
 export const accountSettingsSchema = z.object({
+  first_name: z
+    .string()
+    .min(2, {
+      message: "First name must be at least 2 characters long",
+    })
+    .optional(),
+  last_name: z
+    .string()
+    .min(2, {
+      message: "Last name must be at least 2 characters long",
+    })
+    .optional(),
+  email: z.string().email({ message: "Please enter a valid email address" }),
+});
+
+export const firstNameSchema = z.object({
   firstName: z
     .string()
     .min(2, { message: "First name must be at least 2 characters long" }),
+});
+
+export const lastNameSchema = z.object({
   lastName: z
     .string()
     .min(2, { message: "Last name must be at least 2 characters long" }),
+});
+
+export const emailSchema = z.object({
   email: z.string().email({ message: "Please enter a valid email address" }),
-  imageUri: z.string().url({ message: "Please enter a valid image URL" }),
 });
