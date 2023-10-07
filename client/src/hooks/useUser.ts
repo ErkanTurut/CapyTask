65e5d8c9d30d91ee5dc9d2e2c5a64c35d17c2944
Erkan Turut <erkamturut@gmail.com>
2023-10-08 01:00:26 +0200

    new useSuspensequery function

--- client/src/hooks/useUser.ts
+++ client/src/hooks/useUser.ts
@@ -1,14 +1,28 @@
-import { useQuery, useMutation } from "@tanstack/react-query";
+import {
+  useQuery,
+  useMutation,
+  useSuspenseQuery,
+  useQueryClient,
+} from "@tanstack/react-query";
 import type { user } from "@prisma/client";
+import { catchError } from "@/lib/utils";
 
 export const getUser = (user_id: string) => {
-  return useQuery({
+  return useSuspenseQuery({
     queryKey: ["user", user_id],
     queryFn: async () => {
-      const res = await fetch(`/api/users/${user_id}`, {
-        method: "GET",
-      });
-      return res.json() as Promise<user>;
+      try {
+        const res = await fetch(`/api/users/${user_id}`, {
+          method: "GET",
+          cache: "no-store",
+        });
+        if (!res.ok) {
+          throw catchError(new Error(await res.json()));
+        }
+        return res.json() as Promise<user>;
+      } catch (err) {
+        catchError(err);
+      }
     },
   });
 };
@@ -17,11 +31,16 @@ export const updateUser = (user_id: string) => {
   return useMutation({
     mutationKey: ["user", user_id],
     mutationFn: async (data: Partial<user>) => {
-      await fetch(`/api/users/${user_id}`, {
-        method: "PATCH",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(data),
-      });
+      try {
+        const res = await fetch(`/api/users/${user_id}`, {
+          method: "PATCH",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(data),
+        });
+        if (!res.ok) return catchError(new Error(await res.json()));
+      } catch (err) {
+        catchError(err);
+      }
     },
   });
 };
