d9eb4bb874cc99e76d31e6cd066e97ad3db3d6b1
Erkan Turut <erkamturut@gmail.com>
2023-10-08 23:51:55 +0200

    edit zod object for account settings

--- client/src/lib/validations/settings.ts
+++ client/src/lib/validations/settings.ts
@@ -1,18 +1,10 @@
 import * as z from "zod";
 
 export const accountSettingsSchema = z.object({
-  first_name: z
-    .string()
-    .min(2, {
-      message: "First name must be at least 2 characters long",
-    })
-    .optional(),
-  last_name: z
-    .string()
-    .min(2, {
-      message: "Last name must be at least 2 characters long",
-    })
-    .optional(),
+  first_name: z.string(),
+
+  last_name: z.string(),
+
   email: z.string().email({ message: "Please enter a valid email address" }),
 });
 
