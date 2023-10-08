ce6104e9e76ffbaee06fd12a3224f4d69661e625
Erkan Turut <erkamturut@gmail.com>
2023-10-08 23:55:22 +0200

    prefetch data

--- client/src/app/(dashboard)/dashboard/account/settings/page.tsx
+++ client/src/app/(dashboard)/dashboard/account/settings/page.tsx
@@ -20,10 +20,43 @@ import { Suspense } from "react";
 
 import AccountForm from "@/components/forms/settingsForms/accountSettings";
 import type { user } from "@prisma/client";
+
+import { PostgrestError } from "@supabase/supabase-js";
 import getQueryClient from "@/lib/getQueryClient";
-import { catchError } from "@/lib/utils";
+import Hydrate from "@/lib/hydrate";
+import { dehydrate } from "@tanstack/query-core";
 
 export default async function SettingsPage() {
+  const supabase = createServerComponentClient({ cookies });
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) return null;
+
+  const queryClient = getQueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ["user", user.id],
+    queryFn: async () => {
+      try {
+        const res = await fetch(`http://localhost:3000/api/users/${user.id}`, {
+          method: "GET",
+          cache: "no-store",
+        });
+        if (!res.ok) {
+          throw new Error(await res.json());
+        }
+        const data = await res.json();
+
+        return data;
+      } catch (err) {
+        console.log(err);
+      }
+    },
+  });
+  const dehydratedState = dehydrate(queryClient);
+
   return (
     <Shell variant="sidebar" className="max-w-2xl">
       <PageHeader id="account-header" aria-labelledby="account-header-heading">
@@ -42,8 +75,8 @@ export default async function SettingsPage() {
             <CardDescription>Update your account information.</CardDescription>
           </CardHeader>
           <CardContent>
-            <Suspense fallback={<h1>Loading...</h1>}>
-              <AccountForm />
+            <Suspense fallback={<h1>loading</h1>}>
+              <AccountForm user_id={user.id} />
             </Suspense>
           </CardContent>
         </Card>
