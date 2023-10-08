e43669ee8779ee3e2268cfb009625a530f01de4d
Erkan Turut <erkamturut@gmail.com>
2023-10-08 23:55:40 +0200

    preferences page

--- client/src/app/(dashboard)/dashboard/account/preferences/page.tsx
+++ client/src/app/(dashboard)/dashboard/account/preferences/page.tsx
@@ -1,5 +1,24 @@
 import type { Metadata } from "next";
+import AccountForm from "@/components/forms/settingsForms/accountSettings";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import type { user } from "@prisma/client";
+import { Suspense } from "react";
+
+import { PostgrestError } from "@supabase/supabase-js";
 
 export default async function PreferrencesPage() {
-  return <h1>hello</h1>;
+  const supabase = createServerComponentClient({ cookies });
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) return null;
+
+  return (
+    <Suspense fallback={<h1>loading</h1>}>
+      <AccountForm user_id={user.id} />
+    </Suspense>
+  );
 }
