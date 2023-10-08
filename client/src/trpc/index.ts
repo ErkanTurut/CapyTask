003a4c983bbdd86ebb509bedaa133e589473d385
Erkan Turut <erkamturut@gmail.com>
2023-10-08 23:51:41 +0200

    old trpc

--- client/src/trpc/index.ts
+++ client/src/trpc/index.ts
@@ -3,15 +3,14 @@ import { publicProcedure, router, privateProcedure } from "./trpc";
 import z from "zod";
 
 export const appRouter = router({
-  test: publicProcedure.query(() => {
-    return "hello world";
-  }),
-
-  updateUser: privateProcedure
-    .input(accountSettingsSchema)
-    .mutation(async ({ ctx, input }) => {
-      return console.log(input, ctx);
-    }),
+  // test: publicProcedure.query(() => {
+  //   return "hello world";
+  // }),
+  // updateUser: privateProcedure
+  //   .input(accountSettingsSchema)
+  //   .mutation(async ({ ctx, input }) => {
+  //     return console.log(input, ctx);
+  //   }),
 });
 
 export type AppRouter = typeof appRouter;
