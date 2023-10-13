85a6ef16b8ec6b2f4d9fcc2e2ec575206af26913
Erkan Turut <erkamturut@gmail.com>
2023-10-14 00:11:56 +0200

    added prefetchUser hooks and optimistic update just mind blowing

--- client/src/hooks/useUser.ts
+++ client/src/hooks/useUser.ts
@@ -4,23 +4,26 @@ import {
   useSuspenseQuery,
   useQueryClient,
   QueryClient,
+  dehydrate,
 } from "@tanstack/react-query";
+
+import getQueryClient from "@/lib/getQueryClient";
 import type { user } from "@prisma/client";
 import { catchError } from "@/lib/utils";
-
+import { toast } from "sonner";
 export const getUser = (user_id: string) => {
   return useQuery({
     queryKey: ["user", user_id],
     queryFn: async () => {
       try {
-        const res = await fetch(`http://localhost:3000/api/users/${user_id}`, {
+        const res = await fetch(`http://localhost:3000/api/user/${user_id}`, {
           method: "GET",
           cache: "no-store",
         });
         if (!res.ok) {
           throw catchError(new Error(await res.json()));
         }
-        return (await res.json()) as user;
+        return (await res.json()) as Promise<user>;
       } catch (err) {
         catchError(err);
       }
@@ -33,26 +36,56 @@ export const updateUser = (user_id: string) => {
   return useMutation({
     mutationKey: ["user", user_id],
     mutationFn: async (data: Partial<user>) => {
-      try {
-        const res = await fetch(`/api/users/${user_id}`, {
-          method: "PATCH",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify(data),
-        });
-        if (!res.ok) return catchError(new Error(await res.json()));
-      } catch (err) {
-        catchError(err);
-      }
+      const res = await fetch(`/api/user/${user_id}`, {
+        method: "PATCH",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+      });
+      if (!res.ok) throw new Error(await res.json());
     },
     onMutate: async (data) => {
       await queryClient.cancelQueries({ queryKey: ["user", user_id] });
       const previousUser = queryClient.getQueryData(["user", user_id]) as user;
-      //combine previous user with new data
       queryClient.setQueryData(["user", user_id], {
         ...previousUser,
         ...data,
       });
       return { previousUser };
     },
+    onError: (err, data, context) => {
+      queryClient.setQueryData(["user", user_id], context?.previousUser);
+      toast.error(
+        err ? err.message : "Something went wrong, please try again later."
+      );
+    },
+    onSuccess: () => {
+      toast.success("Profile updated successfully.");
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: ["user", user_id] });
+    },
   });
+};
+
+export const prefetchUser = async (user_id: string) => {
+  const queryClient = getQueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ["user", user_id],
+    queryFn: async () => {
+      try {
+        const res = await fetch(`http://localhost:3000/api/user/${user_id}`, {
+          method: "GET",
+          cache: "no-store",
+        });
+        if (!res.ok) {
+          throw new Error(await res.json());
+        }
+        return (await res.json()) as user;
+      } catch (err) {
+        console.error(err);
+      }
+    },
+  });
+  const dehydratedState = dehydrate(queryClient);
+  return dehydratedState;
 };
