589b506414e78832dc4900882b75740447b5b571
Erkan Turut <erkamturut@gmail.com>
2023-11-03 00:28:50 +0100

    Add SubmitButton component with form status hook.

---
+++ client/src/components/submit-button.tsx
@@ -0,0 +1,19 @@
+import { FC } from "react";
+import { Button } from "./ui/button";
+//@ts-ignore
+import { useFormStatus } from "react-dom";
+
+interface submitButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+
+const SubmitButton: FC<submitButtonProps> = ({ children }) => {
+  const { pending } = useFormStatus();
+  return (
+    <Button type="submit" isLoading={pending}>
+      {children}
+      <span className="sr-only"> {children} </span>
+    </Button>
+  );
+};
+
+export default SubmitButton;
