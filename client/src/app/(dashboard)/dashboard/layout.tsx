2152129583c25efbd826b3aa03ad2e54eb4d4829
Erkan Turut <erkamturut@gmail.com>
2023-11-03 00:27:32 +0100

    Refactor dashboard layout to use Shell and update imports.

--- client/src/app/(dashboard)/dashboard/layout.tsx
+++ client/src/app/(dashboard)/dashboard/layout.tsx
@@ -1,9 +1,9 @@
 import { SidebarNav } from "@/components/layouts/sidebarNav";
-import { prefetchUser } from "@/hooks/useUser";
+import { Shell } from "@/components/shells/shell";
+import { prefetchUser } from "@/hooks/react-query/use-user";
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { HydrationBoundary } from "@tanstack/react-query";
 import { cookies } from "next/headers";
-
 import { redirect } from "next/navigation";
 
 interface DashboardLayoutProps {
@@ -14,7 +14,6 @@ export default async function DashboardLayout({
   children,
 }: DashboardLayoutProps) {
   const supabase = createServerComponentClient({ cookies });
-
   const {
     data: { user },
   } = await supabase.auth.getUser();
@@ -23,22 +22,18 @@ export default async function DashboardLayout({
     redirect("/signin");
   }
 
-  const dehydratedState = async (user_id: string) => {
-    return await prefetchUser(user_id);
-  };
-
   return (
     <div className="flex min-h-screen flex-col">
       {/* <AppNav user={user} /> */}
       <div className="flex-1 items-start md:grid md:grid-cols-[220px_minmax(0,1fr)] md:gap-6 lg:grid-cols-[240px_minmax(0,1fr)] lg:gap-10 ">
-        <HydrationBoundary state={dehydratedState(user.id)}>
-          <aside className="fixed p-2 px-4 z-30 -ml-2 hidden h-full w-full shrink-0  overflow-y-auto border-r md:sticky md:block bottom-0">
-            <SidebarNav user={user} />
-          </aside>
-          <main className="flex container w-full flex-col overflow-hidden">
+        <aside className="fixed p-2 px-4 z-30 -ml-2 hidden h-full w-full shrink-0  overflow-y-auto border-r md:sticky md:block bottom-0">
+          <SidebarNav user_id={user.id} />
+        </aside>
+        <main className="flex container w-full flex-col overflow-hidden">
+          <Shell variant="sidebar" className="max-w-3xl">
             {children}
-          </main>
-        </HydrationBoundary>
+          </Shell>
+        </main>
       </div>
       {/* <SiteFooter /> */}
     </div>
