5e4ef11dc48819a7684ef1d0c7a8b3da4be2588c
Erkan Turut <erkamturut@gmail.com>
2023-11-03 00:28:01 +0100

    Fix import statement in organizationSelector component.

--- client/src/components/organizationSelector.tsx
+++ client/src/components/organizationSelector.tsx
@@ -5,7 +5,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import { cn } from "@/lib/utils";
+import { cn } from "@/utils";
 
 export interface OrganizationSelectorProps
   extends React.HTMLAttributes<HTMLDivElement> {
