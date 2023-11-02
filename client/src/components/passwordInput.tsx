10fd3bb762f14432cb3405789e840ca83bc6d6a3
Erkan Turut <erkamturut@gmail.com>
2023-11-03 00:28:45 +0100

    Fix import path for cn utility function in passwordInput component

--- client/src/components/passwordInput.tsx
+++ client/src/components/passwordInput.tsx
@@ -2,7 +2,7 @@
 
 import { useState, forwardRef } from "react";
 
-import { cn } from "@/lib/utils";
+import { cn } from "@/utils";
 import { Button } from "@/components/ui/button";
 import { Input, type InputProps } from "@/components/ui/input";
 import { Icons } from "@/components/icons";
