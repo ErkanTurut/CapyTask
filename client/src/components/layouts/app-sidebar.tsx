0ab59d05acd3e25d72618f34329529a4b1e0b743
Erkan Turut <erkamturut@gmail.com>
2023-11-05 21:11:30 +0100

    Add app sidebar component with user account dashboard, main navigation, and site logo.

---
+++ client/src/components/layouts/app-sidebar.tsx
@@ -0,0 +1,55 @@
+import Link from "next/link";
+import { Separator } from "@/components/ui/separator";
+import { cn } from "@/utils";
+import { Icons } from "@/components/icons";
+import { MainSideNav } from "./main-navigation/main-sidebar";
+import { dashboardConfig } from "@/config/dashboard.config";
+import { siteConfig } from "@/config/site.config";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import UserAccountDashboard from "@/components/user-account-dashboard";
+
+export interface SidebarNavProps extends React.HTMLAttributes<HTMLDivElement> {
+  user_id: string;
+}
+
+import { getUser } from "@/lib/api/users";
+import { Suspense } from "react";
+import MinimizeButton from "../minimize-button";
+import ThemeToggle from "../themeToggle";
+
+export async function SidebarNav({
+  user_id,
+  className,
+  ...props
+}: SidebarNavProps) {
+  const user = await getUser(user_id);
+
+  return (
+    <div className={cn("flex h-full flex-col gap-2 ", className)} {...props}>
+      <span className="flex justify-between items-center">
+        <Suspense fallback="loading...">
+          <UserAccountDashboard user={user} />
+        </Suspense>
+        <MinimizeButton />
+      </span>
+
+      <div className=" h-[calc(100vh-8rem)] overflow-y-auto ">
+        <ThemeToggle toggle={true} />
+        <ScrollArea>
+          <MainSideNav items={dashboardConfig.sidebarNav} />
+        </ScrollArea>
+      </div>
+      <div className="flex flex-col gap-2">
+        <Separator className="flex" />
+        <Link
+          aria-label="Dashboard"
+          href="/dashboard"
+          className="items-center space-x-2 flex"
+        >
+          <Icons.logo size="m" aria-hidden="true" />
+          <span className="font-bold inline-block">{siteConfig.name}</span>
+        </Link>
+      </div>
+    </div>
+  );
+}
