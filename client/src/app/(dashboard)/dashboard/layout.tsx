5e4ab890c5f278d272a890f922121ed7c362bc5f
Erkan Turut <erkamturut@gmail.com>
2023-11-05 21:11:07 +0100

    Update sidebarNav import and styling in dashboard layout

--- client/src/app/(dashboard)/dashboard/layout.tsx
+++ client/src/app/(dashboard)/dashboard/layout.tsx
@@ -1,4 +1,4 @@
-import { SidebarNav } from "@/components/layouts/sidebarNav";
+import { SidebarNav } from "@/components/layouts/app-sidebar";
 import { Shell } from "@/components/shells/shell";
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
@@ -22,9 +22,8 @@ export default async function DashboardLayout({
 
   return (
     <div className="flex min-h-screen flex-col">
-      {/* <AppNav user={user} /> */}
       <div className="flex-1 items-start md:grid md:grid-cols-[220px_minmax(0,1fr)] md:gap-6 lg:grid-cols-[240px_minmax(0,1fr)] lg:gap-10 ">
-        <aside className="fixed p-2 px-4 z-30 -ml-2 hidden h-full w-full shrink-0  overflow-y-auto border-r md:sticky md:block bottom-0">
+        <aside className="fixed p-2 pl- z-30 -ml-2 hidden h-full w-full shrink-0  overflow-y-auto border-r md:sticky md:block bottom-0">
           <SidebarNav user_id={user.id} />
         </aside>
         <main className="flex container w-full flex-col overflow-hidden">
