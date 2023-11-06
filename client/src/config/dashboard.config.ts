42e8206d6de78a953ea140fa802aa18d494a38e6
Erkan Turut <erkamturut@gmail.com>
2023-11-07 00:17:48 +0100

    Add sidebar navigation items to dashboard config.

--- client/src/config/dashboard.config.ts
+++ client/src/config/dashboard.config.ts
@@ -7,6 +7,45 @@ export interface DashboardConfig {
 export const dashboardConfig: DashboardConfig = {
   sidebarNav: [
     {
+      main: [
+        {
+          title: "Research",
+          href: "/dashboard/research",
+          icon: "search",
+          items: [],
+        },
+        {
+          title: "Settings",
+          href: "/dashboard/account/settings",
+          icon: "gear",
+          items: [],
+        },
+        {
+          title: "Back to website",
+          href: "/",
+          icon: "externalLink",
+          items: [],
+        },
+      ],
+      sidebar: [
+        {
+          title: "Projects",
+          items: [
+            {
+              title: "All Projects",
+              href: "/dashboard/projects",
+              icon: "chevronRight",
+              items: [],
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
+
+/*
+    {
       title: "Organisations",
       icon: "backpack",
       href: "/dashboard/organisations",
@@ -52,5 +91,4 @@ export const dashboardConfig: DashboardConfig = {
         },
       ],
     },
-  ],
-};
+    */
