c37b676a238f5fb4567873c95d2eead62477475e
Erkan Turut <erkamturut@gmail.com>
2023-11-08 00:23:48 +0100

    Add sign-in form component with validation and authentication

---
+++ client/src/components/forms/sign-in.tsx
@@ -0,0 +1,113 @@
+"use client";
+
+import * as React from "react";
+import { useRouter } from "next/navigation";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import type { z } from "zod";
+
+import { authSchema } from "@/lib/validations/auth";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Icons } from "@/components/icons";
+import { PasswordInput } from "@/components/passwordInput";
+
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import type { Database } from "@/types/supabase.types";
+import { toast } from "sonner";
+import { catchError } from "@/utils";
+
+type Inputs = z.infer<typeof authSchema>;
+
+export function SignInForm() {
+  const router = useRouter();
+  const supabase = createClientComponentClient<Database>();
+  const [isPending, startTransition] = React.useTransition();
+
+  // react-hook-form
+  const form = useForm<Inputs>({
+    resolver: zodResolver(authSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  async function onSubmit(data: Inputs) {
+    try {
+      startTransition(async () => {
+        const res = await fetch("api/auth/sign-in", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(data),
+        });
+        if (!res.ok) {
+          catchError(new Error(await res.json()));
+        }
+        if (res.redirected && res.status === 200) {
+          toast.message("You have been connected");
+          router.refresh();
+          router.push(res.url);
+        }
+      });
+    } catch (err) {
+      catchError(err);
+    }
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        className="grid gap-4"
+        onSubmit={(...args) => void form.handleSubmit(onSubmit)(...args)}
+      >
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="rodneymullen180@gmail.com" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <PasswordInput placeholder="**********" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button type="submit" disabled={isPending}>
+          {isPending && (
+            <Icons.spinner
+              className="mr-2 h-4 w-4 animate-spin"
+              aria-hidden="true"
+            />
+          )}
+          Sign in
+          <span className="sr-only">Sign in</span>
+        </Button>
+      </form>
+    </Form>
+  );
+}
