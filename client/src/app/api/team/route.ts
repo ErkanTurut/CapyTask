e47b8b66460f525d905cd93a784a7271608c01a3
Erkan Turut <erkamturut@gmail.com>
2023-11-13 00:00:19 +0100

    Add API endpoint to get teams for a user.

---
+++ client/src/app/api/team/route.ts
@@ -0,0 +1,46 @@
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+
+import { PostgrestError } from "@supabase/supabase-js";
+import type { team, user_team } from "@prisma/client";
+import { Database } from "@/types/supabase.types";
+
+//api that send all the teams that current user is in
+export async function GET(request: NextRequest) {
+  try {
+    const supabase = createRouteHandlerClient<Database>({ cookies });
+    const requestUrl = new URL(request.url);
+    const user_id = requestUrl.searchParams.get("user_id");
+
+    if (!user_id) throw new Error("user_id is required");
+
+    // await new Promise((resolve) => setTimeout(resolve, 10000));
+
+    //@ts-ignore
+    const {
+      data: team,
+      error,
+    }: { data: team[] | null; error: PostgrestError | null } = await supabase
+      .from("user_team")
+      .select(`team!inner(*)`)
+      .eq("user_id", user_id);
+
+    if (error) throw new Error(error.message);
+    return NextResponse.json(team, {
+      status: 200,
+    });
+  } catch (error: Error | z.ZodError | PostgrestError | unknown) {
+    if (error instanceof z.ZodError) {
+      return new Response(JSON.stringify(error.issues), { status: 422 });
+    } else if (error instanceof Error) {
+      return NextResponse.json(error.message, {
+        status: 400,
+      });
+    }
+    return NextResponse.json("Something went wrong, please try again later.", {
+      status: 500,
+    });
+  }
+}
