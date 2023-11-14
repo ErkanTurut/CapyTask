2bbf871a1067c613e61e1e97f254332209552312
Erkan Turut <erkamturut@gmail.com>
2023-11-14 23:44:18 +0100

    Update function to handle edge cases

--- client/src/types/supabase.types.ts
+++ client/src/types/supabase.types.ts
@@ -4,296 +4,286 @@ export type Json =
   | boolean
   | null
   | { [key: string]: Json | undefined }
-  | Json[];
+  | Json[]
 
 export interface Database {
   public: {
     Tables: {
-      folder: {
+      project: {
         Row: {
-          created_at: string;
-          description: string | null;
-          id: string;
-          name: string;
-          organization_id: string;
-          updated_at: string;
-        };
+          created_at: string
+          description: string | null
+          id: string
+          name: string
+          team_id: string
+          updated_at: string
+        }
         Insert: {
-          created_at?: string;
-          description?: string | null;
-          id?: string;
-          name: string;
-          organization_id: string;
-          updated_at?: string;
-        };
+          created_at?: string
+          description?: string | null
+          id?: string
+          name: string
+          team_id: string
+          updated_at?: string
+        }
         Update: {
-          created_at?: string;
-          description?: string | null;
-          id?: string;
-          name?: string;
-          organization_id?: string;
-          updated_at?: string;
-        };
+          created_at?: string
+          description?: string | null
+          id?: string
+          name?: string
+          team_id?: string
+          updated_at?: string
+        }
         Relationships: [
           {
-            foreignKeyName: "folder_organization_id_fkey";
-            columns: ["organization_id"];
-            referencedRelation: "organization";
-            referencedColumns: ["id"];
+            foreignKeyName: "project_team_id_fkey"
+            columns: ["team_id"]
+            isOneToOne: false
+            referencedRelation: "team"
+            referencedColumns: ["id"]
           }
-        ];
-      };
-      organization: {
-        Row: {
-          created_at: string;
-          description: string | null;
-          id: string;
-          image_uri: string | null;
-          name: string;
-          updated_at: string;
-        };
-        Insert: {
-          created_at?: string;
-          description?: string | null;
-          id?: string;
-          image_uri?: string | null;
-          name: string;
-          updated_at?: string;
-        };
-        Update: {
-          created_at?: string;
-          description?: string | null;
-          id?: string;
-          image_uri?: string | null;
-          name?: string;
-          updated_at?: string;
-        };
-        Relationships: [];
-      };
+        ]
+      }
       role: {
         Row: {
-          description: string;
-          id: string;
-          name: string;
-          permissions: string[] | null;
-        };
+          description: string
+          id: string
+          name: string
+          permissions: string[] | null
+        }
         Insert: {
-          description: string;
-          id?: string;
-          name: string;
-          permissions?: string[] | null;
-        };
+          description: string
+          id?: string
+          name: string
+          permissions?: string[] | null
+        }
         Update: {
-          description?: string;
-          id?: string;
-          name?: string;
-          permissions?: string[] | null;
-        };
-        Relationships: [];
-      };
+          description?: string
+          id?: string
+          name?: string
+          permissions?: string[] | null
+        }
+        Relationships: []
+      }
       task: {
         Row: {
-          created_at: string;
-          created_by_id: string | null;
-          description: string | null;
-          folder_id: string;
-          id: string;
-          name: string;
-          parent_task_id: string | null;
-          status: Database["public"]["Enums"]["Status"];
-          updated_at: string;
-        };
+          created_at: string
+          created_by_id: string | null
+          description: string | null
+          id: string
+          name: string
+          parent_task_id: string | null
+          project_id: string
+          status: Database["public"]["Enums"]["Status"]
+          updated_at: string
+        }
         Insert: {
-          created_at?: string;
-          created_by_id?: string | null;
-          description?: string | null;
-          folder_id: string;
-          id?: string;
-          name: string;
-          parent_task_id?: string | null;
-          status: Database["public"]["Enums"]["Status"];
-          updated_at?: string;
-        };
+          created_at?: string
+          created_by_id?: string | null
+          description?: string | null
+          id?: string
+          name: string
+          parent_task_id?: string | null
+          project_id: string
+          status: Database["public"]["Enums"]["Status"]
+          updated_at?: string
+        }
         Update: {
-          created_at?: string;
-          created_by_id?: string | null;
-          description?: string | null;
-          folder_id?: string;
-          id?: string;
-          name?: string;
-          parent_task_id?: string | null;
-          status?: Database["public"]["Enums"]["Status"];
-          updated_at?: string;
-        };
+          created_at?: string
+          created_by_id?: string | null
+          description?: string | null
+          id?: string
+          name?: string
+          parent_task_id?: string | null
+          project_id?: string
+          status?: Database["public"]["Enums"]["Status"]
+          updated_at?: string
+        }
         Relationships: [
           {
-            foreignKeyName: "task_created_by_id_fkey";
-            columns: ["created_by_id"];
-            referencedRelation: "user";
-            referencedColumns: ["id"];
+            foreignKeyName: "task_created_by_id_fkey"
+            columns: ["created_by_id"]
+            isOneToOne: false
+            referencedRelation: "user"
+            referencedColumns: ["id"]
           },
           {
-            foreignKeyName: "task_folder_id_fkey";
-            columns: ["folder_id"];
-            referencedRelation: "folder";
-            referencedColumns: ["id"];
+            foreignKeyName: "task_parent_task_id_fkey"
+            columns: ["parent_task_id"]
+            isOneToOne: false
+            referencedRelation: "task"
+            referencedColumns: ["id"]
           },
           {
-            foreignKeyName: "task_parent_task_id_fkey";
-            columns: ["parent_task_id"];
-            referencedRelation: "task";
-            referencedColumns: ["id"];
+            foreignKeyName: "task_project_id_fkey"
+            columns: ["project_id"]
+            isOneToOne: false
+            referencedRelation: "project"
+            referencedColumns: ["id"]
           }
-        ];
-      };
+        ]
+      }
+      team: {
+        Row: {
+          created_at: string
+          description: string | null
+          id: string
+          image_uri: string | null
+          name: string
+          updated_at: string
+        }
+        Insert: {
+          created_at?: string
+          description?: string | null
+          id?: string
+          image_uri?: string | null
+          name: string
+          updated_at?: string
+        }
+        Update: {
+          created_at?: string
+          description?: string | null
+          id?: string
+          image_uri?: string | null
+          name?: string
+          updated_at?: string
+        }
+        Relationships: []
+      }
       user: {
         Row: {
-          createdAt: string;
-          email: string;
-          first_name: string | null;
-          id: string;
-          image_uri: string | null;
-          last_name: string | null;
-          updatedAt: string;
-        };
+          createdAt: string
+          email: string
+          first_name: string | null
+          id: string
+          image_uri: string | null
+          last_name: string | null
+          updatedAt: string
+        }
         Insert: {
-          createdAt?: string;
-          email: string;
-          first_name?: string | null;
-          id: string;
-          image_uri?: string | null;
-          last_name?: string | null;
-          updatedAt?: string;
-        };
+          createdAt?: string
+          email: string
+          first_name?: string | null
+          id: string
+          image_uri?: string | null
+          last_name?: string | null
+          updatedAt?: string
+        }
         Update: {
-          createdAt?: string;
-          email?: string;
-          first_name?: string | null;
-          id?: string;
-          image_uri?: string | null;
-          last_name?: string | null;
-          updatedAt?: string;
-        };
+          createdAt?: string
+          email?: string
+          first_name?: string | null
+          id?: string
+          image_uri?: string | null
+          last_name?: string | null
+          updatedAt?: string
+        }
         Relationships: [
           {
-            foreignKeyName: "user_id_fkey";
-            columns: ["id"];
-            referencedRelation: "users";
-            referencedColumns: ["id"];
+            foreignKeyName: "user_id_fkey"
+            columns: ["id"]
+            isOneToOne: true
+            referencedRelation: "users"
+            referencedColumns: ["id"]
           }
-        ];
-      };
-      user_folder: {
+        ]
+      }
+      user_project: {
         Row: {
-          folder_id: string;
-          role_id: string;
-          user_id: string;
-        };
+          project_id: string
+          role_id: string
+          user_id: string
+        }
         Insert: {
-          folder_id: string;
-          role_id: string;
-          user_id: string;
-        };
+          project_id: string
+          role_id: string
+          user_id: string
+        }
         Update: {
-          folder_id?: string;
-          role_id?: string;
-          user_id?: string;
-        };
+          project_id?: string
+          role_id?: string
+          user_id?: string
+        }
         Relationships: [
           {
-            foreignKeyName: "user_folder_folder_id_fkey";
-            columns: ["folder_id"];
-            referencedRelation: "folder";
-            referencedColumns: ["id"];
+            foreignKeyName: "user_project_project_id_fkey"
+            columns: ["project_id"]
+            isOneToOne: false
+            referencedRelation: "project"
+            referencedColumns: ["id"]
           },
           {
-            foreignKeyName: "user_folder_role_id_fkey";
-            columns: ["role_id"];
-            referencedRelation: "role";
-            referencedColumns: ["id"];
+            foreignKeyName: "user_project_role_id_fkey"
+            columns: ["role_id"]
+            isOneToOne: false
+            referencedRelation: "role"
+            referencedColumns: ["id"]
           },
           {
-            foreignKeyName: "user_folder_user_id_fkey";
-            columns: ["user_id"];
-            referencedRelation: "user";
-            referencedColumns: ["id"];
+            foreignKeyName: "user_project_user_id_fkey"
+            columns: ["user_id"]
+            isOneToOne: false
+            referencedRelation: "user"
+            referencedColumns: ["id"]
           }
-        ];
-      };
-      user_organization: {
+        ]
+      }
+      user_team: {
         Row: {
-          org_id: string;
-          role_id: string;
-          user_id: string;
-        };
+          role_id: string
+          team_id: string
+          user_id: string
+        }
         Insert: {
-          org_id: string;
-          role_id: string;
-          user_id: string;
-        };
+          role_id: string
+          team_id: string
+          user_id: string
+        }
         Update: {
-          org_id?: string;
-          role_id?: string;
-          user_id?: string;
-        };
+          role_id?: string
+          team_id?: string
+          user_id?: string
+        }
         Relationships: [
           {
-            foreignKeyName: "user_organization_org_id_fkey";
-            columns: ["org_id"];
-            referencedRelation: "organization";
-            referencedColumns: ["id"];
+            foreignKeyName: "user_team_role_id_fkey"
+            columns: ["role_id"]
+            isOneToOne: false
+            referencedRelation: "role"
+            referencedColumns: ["id"]
           },
           {
-            foreignKeyName: "user_organization_role_id_fkey";
-            columns: ["role_id"];
-            referencedRelation: "role";
-            referencedColumns: ["id"];
+            foreignKeyName: "user_team_team_id_fkey"
+            columns: ["team_id"]
+            isOneToOne: false
+            referencedRelation: "team"
+            referencedColumns: ["id"]
           },
           {
-            foreignKeyName: "user_organization_user_id_fkey";
-            columns: ["user_id"];
-            referencedRelation: "user";
-            referencedColumns: ["id"];
+            foreignKeyName: "user_team_user_id_fkey"
+            columns: ["user_id"]
+            isOneToOne: false
+            referencedRelation: "user"
+            referencedColumns: ["id"]
           }
-        ];
-      };
-    };
+        ]
+      }
+    }
     Views: {
-      [_ in never]: never;
-    };
+      [_ in never]: never
+    }
     Functions: {
-      [_ in never]: never;
-    };
+      [_ in never]: never
+    }
     Enums: {
       Status:
         | "NOTSTARTED"
         | "INPROGRESS"
         | "COMPLETED_SUCCESS"
-        | "COMPLETED_FAIL";
-    };
+        | "COMPLETED_FAIL"
+    }
     CompositeTypes: {
-      [_ in never]: never;
-    };
-  };
+      [_ in never]: never
+    }
+  }
 }
-
-export type Provider =
-  | "apple"
-  | "azure"
-  | "bitbucket"
-  | "discord"
-  | "facebook"
-  | "figma"
-  | "github"
-  | "gitlab"
-  | "google"
-  | "kakao"
-  | "keycloak"
-  | "linkedin"
-  | "notion"
-  | "slack"
-  | "spotify"
-  | "twitch"
-  | "twitter"
-  | "workos"
-  | "zoom";
