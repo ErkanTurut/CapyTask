a390c63cc938e6261042c58237ff16f278f07b2b
Erkan Turut <erkamturut@gmail.com>
2023-11-18 21:52:11 +0100

    Add error component to handle errors

---
+++ client/src/app/error.tsx
@@ -0,0 +1,13 @@
+"use client";
+import type { Metadata } from "next";
+import AccountForm from "@/components/forms/settings-forms/account-settings";
+import { cookies } from "next/headers";
+import type { user } from "@prisma/client";
+import { Suspense } from "react";
+
+import { PostgrestError } from "@supabase/supabase-js";
+import { Shell } from "@/components/shells/shell";
+
+export default function Error({ error }: { error: Error }) {
+  return <h1>something went wring :c</h1>;
+}
