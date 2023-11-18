9aa959dcc20fb8f7e25cdfe8506faa203c0192e8
Erkan Turut <erkamturut@gmail.com>
2023-11-18 01:43:30 +0100

    Fix import and update getUserSession function

--- client/src/lib/services/user/index.ts
+++ client/src/lib/services/user/index.ts
@@ -1,35 +1,23 @@
+import "server-only";
+
 import { Database } from "@/types/supabase.types";
 import type { user } from "@prisma/client";
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { PostgrestError } from "@supabase/supabase-js";
 import {
   unstable_noStore as noStore,
   unstable_cache as cache,
   revalidateTag,
 } from "next/cache";
-import { cookies } from "next/headers";
+import createSupabaseServerClient from "@/lib/supabase/server";
 
-// export const getUser = async (user_id: string) => {
-//   try {
-//     const res = await fetch(`http://localhost:3000/api/user/${user_id}`, {
-//       method: "GET",
-//       cache: "no-store",
-//       next: {
-//         tags: ["user", user_id],
-//       },
-//     });
-//     if (!res.ok) {
-//       // throw new Error(await res.json());
-//     }
-//     return (await res.json()) as user;
-//   } catch (err) {
-//     throw err;
-//   }
-// };
-const supabase = createServerComponentClient<Database>({ cookies });
+export const getUserSession = async () => {
+  const supabase = await createSupabaseServerClient();
+  return supabase.auth.getSession();
+};
 
 export const getUser = cache(
   async (user_id: string) => {
+    const supabase = await createSupabaseServerClient();
     const { data: user, error } = await supabase
       .from("user")
       .select()
