d1b326348b08393683a1ae68572562e5a817eb51
Erkan Turut <erkamturut@gmail.com>
2023-11-18 21:51:01 +0100

    Update user actions file

--- client/src/lib/services/user/actions.ts
+++ client/src/lib/services/user/actions.ts
@@ -1,10 +1,10 @@
 "use server";
-import { createServerActionClient } from "@supabase/auth-helpers-nextjs";
 import { Database } from "@/types/supabase.types";
 import { cookies } from "next/headers";
 import { accountSettingsSchema } from "@/lib/validations/settings";
 import { z } from "zod";
 import { revalidateTag } from "next/cache";
+import createSupabaseServerClient from "@/lib/supabase/server";
 
 // create type with accountSettingsSchema from zod
 export type accountSettings = z.infer<typeof accountSettingsSchema>;
@@ -14,7 +14,8 @@ export async function updateUser(
   user_id: string
 ) {
   try {
-    const supabase = createServerActionClient<Database>({ cookies });
+    // const supabase = createServerActionClient<Database>({ cookies });
+    const supabase = await createSupabaseServerClient();
     const data = accountSettingsSchema.parse(formData);
     // await new Promise((resolve) => setTimeout(resolve, 4000));
     const { error } = await supabase
