a37873368609539d06c56be0beccfe0bfce25e0c
Erkan Turut <erkamturut@gmail.com>
2023-11-18 21:50:51 +0100

    Fix createServerClient type in server.ts

--- client/src/lib/supabase/server.ts
+++ client/src/lib/supabase/server.ts
@@ -7,7 +7,7 @@ import { Database } from "@/types/supabase.types";
 export default async function createSupabaseServerClient() {
   const cookieStore = cookies();
 
-  return createServerClient(
+  return createServerClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
