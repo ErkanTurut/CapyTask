c6e2fac3f1b828fa92a59c1f34b756e381db31c6
Erkan Turut <erkamturut@gmail.com>
2023-11-19 21:57:58 +0100

    Remove unnecessary imports from projects page

--- client/src/app/(dashboard)/dashboard/projects/page.tsx
+++ client/src/app/(dashboard)/dashboard/projects/page.tsx
@@ -1,12 +1,3 @@
-import type { Metadata } from "next";
-import AccountForm from "@/components/forms/settings-forms/account-settings";
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-import { cookies } from "next/headers";
-import type { user } from "@prisma/client";
-import { Suspense } from "react";
-
-import { PostgrestError } from "@supabase/supabase-js";
-
 export default async function ProjectsPage() {
   return <h1>Projects</h1>;
 }
