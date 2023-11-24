1705351815e407b678b85321ed7aa768d006ff16
Erkan Turut <erkamturut@gmail.com>
2023-11-24 20:13:47 +0100

    Refactor app-sidebar component

--- client/src/components/layouts/app-sidebar.tsx
+++ client/src/components/layouts/app-sidebar.tsx
@@ -8,7 +8,6 @@ import { FooterSidebar } from "./sidebar/footer-sidebar";
 import { dashboardConfig } from "@/config/dashboard.config";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import UserAccountDashboard from "@/components/user-account-dashboard";
-import ShimmerButton from "@/components/magicui/shimmer-button";
 
 export interface SidebarNavProps extends React.HTMLAttributes<HTMLDivElement> {
   user_id: string;
@@ -16,8 +15,8 @@ export interface SidebarNavProps extends React.HTMLAttributes<HTMLDivElement> {
 
 import { getUser } from "@/lib/services/user";
 import { Suspense } from "react";
-import MinimizeButton from "../minimize-button";
-import ThemeToggle from "../themeToggle";
+import { Shell } from "../shells/shell";
+import ResizeZone from "./resize-zone";
 
 export async function SidebarNav({
   user_id,
@@ -28,31 +27,34 @@ export async function SidebarNav({
   if (!user) return null;
 
   return (
-    <div
-      className={cn("flex w-full h-full flex-grow overflow-hidden", className)}
-      {...props}
-    >
-      <span className="flex flex-col w-full h-full gap-1">
-        <Suspense fallback="loading...">
-          <UserAccountDashboard user={user} />
-        </Suspense>
-        {/* <ThemeToggle toggle={true} /> */}
+    <ResizeZone>
+      <div
+        className={cn(
+          "flex w-full h-full flex-grow overflow-hidden",
+          className
+        )}
+        {...props}
+      >
+        <span className="flex flex-col w-full h-full gap-1">
+          <Suspense fallback="loading...">
+            <UserAccountDashboard user={user} />
+          </Suspense>
+          {/* <ThemeToggle toggle={true} /> */}
 
-        <nav className="flex w-full flex-col gap-1 flex-grow overflow-hidden">
-          <div className="flex w-full flex-col gap-1 overflow-hidden">
-            <HeaderSidebar items={dashboardConfig.sidebarNav} />
-            <Separator className="flex" />
-            <div className="flex-grow overflow-y-auto">
-              <ScrollArea>
-                <MainSidebar items={dashboardConfig.sidebarNav} />
-              </ScrollArea>
+          <nav className="flex w-full flex-col gap-1 flex-grow overflow-hidden">
+            <div className="flex w-full flex-col gap-1 overflow-hidden">
+              <HeaderSidebar items={dashboardConfig.sidebarNav} />
+              <Separator className="flex" />
+              <div className="flex-grow overflow-y-auto">
+                <ScrollArea>
+                  <MainSidebar items={dashboardConfig.sidebarNav} />
+                </ScrollArea>
+              </div>
             </div>
-          </div>
-        </nav>
-
-        <Separator className="flex" />
-        <FooterSidebar items={dashboardConfig.sidebarNav} />
-      </span>
-    </div>
+          </nav>
+          <FooterSidebar items={dashboardConfig.sidebarNav} />
+        </span>
+      </div>
+    </ResizeZone>
   );
 }
