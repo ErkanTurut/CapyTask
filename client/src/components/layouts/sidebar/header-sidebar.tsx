47442a77cb671e2b254e20e778dd09031cc99a67
Erkan Turut <erkamturut@gmail.com>
2023-11-24 20:13:56 +0100

    Refactor header sidebar component

--- client/src/components/layouts/sidebar/header-sidebar.tsx
+++ client/src/components/layouts/sidebar/header-sidebar.tsx
@@ -7,7 +7,6 @@ import { usePathname } from "next/navigation";
 import { cn } from "@/utils";
 import { buttonVariants } from "@/components/ui/button";
 import { Icons } from "@/components/icons";
-import { getTeam } from "@/lib/services/team";
 
 export interface HeaderSidebarProps
   extends React.HTMLAttributes<HTMLDivElement> {
@@ -22,30 +21,43 @@ export function HeaderSidebar({
   const pathname = usePathname();
 
   return (
-    <span className={cn("flex w-full gap-1 flex-col", className)} {...props}>
+    <span className={cn("flex w-full flex-col gap-1 ", className)} {...props}>
       {items.header.map((headerItem, headerIndex) => {
-        const Icon = headerItem.icon ? Icons[headerItem.icon] : null;
-        return headerItem.href ? (
-          <Link
-            aria-label={headerItem.title}
-            key={headerIndex}
-            href={headerItem.href}
-            target={headerItem.external ? "_blank" : ""}
-            rel={headerItem.external ? "noreferrer" : ""}
-            className={cn(
-              buttonVariants({ variant: "ghost", size: "sm" }),
-              pathname === headerItem.href ? "bg-muted font-bold" : "",
-              "justify-start",
-              headerItem.disabled && "pointer-events-none opacity-60"
+        return (
+          <span key={headerIndex} className={cn("flex w-full gap-1 flex-col")}>
+            {headerItem.label && (
+              <h2 className="flex w-full items-center py-1 text-sm font-semibold text-muted-foreground">
+                {headerItem.label}
+              </h2>
             )}
-          >
-            {Icon && <Icon className="mr-2 w-4 h-4" aria-hidden="true" />}
-            {headerItem.title}
-          </Link>
-        ) : (
-          <h2 className="flex w-full items-center py-1 text-sm font-semibold text-muted-foreground">
-            {headerItem.title}
-          </h2>
+            {headerItem.items.length > 0
+              ? headerItem.items.map((subitem, subIndex) => {
+                  const Icon = subitem.icon ? Icons[subitem.icon] : null;
+                  return (
+                    <Link
+                      aria-label={subitem.title}
+                      key={subIndex}
+                      href={subitem.href ? subitem.href : ""}
+                      target={subitem.external ? "_blank" : ""}
+                      rel={subitem.external ? "noreferrer" : ""}
+                      className={cn(
+                        buttonVariants({ variant: "ghost", size: "sm" }),
+                        pathname === subitem.href
+                          ? "bg-muted hover:bg-muted font-bold "
+                          : "hover:bg-muted",
+                        "justify-start",
+                        subitem.disabled && "pointer-events-none opacity-60"
+                      )}
+                    >
+                      {Icon && (
+                        <Icon className="mr-2 w-4 h-4" aria-hidden="true" />
+                      )}
+                      {subitem.title}
+                    </Link>
+                  );
+                })
+              : null}
+          </span>
         );
       })}
     </span>
