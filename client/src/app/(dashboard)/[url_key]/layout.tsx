ad762e1193554552fa6fd727c204cd803d21c495
Erkan Turut <erkamturut@gmail.com>
2023-11-26 22:18:19 +0100

    Update getWorkspace function in layout.tsx

--- client/src/app/(dashboard)/[url_key]/layout.tsx
+++ client/src/app/(dashboard)/[url_key]/layout.tsx
@@ -1,7 +1,7 @@
 import { SidebarNav } from "@/components/layouts/app-sidebar";
 import { Shell } from "@/components/shells/shell";
 import { getUserSession } from "@/lib/services/user";
-import { getWorkspace } from "@/lib/services/workspace";
+import { getWorkspace, getWorkspaces } from "@/lib/services/workspace";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
@@ -16,21 +16,32 @@ export default async function DashboardLayout({
   children,
   params,
 }: DashboardLayoutProps) {
-  const { data } = await getUserSession();
+  const {
+    data: { session },
+  } = await getUserSession();
 
-  if (!data.session) {
+  if (!session) {
     redirect("/signin");
   }
 
-  const { workspace } = await getWorkspace(params.url_key);
-  if (!workspace) {
+  const { data, error } = await getWorkspace(params.url_key);
+  if (!data || error || !data.length) {
     redirect("/create");
   }
+  // console.log("===>", data);
+  // if (data?.length) {
+  //   redirect(`/${params.url_key}/team`);
+  // } else {
+  //   redirect("/create");
+  // }
+  // redirect(`/${params.url_key}/team`);
 
   return (
     <div className="relative mx-auto flex min-h-screen w-full flex-col items-center justify-center">
       <div className="flex w-full flex-1 gap-2 lg:gap-1">
-        <SidebarNav user_id={data.session.user.id} />
+        <SidebarNav
+          props={{ user_id: session.user.id, url_key: params.url_key }}
+        />
         <main className="z-10 flex w-full flex-1 flex-col items-start justify-center">
           <Shell
             variant="sidebar"
