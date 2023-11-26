5a3dacfaabbc634ff09831459762acd9e174b533
Erkan Turut <erkamturut@gmail.com>
2023-11-26 22:18:52 +0100

    Refactor workspace service functions

--- client/src/lib/services/workspace/index.ts
+++ client/src/lib/services/workspace/index.ts
@@ -10,34 +10,44 @@ import { Database } from "@/types/supabase.types";
 // export const getWorkspace = cache(
 //   async (url_key: string) => {
 //     const supabase = await createSupabaseServerClient();
-//     const { data: workspace, error } = await supabase
-//       .from("workspace")
-//       .select(`user_workspace!inner(*)`)
-//       .eq("url_key", url_key)
-//       .single();
+//     console.log(url_key);
+//     return await supabase.from("workspace").select(`*`).eq("url_key", url_key);
 
-//     if (error) {
-//       throw new Error(error.message);
-//     }
-//     if (!workspace) {
-//       throw new Error("User not found");
-//     }
-//     return workspace;
+//     // if (error) {
+//     //   throw new Error(error.message);
+//     // }
+//     // if (!workspace) {
+//     //   throw new Error("User not found");
+//     // }
+//     // return workspace;
 //   },
 //   ["workspace"],
 //   {
 //     tags: ["workspace"],
-//     revalidate: 1,
+//     revalidate: 20,
 //   }
 // );
 
 export const getWorkspace = async (url_key: string) => {
+  noStore();
+  const supabase = await createSupabaseServerClient();
+  return await supabase.from("workspace").select("*").eq("url_key", url_key);
+};
+
+export const getWorkspaces = async () => {
+  noStore();
+  const supabase = await createSupabaseServerClient();
+  return await supabase.from("workspace").select("*");
+};
+
+export const getUserWorkspace = async (user_id: string) => {
   const supabase = await createSupabaseServerClient();
   const { data: workspace, error } = await supabase
-    .from("workspace")
-    .select("*, user(*)")
-    .eq("url_key", url_key)
-    .single();
+    .from("user_workspace")
+    .select(`workspace!inner(*)`)
+    .eq("user_id", user_id);
+
+  console.log("workspace", workspace);
 
   return { workspace, error };
 };
