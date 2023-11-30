a7ea46c97eff502e3fa8dc416f91edd2129f15a0
Erkan Turut <erkamturut@gmail.com>
2023-11-30 20:52:09 +0100

    Refactor accountSettingsSchema in settings.ts

--- client/src/lib/validations/settings.ts
+++ client/src/lib/validations/settings.ts
@@ -1,25 +1,9 @@
 import * as z from "zod";
 
 export const accountSettingsSchema = z.object({
-  first_name: z.string(),
-
-  last_name: z.string(),
-
+  first_name: z.string().optional(),
+  last_name: z.string().optional(),
   email: z.string().email({ message: "Please enter a valid email address" }),
 });
 
-export const firstNameSchema = z.object({
-  firstName: z
-    .string()
-    .min(2, { message: "First name must be at least 2 characters long" }),
-});
-
-export const lastNameSchema = z.object({
-  lastName: z
-    .string()
-    .min(2, { message: "Last name must be at least 2 characters long" }),
-});
-
-export const emailSchema = z.object({
-  email: z.string().email({ message: "Please enter a valid email address" }),
-});
+export type TaccountSettingsSchema = z.infer<typeof accountSettingsSchema>;
