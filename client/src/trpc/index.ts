8ad68e7122062efb5d9c00495a17ad3a5957952c
Erkan Turut <erkamturut@gmail.com>
2023-11-30 20:52:12 +0100

    Add new routers to appRouter

--- client/src/trpc/index.ts
+++ client/src/trpc/index.ts
@@ -1,9 +1,13 @@
 import { publicProcedure, router } from "./trpc";
 
+import { authRouter } from "./routers/auth-router";
+import { userRouter } from "./routers/user";
+import { workspaceRouter } from "./routers/workspace";
+
 export const appRouter = router({
-  getNumber: publicProcedure.query(async () => {
-    return 3;
-  }),
+  auth: authRouter,
+  user: userRouter,
+  workspace: workspaceRouter,
 });
 
 export type AppRouter = typeof appRouter;
