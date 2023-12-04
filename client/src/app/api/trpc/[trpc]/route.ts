8b574a87f570e16b175a2a9ca4ffd1391de92ee5
Erkan Turut <erkamturut@gmail.com>
2023-12-04 21:51:37 +0100

    Refactor trpc route handler

--- client/src/app/api/trpc/[trpc]/route.ts
+++ client/src/app/api/trpc/[trpc]/route.ts
@@ -1,14 +1,38 @@
-import { appRouter } from "@/trpc";
-import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
+// import { appRouter } from "@/trpc";
+// import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 
-const handler = (req: Request) =>
+// const handler = (req: Request) =>
+//   fetchRequestHandler({
+//     endpoint: "/api/trpc",
+//     req,
+//     router: appRouter,
+//     createContext: () => ({}),
+//   });
+
+// export { handler as GET, handler as POST };
+
+import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
+import { type NextRequest } from "next/server";
+
+import { appRouter } from "@/trpc";
+import { createTRPCContext } from "@/trpc/trpc";
+
+/**
+ * This wraps the `createTRPCContext` helper and provides the required context for the tRPC API when
+ * handling a HTTP request (e.g. when you make requests from Client Components).
+ */
+const createContext = async (req: NextRequest) => {
+  return createTRPCContext({
+    headers: req.headers,
+  });
+};
+
+const handler = (req: NextRequest) =>
   fetchRequestHandler({
     endpoint: "/api/trpc",
     req,
     router: appRouter,
-    createContext: () => ({}),
+    createContext: () => createContext(req),
   });
-
-console.log("handler");
 
 export { handler as GET, handler as POST };
