caed6f173696e4789d6afd5150c239b9a7007200
Erkan Turut <erkamturut@gmail.com>
2023-12-04 21:51:09 +0100

    Refactor user and workspace services

--- client/src/app/(dashboard)/[url_key]/layout.tsx
+++ client/src/app/(dashboard)/[url_key]/layout.tsx
@@ -1,9 +1,9 @@
 import { SidebarNav } from "@/components/layouts/app-sidebar";
 import { Shell } from "@/components/shells/shell";
-import { getUserSession } from "@/lib/services/user";
-import { getWorkspace, getWorkspaces } from "@/lib/services/workspace";
-import { cookies } from "next/headers";
+import { getCurrentUser } from "@/lib/services/user";
+import { getWorkspace } from "@/lib/services/workspace";
 import { redirect } from "next/navigation";
+import { serverClient } from "@/trpc";
 
 interface DashboardLayoutProps {
   children: React.ReactNode;
@@ -16,18 +16,20 @@ export default async function DashboardLayout({
   children,
   params,
 }: DashboardLayoutProps) {
-  const {
-    data: { session },
-  } = await getUserSession();
+  const { data: user } = await getCurrentUser();
 
-  if (!session) {
+  if (!user) {
     redirect("/signin");
   }
 
-  const { data, error } = await getWorkspace(params.url_key);
-  if (!data || error || !data.length) {
-    redirect("/create");
-  }
+  // const { data, error } = await getWorkspace(params.url_key);
+  // const workspace = await serverClient.workspace.getWorkspaceById.query({
+  //   url_key: params.url_key,
+  // });
+
+  // if (!data || error || !data.length) {
+  //   redirect("/create");
+  // }
   // console.log("===>", data);
   // if (data?.length) {
   //   redirect(`/${params.url_key}/team`);
@@ -39,9 +41,7 @@ export default async function DashboardLayout({
   return (
     <div className="relative mx-auto flex min-h-screen w-full flex-col items-center justify-center">
       <div className="flex w-full flex-1 gap-2 lg:gap-1">
-        <SidebarNav
-          props={{ user_id: session.user.id, url_key: params.url_key }}
-        />
+        <SidebarNav props={{ url_key: params.url_key }} />
         <main className="z-10 flex w-full flex-1 flex-col items-start justify-center">
           <Shell
             variant="sidebar"
