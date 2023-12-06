d643ef598d313f64e1cc294dad78c9b1a7fbb9ea
Erkan Turut <erkamturut@gmail.com>
2023-12-06 22:01:16 +0100

    Refactor dashboard page to redirect to teams

--- client/src/app/(dashboard)/[url_key]/page.tsx
+++ client/src/app/(dashboard)/[url_key]/page.tsx
@@ -1,15 +1,4 @@
-import type { Metadata } from "next";
-
-import { LogOutButtons } from "@/components/auth/logoutButton";
-import {
-  PageHeader,
-  PageHeaderDescription,
-  PageHeaderHeading,
-} from "@/components/pageHeader";
-import { Shell } from "@/components/shells/shell";
-import { getUserSession } from "@/lib/services/user";
 import { redirect } from "next/navigation";
-import { getWorkspace } from "@/lib/services/workspace";
 interface DashboardLayoutProps {
   params: {
     url_key: string;
@@ -17,17 +6,5 @@ interface DashboardLayoutProps {
 }
 
 export default async function DashboardPage({ params }: DashboardLayoutProps) {
-  const {
-    data: { session },
-  } = await getUserSession();
-
-  if (!session) {
-    redirect("/signin");
-  }
-
-  const { data } = await getWorkspace(params.url_key);
-  if (data?.length) {
-    redirect(`/${params.url_key}/team`);
-  }
-  return null;
+  redirect(`/${params.url_key}/teams`);
 }
