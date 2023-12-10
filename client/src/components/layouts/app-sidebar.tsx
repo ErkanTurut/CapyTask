ed37710199ae508b39c6e8b515cc0e3e674accbd
Erkan Turut <erkamturut@gmail.com>
2023-12-10 23:49:47 +0100

    Refactor app-sidebar component to use Prisma models

--- client/src/components/layouts/app-sidebar.tsx
+++ client/src/components/layouts/app-sidebar.tsx
@@ -7,25 +7,19 @@ import { FooterSidebar } from "./sidebar/footer-sidebar";
 import { dashboardConfig } from "@/config/dashboard.config";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import WorkspaceNav from "@/components/workspace-navigation";
-import { useRouter } from "next/router";
+import { workspace, team } from "@prisma/client";
 
 export interface SidebarNavProps extends React.HTMLAttributes<HTMLDivElement> {
   props: {
-    user_id: string;
-    url_key: string;
+    workspaces: Pick<workspace, "name" | "description" | "url_key">[];
+    teams: Pick<team, "name" | "description">[] | null;
   };
 }
 
 import { Suspense } from "react";
 import ResizeZone from "./resize-zone";
-import { serverClient } from "@/trpc/serverClient";
 
 export async function SidebarNav({ className, props }: SidebarNavProps) {
-  const workspaces = await serverClient.workspace.getWorkspaces();
-  const user = await serverClient.user.getCurrentUser();
-
-  if (!user) return null;
-
   return (
     <ResizeZone>
       <div
@@ -37,23 +31,12 @@ export async function SidebarNav({ className, props }: SidebarNavProps) {
       >
         <div className="flex flex-col w-full h-full gap-1">
           <Suspense fallback="loading...">
-            <WorkspaceNav
-              url_key={props.url_key}
-              initialData={{ workspaces }}
-            />
+            <WorkspaceNav workspaces={props.workspaces} />
           </Suspense>
-          {/* <Suspense fallback="loading...">
-            <UserAccountNav initialData={{ user }} className="h-7 w-7" />
-          </Suspense> */}
-
           {/* <ThemeToggle toggle={true} /> */}
-
           <nav className="flex w-full flex-col gap-1 flex-grow overflow-hidden">
             <div className="flex w-full flex-col gap-1 overflow-hidden">
-              <HeaderSidebar
-                url_key={props.url_key}
-                items={dashboardConfig.sidebarNav}
-              />
+              <HeaderSidebar items={dashboardConfig.sidebarNav} />
               <Separator className="flex" />
               <div className="flex-grow overflow-y-auto">
                 <ScrollArea>
