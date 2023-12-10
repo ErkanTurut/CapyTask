bce517977e9d98a7f021de9b601b7c483900358c
Erkan Turut <erkamturut@gmail.com>
2023-12-10 23:48:17 +0100

    Add signup action and schema

---
+++ client/src/lib/actions/auth/signup/schema.ts
@@ -0,0 +1,19 @@
+import { z } from "zod";
+
+export const ZSignUp = z
+  .object({
+    email: z.string().email({ message: "Please enter a valid email address" }),
+    password: z
+      .string()
+      .min(6, { message: "Password must be at least 6 characters long" })
+      .max(100, { message: "Password must be less than 100 characters long" })
+      .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})/, {
+        message:
+          "Password must contain at least one uppercase letter, one lowercase letter, one number and one special character",
+      }),
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
+  });
