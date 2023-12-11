99ed5505d04c04426efd0404e3395b245e9c60a7
Erkan Turut <erkamturut@gmail.com>
2023-12-12 00:19:15 +0100

    Refactor create page layout and styles

--- client/src/app/(dashboard)/create/page.tsx
+++ client/src/app/(dashboard)/create/page.tsx
@@ -19,6 +19,8 @@ import { redirect } from "next/navigation";
 import { CreateWorspaceForm } from "@/components/workspace/workspace-create";
 import Link from "next/link";
 import { getSession } from "@/lib/auth";
+import { cn } from "@/utils";
+import { buttonVariants } from "@/components/ui/button";
 
 export default async function CreatePage() {
   const { data } = await getSession();
@@ -28,37 +30,29 @@ export default async function CreatePage() {
   }
 
   return (
-    <Shell variant="centered">
-      <PageHeader
-        id="sign-out-page-header"
-        aria-labelledby="sign-out-page-header-heading"
-        className="text-center"
-      >
-        <PageHeaderHeading size="sm">Create</PageHeaderHeading>
-        <PageHeaderDescription size="sm">
-          Create your own workspace
-        </PageHeaderDescription>
-      </PageHeader>
-      <Card>
-        <CardHeader className="space-y-1">
-          <CardTitle className="text-2xl">Sign in</CardTitle>
-          <CardDescription>
-            Choose your preferred sign in method
-          </CardDescription>
-        </CardHeader>
-        <CardContent className="grid gap-4">
-          <CreateWorspaceForm />
-        </CardContent>
-        <CardFooter className="flex flex-wrap items-center justify-between gap-2">
-          <Link
-            aria-label="Reset password"
-            href="/reset-password"
-            className="text-sm text-primary underline-offset-4 transition-colors hover:underline"
-          >
-            Reset password
-          </Link>
-        </CardFooter>
-      </Card>
-    </Shell>
+    <div className="grid min-h-screen">
+      <div className="container max-w-3xl my-auto ">
+        <Card>
+          <CardHeader className="space-y-1">
+            <CardTitle className="text-2xl">Create workspace</CardTitle>
+            <CardDescription>
+              Create a new workspace to collaborate with your buddies.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="grid gap-4">
+            <CreateWorspaceForm />
+          </CardContent>
+          <CardFooter className="flex flex-wrap items-center justify-between gap-2">
+            <Link
+              aria-label="Return to home page"
+              href="/"
+              className={cn(buttonVariants({ variant: "secondary" }), "w-full")}
+            >
+              Return to home page
+            </Link>
+          </CardFooter>
+        </Card>
+      </div>
+    </div>
   );
 }
