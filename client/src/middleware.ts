861ef0aed722c79d31a9dcd299eef21d85018723
Erkan Turut <erkamturut@gmail.com>
2023-12-12 22:21:10 +0100

    Refactor middleware and update routing logic

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -1,6 +1,5 @@
 import { createServerClient, type CookieOptions } from "@supabase/ssr";
 import { NextResponse, type NextRequest } from "next/server";
-import { serverClient } from "./trpc";
 
 export async function middleware(request: NextRequest) {
   let response = NextResponse.next({
@@ -55,39 +54,62 @@ export async function middleware(request: NextRequest) {
     }
   );
 
-  await supabase.auth.getSession();
+  const {
+    data: { session },
+    error,
+  } = await supabase.auth.getSession();
 
-  // if (!session) {
-  //   return NextResponse.redirect(new URL("signin", request.url));
-  // }
+  const redirectToDefaultWorkspace = async () => {
+    const { data: workspaces } = await supabase.from("workspace").select("*");
+    let url = new URL("/", request.url);
+    if (!workspaces || workspaces.length < 1) {
+      url = new URL("/create", request.url);
+    } else {
+      url = new URL(`/dashboard/${workspaces[0].url_key}`, request.url);
+    }
+    return NextResponse.redirect(url);
+  };
 
-  // if (
-  //   request.nextUrl.pathname === "/signin" ||
-  //   (request.nextUrl.pathname === "/signup" && session)
-  // ) {
-  //   const workspaces = await serverClient.workspace.getWorkspaces.query();
-  //   if (workspaces.length === 0) {
-  //     return NextResponse.redirect(new URL("/create", request.url));
-  //   }
-  //   if (workspaces.length > 0) {
-  //     return NextResponse.redirect(
-  //       new URL(`/${workspaces[0].url_key}`, request.url)
-  //     );
-  //   }
-  //   // console.log(request.nextUrl.pathname);
-  //   // const { data, error } = await getWorkspace(slug);
-  //   // console.log(data, error);
-  //   // if (error || !data || data.length === 0) {
-  //   //   return NextResponse.redirect(new URL("dashboard/create", request.url));
-  //   // }
-  //   // return NextResponse.redirect(
-  //   //   new URL("/dashboard/account/settings", request.url)
-  //   // );
-  // }
+  const navigateToWorkspace = async (url_key: string) => {
+    const { data: workspaces } = await supabase
+      .from("workspace")
+      .select("*")
+      .eq("url_key", url_key);
+    if (!workspaces || workspaces.length < 1) {
+      return NextResponse.redirect(new URL("/create", request.url));
+    }
+    return NextResponse.next();
+  };
+
+  // f**k this
+  if (
+    request.nextUrl.pathname === "/signin" ||
+    request.nextUrl.pathname === "/signup"
+  ) {
+    if (!session) {
+      return NextResponse.next();
+    }
+    return redirectToDefaultWorkspace();
+  }
+  if (request.nextUrl.pathname.startsWith("/dashboard")) {
+    if (!session) {
+      return NextResponse.redirect(new URL("/signin", request.url));
+    }
+    const url_key = request.nextUrl.pathname.split("/")?.[2];
+    if (!url_key) {
+      return redirectToDefaultWorkspace();
+    }
+    if (url_key === "create" || url_key === "join") {
+      return NextResponse.next();
+    }
+    return navigateToWorkspace(url_key);
+  }
 
   return response;
 }
 
-// export const config = {
-//   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
-// };
+export const config = {
+  matcher: [
+    "/((?!api|assets|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+  ],
+};
