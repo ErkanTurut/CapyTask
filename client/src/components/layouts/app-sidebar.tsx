d86c3187c741c5355f4665b79eb2caefab979727
Erkan Turut <erkamturut@gmail.com>
2023-12-14 22:38:21 +0100

    Refactor app-sidebar component

--- client/src/components/layouts/app-sidebar.tsx
+++ client/src/components/layouts/app-sidebar.tsx
@@ -1,51 +1,65 @@
-import Link from "next/link";
 import { Separator } from "@/components/ui/separator";
 import { cn } from "@/utils";
 import { MainSidebar } from "./sidebar/main-sidebar";
 import { HeaderSidebar } from "./sidebar/header-sidebar";
 import { FooterSidebar } from "./sidebar/footer-sidebar";
-import { dashboardConfig } from "@/config/dashboard.config";
+
 import { ScrollArea } from "@/components/ui/scroll-area";
 import WorkspaceNav from "@/components/workspace-navigation";
-
-export interface SidebarProps extends React.HTMLAttributes<HTMLDivElement> {
-  props: {
-    workspaces: Database["public"]["Tables"]["workspace"]["Row"][];
-    teams: Database["public"]["Tables"]["team"]["Row"][] | null;
-  };
-}
 
 import { Suspense } from "react";
 import ResizeZone from "./resize-zone";
 import { Database } from "@/types/supabase.types";
+import { TDashboardConfig } from "@/config/dashboard.config";
+import { SidebarNavItem } from "@/types";
+import UserAccountNav from "../user-account-navigation";
+
+export interface SidebarProps extends React.HTMLAttributes<HTMLDivElement> {
+  props: {
+    workspaces?: Database["public"]["Tables"]["workspace"]["Row"][];
+    teams?: Database["public"]["Tables"]["team"]["Row"][] | null;
+    sidebarnav: SidebarNavItem;
+    user?: Database["public"]["Tables"]["user"]["Row"];
+  };
+}
 
 export async function Sidebar({ className, props }: SidebarProps) {
   return (
     <ResizeZone>
-      <div
-        className={cn(
-          "flex w-full h-full flex-grow overflow-hidden",
-          className
-        )}
-        {...props}
-      >
+      <div className={cn("flex w-full h-full", className)} {...props}>
         <div className="flex flex-col w-full h-full gap-1">
-          <Suspense fallback="loading...">
-            <WorkspaceNav workspaces={props.workspaces} />
-          </Suspense>
-          {/* <ThemeToggle toggle={true} /> */}
-          <nav className="flex w-full flex-col gap-1 flex-grow overflow-hidden">
-            <div className="flex w-full flex-col gap-1 overflow-hidden">
-              <HeaderSidebar items={dashboardConfig.sidebarNav} />
+          <div className="flex gap-1 items-center">
+            {props.workspaces && (
+              <Suspense fallback="loading...">
+                <WorkspaceNav
+                  className="max-w-s overflow-x-hidden"
+                  workspaces={props.workspaces}
+                />
+              </Suspense>
+            )}
+            {props.user && (
+              <Suspense fallback="loading...">
+                <UserAccountNav
+                  className="w-7 h-7 flex-grow"
+                  user={props.user}
+                />
+              </Suspense>
+            )}
+          </div>
+
+          <nav className="flex w-full flex-col gap-1 flex-grow">
+            <div className="flex w-full flex-col gap-1">
+              <HeaderSidebar items={props.sidebarnav} />
               <Separator className="flex" />
               <div className="flex-grow overflow-y-auto">
                 <ScrollArea>
-                  <MainSidebar items={dashboardConfig.sidebarNav} />
+                  <MainSidebar items={props.sidebarnav} />
                 </ScrollArea>
               </div>
             </div>
           </nav>
-          <FooterSidebar items={dashboardConfig.sidebarNav} />
+
+          {props.sidebarnav && <FooterSidebar items={props.sidebarnav} />}
         </div>
       </div>
     </ResizeZone>
