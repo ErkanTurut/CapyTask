f86ce54dd1f1772d55f3e77a71cd2d9498ec9b70
Erkan Turut <erkamturut@gmail.com>
2023-12-14 22:38:30 +0100

    Fix import in HeaderSidebar component

--- client/src/components/layouts/sidebar/header-sidebar.tsx
+++ client/src/components/layouts/sidebar/header-sidebar.tsx
@@ -7,7 +7,7 @@ import { useParams, usePathname } from "next/navigation";
 import { cn } from "@/utils";
 import { buttonVariants } from "@/components/ui/button";
 import { Icons } from "@/components/icons";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 export interface HeaderSidebarProps
   extends React.HTMLAttributes<HTMLDivElement> {
@@ -16,32 +16,50 @@ export interface HeaderSidebarProps
 
 export function HeaderSidebar({
   items,
-
   className,
   ...props
 }: HeaderSidebarProps) {
   const pathname = usePathname();
   const params = useParams();
+  const router = useRouter();
 
+  if (!items.header) return null;
   return (
     <span className={cn("flex w-full flex-col gap-1 ", className)} {...props}>
       {items.header.map((headerItem, headerIndex) => {
+        const Icon = headerItem.icon ? Icons[headerItem.icon] : null;
+
         return (
           <span key={headerIndex} className={cn("flex w-full gap-1 flex-col")}>
             {headerItem.label && (
-              <h2 className="flex w-full items-center py-1 text-sm font-semibold text-muted-foreground">
+              <h2
+                onClick={() => router.back()}
+                className={cn(
+                  "flex w-full items-center py-1 text-lg font-semibold text-muted-foreground cursor-pointer hover:text-primary"
+                )}
+              >
+                {Icon && (
+                  <Icon
+                    className="mr-2 w-6 h-6"
+                    aria-hidden="true"
+                    fill="currentColor"
+                  />
+                )}
                 {headerItem.label}
               </h2>
             )}
             {headerItem.items.length > 0
               ? headerItem.items.map((subitem, subIndex) => {
                   const Icon = subitem.icon ? Icons[subitem.icon] : null;
-                  const link = `/dashboard/${params?.url_key}${subitem.href}`;
                   return (
                     <Link
                       aria-label={subitem.title}
                       key={subIndex}
-                      href={subitem.href ? link : ""}
+                      href={
+                        subitem.href
+                          ? `dashboard/${params?.url_key}${subitem.href}`
+                          : ""
+                      }
                       target={subitem.external ? "_blank" : ""}
                       rel={subitem.external ? "noreferrer" : ""}
                       className={cn(
