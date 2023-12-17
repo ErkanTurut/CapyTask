9507822d23960e414421a6753725c8a5a4e83bc9
Erkan Turut <erkamturut@gmail.com>
2023-12-18 00:36:46 +0100

    Add Button and Icons imports to Accordion component

--- client/src/components/ui/accordion.tsx
+++ client/src/components/ui/accordion.tsx
@@ -5,6 +5,8 @@ import * as AccordionPrimitive from "@radix-ui/react-accordion";
 import { ChevronDownIcon } from "@radix-ui/react-icons";
 
 import { cn } from "@/utils";
+import { Button } from "./button";
+import { Icons } from "../icons";
 
 const Accordion = AccordionPrimitive.Root;
 
@@ -12,11 +14,7 @@ const AccordionItem = React.forwardRef<
   React.ElementRef<typeof AccordionPrimitive.Item>,
   React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Item>
 >(({ className, ...props }, ref) => (
-  <AccordionPrimitive.Item
-    ref={ref}
-    className={cn("border-b", className)}
-    {...props}
-  />
+  <AccordionPrimitive.Item ref={ref} className={cn(className)} {...props} />
 ));
 AccordionItem.displayName = "AccordionItem";
 
