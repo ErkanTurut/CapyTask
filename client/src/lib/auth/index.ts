2c4b9cb0edb00ee6b1686b660ac67c1b476a09eb
Erkan Turut <erkamturut@gmail.com>
2023-12-18 00:36:54 +0100

    Remove unused auth code

--- client/src/lib/auth/index.ts
+++
@@ -1,6 +0,0 @@
-import createSupabaseServerClient from "../supabase/server";
-
-export const getSession = async () => {
-  const supabase = await createSupabaseServerClient();
-  return supabase.auth.getSession();
-};
