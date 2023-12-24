9bf64639d5e8822ca82b68085487482fb037079c
Erkan Turut <erkamturut@gmail.com>
2023-12-25 00:50:52 +0100

    Add App and Sidebar components

---
+++ client/src/app/(dashboard)/dashboard/[url_key]/(app)/_components/app.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
+import { cn } from "@/lib/utils";
+import { useState, FC, useEffect } from "react";
+import { Sidebar } from "./sidebar";
+import { Shell } from "@/components/shells/shell";
+import { useParams } from "next/navigation";
+
+interface appProps {
+  children: React.ReactNode;
+  defaultLayout: number[];
+  defaultCollapsed: boolean;
+  navCollapsedSize: number;
+}
+
+const App: FC<appProps> = ({
+  children,
+  defaultLayout = [30, 70],
+  navCollapsedSize,
+  defaultCollapsed = false,
+}) => {
+  const [isCollapsed, setIsCollapsed] = useState(defaultCollapsed);
+  const { url_key } = useParams();
+
+  useEffect(() => {
+    document.cookie = `gembuddy:workspace_url_key=${JSON.stringify(
+      url_key
+    )};path=/`;
+  }, [url_key]);
+
+  return (
+    <ResizablePanelGroup
+      direction="horizontal"
+      onLayout={(sizes: number[]) => {
+        document.cookie = `react-resizable-panels:layout=${JSON.stringify(
+          sizes
+        )};path=/`;
+      }}
+      className="min-h-screen  items-stretch"
+    >
+      <ResizablePanel
+        defaultSize={defaultLayout[0]}
+        collapsedSize={navCollapsedSize}
+        collapsible={true}
+        minSize={14}
+        maxSize={30}
+        onCollapse={() => {
+          setIsCollapsed(true);
+          document.cookie = `react-resizable-panels:collapsed=${JSON.stringify(
+            true
+          )};path=/`;
+        }}
+        onExpand={() => {
+          console.log("expanded");
+          setIsCollapsed(false);
+          document.cookie = `react-resizable-panels:collapsed=${JSON.stringify(
+            false
+          )};path=/`;
+        }}
+        className={cn(isCollapsed && "transition-all duration-300 ease-in-out")}
+      >
+        <Sidebar isCollapsed={isCollapsed} />
+      </ResizablePanel>
+      <ResizableHandle withHandle />
+      <ResizablePanel defaultSize={defaultLayout[1]}>
+        <Shell
+          variant="sidebar"
+          className="relative flex-1 flex-col overflow-x-hidden  lg:ml-0 backdrop-blur-[1px]"
+        >
+          {children}
+        </Shell>
+      </ResizablePanel>
+    </ResizablePanelGroup>
+  );
+};
+
+export default App;
