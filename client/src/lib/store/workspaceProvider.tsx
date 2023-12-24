006e66b7251f62c26cd93cff51441eb49ad1f05f
Erkan Turut <erkamturut@gmail.com>
2023-12-25 00:51:34 +0100

    Add WorkspaceProvider component

---
+++ client/src/lib/store/workspaceProvider.tsx
@@ -0,0 +1,55 @@
+import { Database } from "@/types/supabase.types";
+import { createContext, useContext, useState } from "react";
+import { create } from "zustand";
+
+interface WorkspaceState {
+  workspace: Database["public"]["Tables"]["workspace"]["Row"] | null;
+  setWorkspace: (
+    workspace: Database["public"]["Tables"]["workspace"]["Row"] | null
+  ) => void;
+
+  workspaceList: Database["public"]["Tables"]["workspace"]["Row"][] | null;
+  setWorkspaceList: (
+    workspaceList: Database["public"]["Tables"]["workspace"]["Row"][] | null
+  ) => void;
+}
+
+const createWorkspace = (
+  workspace: WorkspaceState["workspace"],
+  workspaceList: WorkspaceState["workspaceList"]
+) =>
+  create<WorkspaceState>((set) => ({
+    workspace,
+    setWorkspace: (workspace) => set({ workspace }),
+    workspaceList,
+    setWorkspaceList: (workspaceList) => set({ workspaceList }),
+  }));
+
+const WorkspaceContext = createContext<ReturnType<
+  typeof createWorkspace
+> | null>(null);
+
+export const useWorkspace = () => {
+  if (!WorkspaceContext)
+    throw new Error("useWorkspace must be used within a WorkspaceProvider");
+  return useContext(WorkspaceContext)!;
+};
+
+// workspace provider
+
+export const WorkspaceProvider = ({
+  workspace,
+  workspaceList,
+  children,
+}: {
+  workspace: Database["public"]["Tables"]["workspace"]["Row"];
+  workspaceList: Database["public"]["Tables"]["workspace"]["Row"][];
+  children: React.ReactNode;
+}) => {
+  const [store] = useState(() => createWorkspace(workspace, workspaceList));
+  return (
+    <WorkspaceContext.Provider value={store}>
+      {children}
+    </WorkspaceContext.Provider>
+  );
+};
