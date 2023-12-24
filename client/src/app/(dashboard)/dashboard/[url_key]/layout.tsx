af77da6f1b09d16e07290a7e37ad6932c2fba690
Erkan Turut <erkamturut@gmail.com>
2023-12-25 00:49:39 +0100

    Delete unused files and functions

--- client/src/app/(dashboard)/dashboard/[url_key]/layout.tsx
+++
@@ -1,9 +0,0 @@
-interface DashboardLayoutProps {
-  children: React.ReactNode;
-}
-
-export default async function DashboardLayout({
-  children,
-}: DashboardLayoutProps) {
-  return <>{children}</>;
-}
