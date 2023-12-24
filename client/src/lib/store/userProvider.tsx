c27f24a76da1706e37989c53cef5c5d5d2e246bb
Erkan Turut <erkamturut@gmail.com>
2023-12-25 00:51:39 +0100

    Add UserProvider component

---
+++ client/src/lib/store/userProvider.tsx
@@ -0,0 +1,39 @@
+import { Database } from "@/types/supabase.types";
+import { createContext, useContext, useState } from "react";
+import { create } from "zustand";
+// Database["public"]["Tables"]["user"]["Row"];
+interface UserState {
+  user: Database["public"]["Tables"]["user"]["Row"] | null;
+  setUser: (user: Database["public"]["Tables"]["user"]["Row"] | null) => void;
+  // updateUser: (
+  //   updatedUser: Database["public"]["Tables"]["user"]["Row"]
+  // ) => void;
+}
+
+const createUser = (user: UserState["user"]) =>
+  create<UserState>((set) => ({
+    user,
+    setUser: (user) => set({ user }),
+
+    //  updateUser: (updatedUser) =>
+    //   set((state) => ({ user: { ...state.user, ...updatedUser } })),
+  }));
+
+const UserContext = createContext<ReturnType<typeof createUser> | null>(null);
+
+export const useUser = () => {
+  if (!UserContext)
+    throw new Error("useUser must be used within a UserProvider");
+  return useContext(UserContext)!;
+};
+
+export const UserProvider = ({
+  user,
+  children,
+}: {
+  user: Database["public"]["Tables"]["user"]["Row"];
+  children: React.ReactNode;
+}) => {
+  const [store] = useState(() => createUser(user));
+  return <UserContext.Provider value={store}>{children}</UserContext.Provider>;
+};
