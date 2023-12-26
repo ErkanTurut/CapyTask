b520cb414310accea1e579c84ca09fd2415e0078
Erkan Turut <erkamturut@gmail.com>
2023-12-26 22:27:50 +0100

    Refactor settings layout component

--- client/src/app/(dashboard)/dashboard/[url_key]/(settings)/settings/layout.tsx
+++ client/src/app/(dashboard)/dashboard/[url_key]/(settings)/settings/layout.tsx
@@ -1,10 +1,5 @@
-import { Sidebar } from "@/components/layouts/app-sidebar";
 import { Shell } from "@/components/shells/shell";
-import { getUser } from "@/lib/services/user";
-import { getWorkspaces } from "@/lib/services/workspace";
-import { redirect } from "next/navigation";
-import { getTeams } from "@/lib/services/team";
-import { dashboardConfig } from "@/config/dashboard.config";
+import { Sidebar } from "./_components/sidebar";
 
 interface SettingsLayoutProps {
   children: React.ReactNode;
@@ -20,12 +15,7 @@ export default async function SettingsLayout({
   return (
     <div className="relative mx-auto flex min-h-screen w-full flex-col items-center justify-center">
       <div className="flex w-full flex-1 gap-2 lg:gap-1">
-        {/* <Sidebar
-          props={{
-            sidebarnav: dashboardConfig.settingsNav,
-          }}
-        /> */}
-
+        <Sidebar />
         <main className="z-10 flex w-full flex-1 flex-col items-start">
           <Shell variant="markdown">{children}</Shell>
         </main>
