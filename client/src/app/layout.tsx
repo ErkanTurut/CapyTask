df01e1d6af209661eb353b98c56ebc8a3363a06e
Erkan Turut <erkamturut@gmail.com>
2024-01-01 00:05:31 +0100

    Enable TailwindIndicator in RootLayout

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -21,7 +21,7 @@ export default async function RootLayout({
       >
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           {children}
-          {/* <TailwindIndicator /> */}
+          <TailwindIndicator />
           <Toaster />
         </ThemeProvider>
       </body>
