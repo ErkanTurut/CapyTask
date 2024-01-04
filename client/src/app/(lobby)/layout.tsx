95ddfedef230500d1df963b2a84d8a079f770f71
Erkan Turut <erkamturut@gmail.com>
2024-01-04 21:38:47 +0100

    test on branch

--- client/src/app/(lobby)/layout.tsx
+++ client/src/app/(lobby)/layout.tsx
@@ -8,6 +8,7 @@ export default async function LobbyLayout({ children }: LobbyLayoutProps) {
   return (
     <div className="relative flex min-h-screen flex-col">
       <NavBar />
+      {/* test */}
       <main className="flex-1">{children}</main>
     </div>
   );
