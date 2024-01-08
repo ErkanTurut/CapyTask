3bca59efa0e72d3e95d5f27c6f340e6d2b310f84
Erkan Turut <erkamturut@gmail.com>
2024-01-08 22:09:57 +0100

    Update image source in AuthLayout component

--- client/app/app/(auth)/layout.tsx
+++ client/app/app/(auth)/layout.tsx
@@ -21,8 +21,8 @@ export default async function AuthLayout({ children }: AuthLayoutProps) {
     <div className="grid min-h-screen grid-cols-1 overflow-hidden md:grid-cols-3 lg:grid-cols-2">
       <AspectRatio ratio={16 / 9}>
         <Image
-          src="/images/auth-light-bg.jpg"
-          alt="A skateboarder doing a high drop"
+          src="https://images.unsplash.com/photo-1573655349936-de6bed86f839?q=80&w=2820&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="Polar aurora landscape"
           fill
           className="absolute inset-0 object-cover"
           priority
