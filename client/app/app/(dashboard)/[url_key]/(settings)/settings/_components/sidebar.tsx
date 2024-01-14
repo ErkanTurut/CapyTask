13c931003a7aa6aece065ee24b50643832d8eb2d
Erkan Turut <erkamturut@gmail.com>
2024-01-14 23:51:28 +0100

    Refactor sidebar component and add workspace validation

--- client/app/app/(dashboard)/[url_key]/(settings)/settings/_components/sidebar.tsx
+++ client/app/app/(dashboard)/[url_key]/(settings)/settings/_components/sidebar.tsx
@@ -5,14 +5,16 @@ import { Nav } from "./nav";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { settingsNavItems } from "@/config/dashboard.config";
 import { TeamList } from "./team-list";
-import { getTeamsByUrlKey } from "@/lib/service/team/fetch";
+import { getTeams, getTeamsByUrlKey } from "@/lib/service/team/fetch";
+import { Database } from "@/types/supabase.types";
 
 export interface SidebarProps extends React.HTMLAttributes<HTMLDivElement> {
   url_key: string;
+  workspace: Database["public"]["Tables"]["workspace"]["Row"];
 }
 
-export async function Sidebar({ url_key }: SidebarProps) {
-  const { data: teams } = await getTeamsByUrlKey(url_key);
+export async function Sidebar({ url_key, workspace }: SidebarProps) {
+  const { data: teams } = await getTeams(workspace.id);
 
   return (
     <aside className="group/sidebar hidden max-h-[100vh] w-[230px] shrink-0  overflow-y-hidden border-r p-2 backdrop-blur-[1px] lg:sticky lg:top-28 lg:block">
@@ -44,7 +46,7 @@ export async function Sidebar({ url_key }: SidebarProps) {
                 href: "/members",
               },
             ]}
-            teams={teams ? teams.team : null}
+            teams={teams}
           />
           <Separator />
           <Nav
--- client/app/app/(dashboard)/[url_key]/(settings)/settings/layout.tsx
+++ client/app/app/(dashboard)/[url_key]/(settings)/settings/layout.tsx
@@ -1,5 +1,7 @@
 import { Shell } from "@/components/shells";
 import { Sidebar } from "./_components/sidebar";
+import { getWorkspace, getWorkspaces } from "@/lib/service/workspace/fetch";
+import { redirect } from "next/navigation";
 
 interface SettingsLayoutProps {
   children: React.ReactNode;
@@ -12,10 +14,16 @@ export default async function SettingsLayout({
   children,
   params,
 }: SettingsLayoutProps) {
+  const { data: workspace } = await getWorkspace(params.url_key);
+
+  if (!workspace) {
+    redirect("/create");
+  }
+
   return (
     <div className="relative mx-auto flex min-h-screen w-full flex-col items-center justify-center">
       <div className="flex w-full flex-1 gap-2 lg:gap-1">
-        <Sidebar url_key={params.url_key} />
+        <Sidebar url_key={params.url_key} workspace={workspace} />
         <main className="z-10 flex w-full flex-1 flex-col items-start">
           <Shell variant="markdown">{children}</Shell>
         </main>
