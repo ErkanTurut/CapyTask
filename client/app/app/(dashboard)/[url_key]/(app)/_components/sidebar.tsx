91cfe1b69ebda82971f3f9d37610b2f65528bb7d
Erkan Turut <erkamturut@gmail.com>
2024-01-20 00:53:31 +0100

    create sidebar rsc

--- client/app/app/(dashboard)/[url_key]/(app)/_components/sidebar.tsx
+++ client/app/app/(dashboard)/[url_key]/(app)/_components/sidebar.tsx
@@ -1,78 +1,129 @@
-"use client";
+import { Nav } from "@/components/layouts/side-navigation/nav";
+import { TeamList } from "@/components/team/team-list";
+import { Separator } from "@/components/ui/separator";
+import UserAccountNav from "@/components/user/user-account-nav";
+import WorkspaceNav from "@/components/workspace/workspace-navigation";
+import WorkspaceSkeleton from "@/components/workspace/workspace-skeleton";
+import { appNavItems } from "@/config/dashboard.config";
+import { cookies } from "next/headers";
+import { FC, Suspense } from "react";
+import {
+  SidebarBody,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarLayout,
+} from "./sidebar-layout";
 
-import { ResizableHandle, ResizablePanel } from "@/components/ui/resizable";
-import { cn } from "@/lib/utils";
-import React from "react";
+import { getTeams } from "@/lib/service/team/fetch";
 
-import { useSidebar } from "@/lib/store";
-import { unstable_batchedUpdates } from "react-dom";
+import { getSession } from "@/lib/service/auth/fetch";
+import { getUser } from "@/lib/service/user/fetch";
 
-export interface SidebarProps extends React.HTMLAttributes<HTMLDivElement> {
-  children: React.ReactNode;
-  defaultLayout?: number[];
-  defaultCollapsed?: boolean;
-  navCollapsedSize?: number;
+import { getWorkspaces } from "@/lib/service/workspace/fetch";
+import { createClient } from "@/lib/supabase/server";
+import { redirect } from "next/navigation";
+
+interface sidebarProps {
+  params: {
+    url_key: string;
+  };
 }
 
-export function Sidebar({
-  children,
-  defaultLayout = [20, 80],
-  navCollapsedSize,
-  defaultCollapsed = false,
-  className,
-}: SidebarProps) {
-  const setIsCollapsed = useSidebar()((state) => state.setIsCollapsed);
-  const isCollapsed = useSidebar()((state) => state.isCollapsed);
-  const handlePanelChange = (collapsed: boolean) => {
-    unstable_batchedUpdates(() => {
-      setIsCollapsed(collapsed);
-    });
-    document.cookie = `react-resizable-panels:collapsed=${JSON.stringify(
-      collapsed,
-    )};path=/`;
-  };
+const Sidebar: FC<sidebarProps> = async ({ params }) => {
+  const cookieStore = cookies();
+  const supabase = createClient(cookieStore);
+  const layout = cookies().get("react-resizable-panels:layout");
+  const collapsed = cookies().get("react-resizable-panels:collapsed");
+  const defaultLayout = layout
+    ? (JSON.parse(layout.value) as number[])
+    : undefined;
+  const defaultCollapsed = collapsed
+    ? (JSON.parse(collapsed.value) as boolean)
+    : undefined;
+  const {
+    data: { session },
+    error,
+  } = await getSession(supabase);
+  if (!session || error) {
+    redirect("/login");
+  }
+
+  const { data: workspaces } = await getWorkspaces(supabase);
+
+  if (!workspaces) {
+    redirect("/create");
+  }
+  const workspace = workspaces.find(
+    (workspace) => workspace.url_key === params.url_key,
+  );
+
+  if (!workspace) {
+    redirect("/create");
+  }
 
   return (
-    <>
-      <ResizablePanel
-        defaultSize={defaultLayout[0]}
-        collapsedSize={navCollapsedSize}
-        collapsible={true}
-        minSize={13}
-        maxSize={20}
-        onCollapse={() => handlePanelChange(true)}
-        onExpand={() => handlePanelChange(false)}
-        className={cn(
-          "flex min-w-[120px] flex-col justify-between",
-          isCollapsed && "min-w-[50px] transition-all duration-300 ease-in-out",
-          className,
-        )}
-        data-collapsed={isCollapsed}
-      >
-        {children}
-      </ResizablePanel>
-      <ResizableHandle withHandle />
-    </>
+    <SidebarLayout
+      defaultLayout={defaultLayout}
+      defaultCollapsed={defaultCollapsed}
+    >
+      <div>
+        <SidebarHeader className="grid gap-1 p-2">
+          <Suspense fallback={<WorkspaceSkeleton />}>
+            <WorkspaceNav workspaces={workspaces} workspace={workspace} />
+          </Suspense>
+          <Nav rootPath={`/${params.url_key}`} items={appNavItems.header} />
+          <Separator />
+        </SidebarHeader>
+        <SidebarBody className="px-2">
+          <Suspense fallback={<div>Loading...</div>}>
+            {(async () => {
+              const { data: teams } = await getTeams(workspace.id, supabase);
+              return (
+                <TeamList
+                  rootPath={`/${params.url_key}/team`}
+                  items={[
+                    {
+                      title: "Members",
+                      icon: "user",
+                      variant: "ghost",
+                      href: "/members",
+                    },
+                    {
+                      title: "Plans",
+                      icon: "route",
+                      variant: "ghost",
+                      href: "/plans",
+                    },
+                    {
+                      title: "Repports",
+                      icon: "fileText",
+                      variant: "ghost",
+                      href: "/repports",
+                    },
+                  ]}
+                  teams={teams}
+                />
+              );
+            })()}
+          </Suspense>
+        </SidebarBody>
+      </div>
+      <SidebarFooter className="flex flex-col gap-1 p-2">
+        <Separator />
+        <Nav rootPath={`/${params.url_key}`} items={appNavItems.footer} />
+        <Separator />
+        <Suspense fallback={<div>Loading...</div>}>
+          {(async () => {
+            const { data: user } = await getUser(session.user.id, supabase);
+            if (!user) {
+              redirect("/login");
+            }
+            return <UserAccountNav className="w-full" user={user} />;
+          })()}
+        </Suspense>
+      </SidebarFooter>
+    </SidebarLayout>
   );
-}
-
-export const SidebarHeader: React.FC<SidebarProps> = ({
-  children,
-  className,
-}) => {
-  return <div className={cn(className)}>{children}</div>;
 };
 
-export const SidebarBody: React.FC<SidebarProps> = ({
-  children,
-  className,
-}) => {
-  return <div className={cn(className)}>{children}</div>;
-};
-
-export const SidebarFooter: React.FC<SidebarProps> = ({
-  children,
-  className,
-}) => {
-  return <div className={cn(className)}>{children}</div>;
-};
+export default Sidebar;
