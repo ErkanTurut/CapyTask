2077a722d94630bd05e5e223ce7ae7bf68d5e456
Erkan Turut <erkamturut@gmail.com>
2024-01-20 00:54:10 +0100

    Add loading component to dashboard app

--- client/app/app/(dashboard)/[url_key]/(app)/layout.tsx
+++ client/app/app/(dashboard)/[url_key]/(app)/layout.tsx
@@ -1,31 +1,14 @@
 import { cookies } from "next/headers";
 
-import { getTeams } from "@/lib/service/team/fetch";
 import { TooltipProvider } from "@/components/ui/tooltip";
 
-import Resizable from "./_components/rezisable";
 import ResizableGroup from "./_components/resizableGroup";
-import {
-  Sidebar,
-  SidebarBody,
-  SidebarFooter,
-  SidebarHeader,
-} from "./_components/sidebar";
-import UserAccountNav from "@/components/user/user-account-nav";
-import { getSession } from "@/lib/service/auth/fetch";
-import { getUser } from "@/lib/service/user/fetch";
-import { Suspense } from "react";
-import { SidebarProvider, useTeam } from "@/lib/store";
-import WorkspaceNav from "@/components/workspace/workspace-navigation";
-import { TeamList } from "@/components/team/team-list";
-import { Nav } from "@/components/layouts/side-navigation/nav";
-import { appNavItems } from "@/config/dashboard.config";
-import { Separator } from "@/components/ui/separator";
-import { redirect } from "next/navigation";
-import { getWorkspaces } from "@/lib/service/workspace/fetch";
-import { Shell } from "@/components/shells";
-import { createClient } from "@/lib/supabase/server";
+import Resizable from "./_components/rezisable";
 
+import { SidebarProvider } from "@/lib/store";
+
+import { Shell } from "@/components/shells";
+import Sidebar from "./_components/sidebar";
 interface DashboardLayoutProps {
   children: React.ReactNode;
   modal: React.ReactNode;
@@ -39,8 +22,6 @@ export default async function DashboardLayout({
   params,
   modal,
 }: DashboardLayoutProps) {
-  const cookieStore = cookies();
-  const supabase = createClient(cookieStore);
   const layout = cookies().get("react-resizable-panels:layout");
   const collapsed = cookies().get("react-resizable-panels:collapsed");
   const defaultLayout = layout
@@ -50,94 +31,13 @@ export default async function DashboardLayout({
     ? (JSON.parse(collapsed.value) as boolean)
     : undefined;
 
-  const { data: workspaces } = await getWorkspaces(supabase);
-
-  if (!workspaces) {
-    redirect("/create");
-  }
-  const workspace = workspaces.find(
-    (workspace) => workspace.url_key === params.url_key,
-  );
-
-  if (!workspace) {
-    redirect("/create");
-  }
-  const { data: teams } = await getTeams(workspace.id, supabase);
-
-  const {
-    data: { session },
-    error,
-  } = await getSession(supabase);
-  if (!session || error) {
-    redirect("/login");
-  }
-  const { data: user } = await getUser(session.user.id, supabase);
-  if (!user) {
-    redirect("/login");
-  }
-
   return (
     <SidebarProvider isCollapsed={defaultCollapsed || false}>
       <div className="min-h-screen">
         {modal}
         <TooltipProvider delayDuration={0}>
           <ResizableGroup>
-            <Sidebar
-              defaultLayout={defaultLayout}
-              defaultCollapsed={defaultCollapsed}
-            >
-              <div>
-                <SidebarHeader className="grid gap-1 p-2">
-                  <Suspense fallback={<div>Loading...</div>}>
-                    <WorkspaceNav />
-                  </Suspense>
-                  <Nav
-                    rootPath={`/${params.url_key}`}
-                    items={appNavItems.header}
-                  />
-                  <Separator />
-                </SidebarHeader>
-                <SidebarBody className="px-2">
-                  <Suspense fallback={<div>Loading...</div>}>
-                    <TeamList
-                      rootPath={`/${params.url_key}/team`}
-                      items={[
-                        {
-                          title: "Members",
-                          icon: "user",
-                          variant: "ghost",
-                          href: "/members",
-                        },
-                        {
-                          title: "Projects",
-                          icon: "lightning",
-                          variant: "ghost",
-                          href: "/projects",
-                        },
-                        {
-                          title: "Repports",
-                          icon: "fileText",
-                          variant: "ghost",
-                          href: "/repports",
-                        },
-                      ]}
-                      teams={teams}
-                    />
-                  </Suspense>
-                </SidebarBody>
-              </div>
-              <SidebarFooter className="flex flex-col gap-1 p-2">
-                <Separator />
-                <Nav
-                  rootPath={`/${params.url_key}`}
-                  items={appNavItems.footer}
-                />
-                <Separator />
-                <Suspense fallback={<div>Loading...</div>}>
-                  <UserAccountNav className="w-full" user={user} />
-                </Suspense>
-              </SidebarFooter>
-            </Sidebar>
+            <Sidebar params={params} />
             <Resizable defaultLayout={defaultLayout}>
               <Shell>{children}</Shell>
             </Resizable>
--- client/app/app/(dashboard)/[url_key]/layout.tsx
+++ client/app/app/(dashboard)/[url_key]/layout.tsx
@@ -2,8 +2,6 @@ import { redirect } from "next/navigation";
 import { getSession } from "@/lib/service/auth/fetch";
 import { WorkspaceProvider, UserProvider, TeamProvider } from "@/lib/store";
 import { getUser } from "@/lib/service/user/fetch";
-import { getWorkspaces } from "@/lib/service/workspace/fetch";
-import { getTeams } from "@/lib/service/team/fetch";
 import { createClient } from "@/lib/supabase/server";
 import { cookies } from "next/headers";
 
@@ -26,26 +24,6 @@ export default async function DashboardLayout({
   if (!session?.user || error) {
     redirect("/login");
   }
-  const { data: user } = await getUser(session.user.id, supabase);
-  if (!user) {
-    redirect("/login");
-  }
-  const { data: workspaces } = await getWorkspaces(supabase);
-  if (!workspaces) {
-    redirect("/create");
-  }
-  const workspace = workspaces.find((w) => w.url_key === params.url_key);
-  if (!workspace) {
-    redirect("/create");
-  }
 
-  const { data: teams } = await getTeams(workspace.id, supabase);
-
-  return (
-    <UserProvider user={user}>
-      <WorkspaceProvider workspace={workspace} workspaceList={workspaces}>
-        <TeamProvider teamList={teams}>{children}</TeamProvider>
-      </WorkspaceProvider>
-    </UserProvider>
-  );
+  return <>{children}</>;
 }
