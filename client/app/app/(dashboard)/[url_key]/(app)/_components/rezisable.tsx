c9c84f3af3bdca29a9592293b47cc091f01c98f6
Erkan Turut <erkamturut@gmail.com>
2024-01-20 00:54:46 +0100

    Refactor resizable component imports

--- client/app/app/(dashboard)/[url_key]/(app)/_components/rezisable.tsx
+++ client/app/app/(dashboard)/[url_key]/(app)/_components/rezisable.tsx
@@ -1,18 +1,7 @@
 "use client";
 
-import {
-  ResizableHandle,
-  ResizablePanel,
-  ResizablePanelGroup,
-} from "@/components/ui/resizable";
-import { cn } from "@/lib/utils";
-import { useState, FC, useEffect } from "react";
-import { Shell } from "@/components/shells";
-import { useParams } from "next/navigation";
-import { useTeam, useUser, useWorkspace } from "@/lib/store";
-import { Sidebar } from "./sidebar";
-import { Database } from "@/types/supabase.types";
-import { TooltipProvider } from "@/components/ui/tooltip";
+import { ResizablePanel } from "@/components/ui/resizable";
+import { FC } from "react";
 
 interface AppProps {
   children: React.ReactNode;
