5a3e2b34b3770990bdece002247943d1c83025ae
Erkan Turut <erkamturut@gmail.com>
2024-01-21 00:05:35 +0100

    Refactor data table toolbar

--- client/components/team/table/data-table-toolbar.tsx
+++ client/components/team/table/data-table-toolbar.tsx
@@ -17,16 +17,16 @@ export function DataTableToolbar<TData>({
     <div className="flex items-center justify-between">
       <div className="flex flex-1 items-center space-x-2">
         <Input
-          //   placeholder="Filter tasks..."
-          //   value={(table.getColumn("title")?.getFilterValue() as string) ?? ""}
-          //   onChange={(event) =>
-          //     table.getColumn("title")?.setFilterValue(event.target.value)
-          //   }
+          placeholder="Filter tasks..."
+          // value={(table.getColumn("title")?.getFilterValue() as string) ?? ""}
+          // onChange={(event) =>
+          //   table.getColumn("title")?.setFilterValue(event.target.value)
+          // }
           className="h-8 w-[150px] lg:w-[250px]"
         />
       </div>
       <Button variant="default" className="gap-1">
-        create project
+        Create case
         <Icons.plus className="h-4 w-4" />
       </Button>
     </div>
