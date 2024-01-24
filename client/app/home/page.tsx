edbbfce280f094df55a43776b390f8c581318da5
Erkan Turut <erkamturut@gmail.com>
2024-01-24 23:40:26 +0100

    Add lorem ipsum content to Home page

--- client/app/home/page.tsx
+++ client/app/home/page.tsx
@@ -4,6 +4,15 @@ export default async function Home() {
   return (
     <Shell as="div" className="gap-12">
       <section>Home </section>
+      {Array.from({ length: 30 }).map((_, index) => (
+        <p key={index}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+          voluptate, voluptatum, quidem, quas accusantium voluptates doloremque
+          quae fugiat voluptatibus doloribus magnam. Quisquam voluptate,
+          voluptatum, quidem, quas accusantium voluptates doloremque quae fugiat
+          voluptatibus doloribus magnam.
+        </p>
+      ))}
     </Shell>
   );
 }
