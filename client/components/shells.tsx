12821f0de947467fd0e24ea3150c7270b220c0e8
Erkan Turut <erkamturut@gmail.com>
2024-02-05 23:34:27 +0100

    Remove unnecessary line in shellVariants

--- client/components/shells.tsx
+++ client/components/shells.tsx
@@ -6,7 +6,6 @@ const shellVariants = cva("grid items-start gap-8 pb-8 pt-6 md:py-2", {
   variants: {
     variant: {
       default: "container",
-
       centered: "container flex h-[100dvh] max-w-2xl flex-col justify-center",
       markdown: "container max-w-3xl gap-0 py-8 md:py-10 lg:py-10",
       markdown_centered:
