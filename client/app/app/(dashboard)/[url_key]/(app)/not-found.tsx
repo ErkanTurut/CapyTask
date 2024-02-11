76de8a7f11129b2ac2c4bcebb75946f9c15e22eb
Erkan Turut <erkamturut@gmail.com>
2024-02-11 22:29:41 +0100

    Refactor not-found page component

--- client/app/app/(dashboard)/[url_key]/(app)/not-found.tsx
+++ client/app/app/(dashboard)/[url_key]/(app)/not-found.tsx
@@ -1,11 +1,31 @@
-import Link from "next/link";
-
+"use client";
+import { Shell } from "@/components/shells";
+import {
+  PageHeader,
+  PageHeaderDescription,
+  PageHeaderHeading,
+} from "@/components/page-header";
+import { Button } from "@/components/ui/button";
+import { Icons } from "@/components/icons";
+import { useRouter } from "next/navigation";
 export default function NotFound() {
+  const router = useRouter();
+
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
-    </div>
+    <Shell variant="centered">
+      <PageHeader>
+        <PageHeaderHeading className="text-center" size={"lg"}>
+          Are you lost buddy?
+        </PageHeaderHeading>
+        <PageHeaderDescription className="mx-auto text-center" size={"lg"}>
+          The page you are looking for does not exist. Please check the URL or
+          click the button below to go back to the home page.
+        </PageHeaderDescription>
+      </PageHeader>
+      <Button onClick={() => router.back()}>
+        Go back
+        {/* <Icons.arrowRight className="ml-2 h-4 w-4" /> */}
+      </Button>
+    </Shell>
   );
 }
