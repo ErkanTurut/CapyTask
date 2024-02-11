b4f937ed9a0482527a764157d590a8ffad68b929
Erkan Turut <erkamturut@gmail.com>
2024-02-11 22:28:30 +0100

    Add client import and update StepList component

--- client/components/step/step-list.tsx
+++ client/components/step/step-list.tsx
@@ -1,37 +1,50 @@
+"use client";
 import { Database } from "@/types/supabase.types";
-import { FC } from "react";
-
-import { Icons } from "../icons";
+import { buttonVariants } from "@/ui/button";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { FC } from "react";
+import { Icons } from "../icons";
+
+import { cn } from "@/lib/utils";
 
 interface stepListProps {
   steps: Database["public"]["Tables"]["step"]["Row"][] | null;
   searchParams?: { [key: string]: string | string[] | undefined };
+  children?: React.ReactNode;
 }
 
-const StepList: FC<stepListProps> = ({ steps, searchParams }) => {
-  // steps can have an order value, and if so we want to sort the steps by that order
+const StepList: FC<stepListProps> = ({ steps, searchParams, children }) => {
   const sortedSteps = steps?.sort((a, b) => (a.order || 0) - (b.order || 0));
+  const pathname = usePathname().replace("/create", "");
+
   return (
-    <ul className="grid gap-1">
+    <ul className="grid w-full gap-1">
       {sortedSteps?.map((step) => (
         <li
           key={step.id}
-          autoFocus={searchParams?.step_id === step.id}
-          className="flex flex-row items-center  justify-between rounded-md border border-border  p-2"
+          className={cn(
+            buttonVariants({ variant: "secondary", size: "default" }),
+            "group w-full grow justify-between overflow-hidden  px-2",
+          )}
+          draggable
         >
-          <span className="grid gap-1">
-            <h3>{step.name}</h3>
-            <p className="text-muted-foreground">{step.description}</p>
-          </span>
           <Link
-            scroll={false}
+            className="flex w-full items-center justify-between px-2"
             href={{
-              pathname: "",
-              query: { step_id: step.id },
+              href: pathname,
+              query: {
+                ...searchParams,
+                step_id: step.id,
+              },
             }}
           >
-            <Icons.chevronRight className="h-6 w-6" />
+            {step.name}
+
+            <span className="sr-only">Get Started</span>
+            <div className="ml-1 transition group-hover:translate-x-1">
+              <Icons.arrowRight className=" h-4 w-4" />
+            </div>
           </Link>
         </li>
       ))}
