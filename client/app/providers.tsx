afcab6f805ed6a7d8041038dbc29198cebce76d1
Erkan Turut <erkamturut@gmail.com>
2024-02-11 22:29:51 +0100

    Add useWindowSize hook to Providers component

--- client/app/providers.tsx
+++ client/app/providers.tsx
@@ -7,18 +7,23 @@ import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { type ThemeProviderProps } from "next-themes/dist/types";
 import { Toaster } from "@/components/ui/sonner";
 import { TailwindIndicator } from "@/components/tailwind-indicator";
+import useWindowSize from "@/lib/hooks/use-window-size";
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
 
 export function Providers({ children }: { children: React.ReactNode }) {
+  const { isMobile } = useWindowSize();
   return (
     <NextThemesProvider attribute="class" defaultTheme="system" enableSystem>
       <ModalProvider>
         <TailwindIndicator />
         {children}
-        <Toaster closeButton />
+        <Toaster
+          closeButton
+          position={isMobile ? "top-center" : "bottom-right"}
+        />
       </ModalProvider>
     </NextThemesProvider>
   );
