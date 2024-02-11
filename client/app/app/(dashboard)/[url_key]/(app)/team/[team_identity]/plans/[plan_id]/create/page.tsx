91c5c3ea95bc61b225505cdc29570100cc802ac3
Erkan Turut <erkamturut@gmail.com>
2024-02-11 22:29:23 +0100

    Add responsive card component and create page

---
+++ client/app/app/(dashboard)/[url_key]/(app)/team/[team_identity]/plans/[plan_id]/create/page.tsx
@@ -0,0 +1,35 @@
+import StepCreate from "@/components/step/step-create";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { ResponsiveCard } from "../_components/responsive-card";
+
+interface CreatePageProps {
+  params: {
+    team_identity: string;
+    plan_id: string;
+  };
+  searchParams: { [key: string]: string | string[] | undefined };
+}
+
+export default async function CreatePage({
+  params,
+  searchParams,
+}: CreatePageProps) {
+  return (
+    <ResponsiveCard>
+      <CardHeader>
+        <CardTitle>Create step</CardTitle>
+        <CardDescription>Create step</CardDescription>
+      </CardHeader>
+
+      <CardContent>
+        <StepCreate plan_id={params.plan_id} />
+      </CardContent>
+    </ResponsiveCard>
+  );
+}
