bb83291a22ae5b5ee153d99530fe6252bae91e07
Erkan Turut <erkamturut@gmail.com>
2024-02-15 22:27:05 +0100

    Update redirect URL in auth callback route

--- client/app/api/auth/callback/route.ts
+++ client/app/api/auth/callback/route.ts
@@ -30,9 +30,8 @@ export async function GET(request: Request) {
 
     const { error, data } = await supabase.auth.exchangeCodeForSession(code);
     if (!error) {
-      return NextResponse.redirect(`${origin}${next}`);
+      return NextResponse.redirect(`http://app.localhost:3000${next}`);
     }
-    return NextResponse.json(error);
   }
 
   // return the user to an error page with instructions
