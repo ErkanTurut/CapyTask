7273ebd0ceadd7126e6dc9e0f2c28f0c99cdf275
Erkan Turut <erkamturut@gmail.com>
2024-02-18 01:16:14 +0100

    Refactor useDebounce hook to handle empty string values

--- client/lib/hooks/use-debounce.ts
+++ client/lib/hooks/use-debounce.ts
@@ -1,15 +1,37 @@
 import { useEffect, useState } from "react";
 
-export function useDebounce<T>(value: T, delay?: number): T {
-  const [debouncedValue, setDebouncedValue] = useState<T>(value);
+/**
+ * useDebounce hook
+ * This hook allows you to debounce any fast changing value. The debounced value will only
+ * reflect the latest value when the useDebounce hook has not been called for the specified delay period.
+ *
+ * @param value - The value to be debounced.
+ * @param delay - The delay in milliseconds for the debounce.
+ * @returns The debounced value.
+ */
+function useDebounce<T>(value: T, delay: number = 500): { debouncedValue: T } {
+  // State and setters for debounced value
+  const [debouncedValue, setDebouncedValue] = useState(value);
 
   useEffect(() => {
-    const timer = setTimeout(() => setDebouncedValue(value), delay || 500);
+    if (typeof value === "string" && value.trim().length === 0) {
+      setDebouncedValue(value);
+      return;
+    }
 
+    // Update debounced value after the specified delay
+    const handler = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    // Cancel the timeout if value changes (also on delay change or unmount)
+    // This is how we prevent debounced value from updating if value is changed within the delay period
     return () => {
-      clearTimeout(timer);
+      clearTimeout(handler);
     };
-  }, [value, delay]);
+  }, [value, delay]); // Only re-call effect if value or delay changes
 
-  return debouncedValue;
+  return { debouncedValue };
 }
+
+export default useDebounce;
