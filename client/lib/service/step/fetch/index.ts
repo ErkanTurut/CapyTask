2d1ef61fd78c2a9d094d5cd89861eb8e71c47d63
Erkan Turut <erkamturut@gmail.com>
2024-02-26 00:05:43 +0100

    Update imports in fetch/index.ts

--- client/lib/service/step/fetch/index.ts
+++ client/lib/service/step/fetch/index.ts
@@ -5,7 +5,7 @@ import {
   unstable_noStore as noStore,
 } from "next/cache";
 import { SupabaseClient } from "@/lib/supabase/server";
-import { sleep } from "@/lib/utils";
+import { QueryResult, QueryData, QueryError } from "@supabase/supabase-js";
 
 export async function getSteps({
   plan_id,
@@ -19,6 +19,36 @@ export async function getSteps({
     .select("*")
     .order("order")
     .eq("plan_id", plan_id);
+}
+
+export async function getStepsByPlan({
+  plan_id,
+  client,
+}: {
+  plan_id: string;
+  client: SupabaseClient;
+}) {
+  return await client
+    .from("plan")
+    .select("*, step!inner(*)")
+    .eq("id", plan_id)
+    .single();
+}
+
+export async function getStepsByIdentity({
+  team_identity,
+  db,
+  range,
+}: {
+  team_identity: string;
+  db: SupabaseClient;
+  range: { start: number; end: number };
+}) {
+  return await db
+    .from("step")
+    .select("*, team!inner(*)")
+    .eq("team.identity", team_identity)
+    .range(range.start, range.end);
 }
 
 export async function getStep({
