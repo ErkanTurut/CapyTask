2177635777f4a26141950c429e429e0967f43a0c
Erkan Turut <erkamturut@gmail.com>
2024-02-26 00:03:57 +0100

    Update user authentication logic

--- client/components/layouts/side-navigation/nav.tsx
+++ client/components/layouts/side-navigation/nav.tsx
@@ -20,6 +20,7 @@ import {
 import { Separator } from "@/components/ui/separator";
 import { useSidebar } from "@/lib/store";
 import { usePathname } from "next/navigation";
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
 
 interface NavProps extends React.HTMLAttributes<HTMLDivElement> {
   items: NavItem[];
@@ -31,17 +32,17 @@ interface NavProps extends React.HTMLAttributes<HTMLDivElement> {
 export function Nav({ items, size, className, rootPath }: NavProps) {
   const isCollapsed = useSidebar()((state) => state.isCollapsed);
   const pathname = usePathname();
-
   return (
-    <div
+    <nav
       data-collapsed={isCollapsed}
-      className="group flex w-full flex-col gap-4"
+      className={cn("group  flex w-full flex-col gap-1", className)}
     >
-      <nav className={cn("grid w-full gap-1", className)}>
-        {items.map((item, index) => {
-          const Icon = item.icon ? Icons[item.icon] : null;
-          const href = item.href ? `${rootPath}${item.href}` : "#";
-          return isCollapsed ? (
+      {items.map((item, index) => {
+        const Icon = item.icon ? Icons[item.icon] : null;
+        const href = item.href ? `${rootPath}${item.href}` : rootPath;
+
+        if (isCollapsed) {
+          return (
             <Tooltip key={index} delayDuration={0}>
               <TooltipTrigger asChild>
                 <Link
@@ -54,7 +55,7 @@ export function Nav({ items, size, className, rootPath }: NavProps) {
                     "h-8 w-8",
                   )}
                 >
-                  {Icon && <Icon className="h-4 w-4" />}
+                  {Icon && <Icon className="h-4 w-4 shrink-0" />}
                   <span className="sr-only">{item.title}</span>
                 </Link>
               </TooltipTrigger>
@@ -67,9 +68,11 @@ export function Nav({ items, size, className, rootPath }: NavProps) {
                 )}
               </TooltipContent>
             </Tooltip>
-          ) : item.items && item.items.length > 0 ? (
-            <Accordion key={index} type="multiple">
-              <AccordionItem value={item.title}>
+          );
+        } else if (item.items && item.items.length > 0) {
+          return (
+            <Accordion key={index} type="single" collapsible defaultValue="all">
+              <AccordionItem value={item.id}>
                 <AccordionTrigger
                   className={cn(
                     buttonVariants({
@@ -79,31 +82,46 @@ export function Nav({ items, size, className, rootPath }: NavProps) {
                     "justify-between",
                   )}
                 >
-                  {item.title}
-                  {item.label && (
-                    <span
-                      className={cn(
-                        "ml-auto",
-                        item.variant === "default" &&
-                          "text-background dark:text-white",
-                      )}
-                    >
-                      {item.label}
-                    </span>
-                  )}
+                  <span className="flex justify-start">
+                    {item.image_url && (
+                      <Avatar className="mr-2 h-5 w-5 rounded-md">
+                        <AvatarImage
+                          src={item.image_url}
+                          alt={item.title ?? ""}
+                        />
+                      </Avatar>
+                    )}
+                    {Icon && (
+                      <Icon
+                        className="mr-2 h-4 w-4 shrink-0"
+                        aria-hidden="true"
+                      />
+                    )}
+                    {item.title}
+                    {item.label && (
+                      <span
+                        className={cn(
+                          "ml-auto",
+                          item.variant === "default" &&
+                            "text-background dark:text-white",
+                        )}
+                      >
+                        {item.label}
+                      </span>
+                    )}
+                  </span>
                 </AccordionTrigger>
-                <AccordionContent>
-                  <span className="ml-1 flex gap-1">
-                    <Separator
-                      orientation="vertical"
-                      className="h-auto bg-primary "
-                    />
+                <AccordionContent className="py-1">
+                  <span className="flex w-full gap-1 pl-2 ">
+                    <Separator orientation="vertical" className="h-auto " />
                     <Nav items={item.items} size={size} rootPath={href} />
                   </span>
                 </AccordionContent>
               </AccordionItem>
             </Accordion>
-          ) : (
+          );
+        } else {
+          return (
             <Link
               key={index}
               href={href}
@@ -113,18 +131,126 @@ export function Nav({ items, size, className, rootPath }: NavProps) {
                     pathname === href ? "secondary" : item.variant || "ghost",
                   size: size || "sm",
                 }),
-                "justify-start",
+                "justify-start ",
               )}
             >
-              {Icon && <Icon className="mr-2 h-4 w-4" aria-hidden="true" />}
-              {item.title}
+              {Icon && (
+                <Icon className="mr-2 h-4 w-4 shrink-0" aria-hidden="true" />
+              )}
+              <span className="overflow-x-auto overflow-ellipsis whitespace-nowrap	">
+                {item.title}
+              </span>
+
               {item.label && (
                 <span className={cn("ml-auto")}>{item.label}</span>
               )}
             </Link>
           );
-        })}
-      </nav>
-    </div>
+        }
+      })}
+    </nav>
   );
 }
+
+// {items.map((item, index) => {
+//   const Icon = item.icon ? Icons[item.icon] : null;
+//   const href = item.href ? `${rootPath}${item.href}` : rootPath;
+//   return isCollapsed ? (
+//     <Tooltip key={index} delayDuration={0}>
+//       <TooltipTrigger asChild>
+//         <Link
+//           href={href}
+//           className={cn(
+//             buttonVariants({
+//               variant: item.variant || "ghost",
+//               size: "icon",
+//             }),
+//             "h-8 w-8",
+//           )}
+//         >
+//           {Icon && <Icon className="h-4 w-4 shrink-0" />}
+//           <span className="sr-only">{item.title}</span>
+//         </Link>
+//       </TooltipTrigger>
+//       <TooltipContent side="right" className="flex items-center gap-4">
+//         {item.title}
+//         {item.label && (
+//           <span className="ml-auto text-muted-foreground">
+//             {item.label}
+//           </span>
+//         )}
+//       </TooltipContent>
+//     </Tooltip>
+//   ) : item.items && item.items.length > 0 ? (
+//     <Accordion key={index} type="single" collapsible defaultValue="all">
+//       <AccordionItem value={item.id}>
+//         <AccordionTrigger
+//           className={cn(
+//             buttonVariants({
+//               variant: item.variant || "ghost",
+//               size: size || "sm",
+//             }),
+//             "justify-between",
+//           )}
+//         >
+//           <span className="flex justify-start">
+//             {item.image_url && (
+//               <Avatar className="mr-2 h-5 w-5 rounded-md">
+//                 <AvatarImage
+//                   src={item.image_url}
+//                   alt={item.title ?? ""}
+//                 />
+//               </Avatar>
+//             )}
+//             {Icon && (
+//               <Icon
+//                 className="mr-2 h-4 w-4 shrink-0"
+//                 aria-hidden="true"
+//               />
+//             )}
+//             {item.title}
+//             {item.label && (
+//               <span
+//                 className={cn(
+//                   "ml-auto",
+//                   item.variant === "default" &&
+//                     "text-background dark:text-white",
+//                 )}
+//               >
+//                 {item.label}
+//               </span>
+//             )}
+//           </span>
+//         </AccordionTrigger>
+//         <AccordionContent className="py-1">
+//           <span className="flex w-full gap-1 pl-2 ">
+//             <Separator orientation="vertical" className="h-auto " />
+//             <Nav items={item.items} size={size} rootPath={href} />
+//           </span>
+//         </AccordionContent>
+//       </AccordionItem>
+//     </Accordion>
+//   ) : (
+// <Link
+//   key={index}
+//   href={href}
+//   className={cn(
+//     buttonVariants({
+//       variant:
+//         pathname === href ? "secondary" : item.variant || "ghost",
+//       size: size || "sm",
+//     }),
+//     "justify-start ",
+//   )}
+// >
+//   {Icon && (
+//     <Icon className="mr-2 h-4 w-4 shrink-0" aria-hidden="true" />
+//   )}
+//   <span className="overflow-x-auto overflow-ellipsis whitespace-nowrap	">
+//     {item.title}
+//   </span>
+
+//   {item.label && <span className={cn("ml-auto")}>{item.label}</span>}
+// </Link>
+//   );
+// })}
