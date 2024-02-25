bf9ef98ac622be3abddc94f77d9d3af6a39c1e1d
Erkan Turut <erkamturut@gmail.com>
2024-02-26 00:04:43 +0100

    Fix bug in login functionality

--- client/components/team/team-list.tsx
+++ client/components/team/team-list.tsx
@@ -35,189 +35,184 @@ export function TeamList({ items, teams, rootPath }: TeamListProps) {
   const { team_identity }: { url_key: string; team_identity: string } =
     useParams();
   return (
-    <div data-collapsed={isCollapsed} className=" group flex flex-col gap-4 ">
-      <nav className="gap-1">
-        <div className="flex flex-col gap-1">
-          {isCollapsed ? (
-            <>
-              <Tooltip delayDuration={0}>
-                <TooltipTrigger asChild>
-                  <Link
-                    href={`${rootPath}/create`}
-                    className={cn(
-                      buttonVariants({
-                        variant: "outline",
-                        size: "icon",
-                      }),
-                      "h-8 w-8",
-                    )}
-                  >
-                    <Icons.plusCircled className="h-5 w-5" />
-                  </Link>
-                </TooltipTrigger>
-                <TooltipContent
-                  side="right"
-                  className="flex items-center gap-4"
-                >
-                  Create Team
-                  <span className="ml-auto text-muted-foreground">
-                    Start a new team
-                  </span>
-                </TooltipContent>
-              </Tooltip>
-              {teams &&
-                teams.length > 0 &&
-                teams.map((team, index) => {
-                  const { image_uri, initials } = generateAvatar({
-                    name: team.name,
-                  });
-                  return (
-                    <Tooltip key={index} delayDuration={0}>
-                      <TooltipTrigger key={index} asChild>
-                        <Link
-                          href={`${rootPath}/${team.identity}`}
-                          className={cn(
-                            buttonVariants({
-                              variant:
-                                team_identity === team.identity
-                                  ? "secondary"
-                                  : "ghost",
-
-                              size: "icon",
-                            }),
-                            "h-8 w-8",
-                          )}
-                          key={index}
-                        >
-                          <Avatar className={cn("h-5 w-5 rounded-sm")}>
-                            <AvatarImage
-                              src={team.image_uri || image_uri}
-                              alt={team.name ?? ""}
-                            />
-                            <AvatarFallback>{initials}</AvatarFallback>
-                          </Avatar>
-                        </Link>
-                      </TooltipTrigger>
-                      <TooltipContent
-                        side="right"
-                        key={team.id}
-                        className="flex items-center gap-4"
-                      >
-                        {team.name}
-                        {team.name && (
-                          <span className="ml-auto text-muted-foreground">
-                            {team.name}
-                          </span>
-                        )}
-                      </TooltipContent>
-                    </Tooltip>
-                  );
-                })}
-            </>
-          ) : (
-            <Accordion
-              type="single"
-              className="flex w-full flex-col gap-1"
-              defaultValue="all"
-              collapsible
-            >
-              {teams && teams.length > 0 ? (
-                <AccordionItem
-                  value="all"
-                  className="flex flex-col gap-1 border-0"
-                >
-                  <AccordionTrigger
-                    className={cn(
-                      buttonVariants({ variant: "ghost", size: "sm" }),
-                      "items-center  justify-between gap-1 py-0 underline",
-                    )}
-                  >
-                    <Link
-                      href={`${rootPath}/create`}
-                      className={cn(
-                        buttonVariants({
-                          variant: "ghost",
-                          size: "icon",
-                        }),
-                        " h-7 w-7",
-                      )}
-                    >
-                      <Icons.plusCircled className="h-4 w-4" />
-                    </Link>
-                    <span>Your Teams</span>
-                  </AccordionTrigger>
-
-                  <AccordionContent>
-                    <Accordion
-                      type="single"
-                      collapsible
-                      defaultValue={team_identity}
-                    >
-                      {teams.map((team, index) => {
-                        const { image_uri, initials } = generateAvatar({
-                          name: team.name,
-                        });
-                        return (
-                          <AccordionItem key={team.id} value={team.identity}>
-                            <AccordionTrigger
-                              className={cn(
-                                buttonVariants({
-                                  variant: "ghost",
-                                  size: "sm",
-                                }),
-                                "justify-between py-1",
-                              )}
-                              key={index}
-                              draggable
-                            >
-                              <Avatar className="h-5 w-5 rounded-sm">
-                                <AvatarImage
-                                  src={team.image_uri || image_uri}
-                                  alt={team.name ?? ""}
-                                />
-                                <AvatarFallback>{initials}</AvatarFallback>
-                              </Avatar>
-                              <span className="cursor-pointer overflow-x-auto overflow-ellipsis whitespace-nowrap">
-                                {team.name}
-                              </span>
-                            </AccordionTrigger>
-                            <AccordionContent key={team.id} className="pt-1">
-                              <div className="ml-3 flex gap-1">
-                                <Separator
-                                  orientation="vertical"
-                                  className="h-auto "
-                                />
-                                <Nav
-                                  rootPath={`${rootPath}/${team.identity}`}
-                                  items={items}
-                                />
-                              </div>
-                            </AccordionContent>
-                          </AccordionItem>
-                        );
-                      })}
-                    </Accordion>
-                  </AccordionContent>
-                </AccordionItem>
-              ) : (
+    <nav data-collapsed={isCollapsed} className=" group flex flex-col gap-4 ">
+      <div className="flex flex-col gap-1">
+        {isCollapsed ? (
+          <>
+            <Tooltip delayDuration={0}>
+              <TooltipTrigger asChild>
                 <Link
                   href={`${rootPath}/create`}
                   className={cn(
                     buttonVariants({
-                      variant: "ghost",
-                      size: "sm",
+                      variant: "outline",
+                      size: "icon",
                     }),
-                    "flex w-full justify-between gap-2 py-0",
+                    "h-8 w-8",
                   )}
                 >
-                  Start new team
-                  <Icons.plusCircled className="h-4 w-4" />
+                  <Icons.plusCircled className="h-5 w-5" />
                 </Link>
-              )}
-            </Accordion>
-          )}
-        </div>
-      </nav>
-    </div>
+              </TooltipTrigger>
+              <TooltipContent side="right" className="flex items-center gap-4">
+                Create Team
+                <span className="ml-auto text-muted-foreground">
+                  Start a new team
+                </span>
+              </TooltipContent>
+            </Tooltip>
+            {teams &&
+              teams.length > 0 &&
+              teams.map((team, index) => {
+                const { image_url, initials } = generateAvatar({
+                  name: team.name,
+                });
+                return (
+                  <Tooltip key={index} delayDuration={0}>
+                    <TooltipTrigger key={index} asChild>
+                      <Link
+                        href={`${rootPath}/${team.identity}`}
+                        className={cn(
+                          buttonVariants({
+                            variant:
+                              team_identity === team.identity
+                                ? "secondary"
+                                : "ghost",
+
+                            size: "icon",
+                          }),
+                          "h-8 w-8",
+                        )}
+                        key={index}
+                      >
+                        <Avatar className={cn("h-5 w-5 rounded-full")}>
+                          <AvatarImage
+                            src={team.image_uri || image_url}
+                            alt={team.name ?? ""}
+                          />
+                          <AvatarFallback>{initials}</AvatarFallback>
+                        </Avatar>
+                      </Link>
+                    </TooltipTrigger>
+                    <TooltipContent
+                      side="right"
+                      key={team.id}
+                      className="flex items-center gap-4"
+                    >
+                      {team.name}
+                      {team.name && (
+                        <span className="ml-auto text-muted-foreground">
+                          {team.name}
+                        </span>
+                      )}
+                    </TooltipContent>
+                  </Tooltip>
+                );
+              })}
+          </>
+        ) : (
+          <Accordion
+            type="single"
+            className="flex w-full flex-col gap-1"
+            defaultValue="all"
+            collapsible
+          >
+            {teams && teams.length > 0 ? (
+              <AccordionItem
+                value="all"
+                className="flex flex-col gap-1 border-0"
+              >
+                <AccordionTrigger
+                  className={cn(
+                    buttonVariants({ variant: "ghost", size: "sm" }),
+                    "items-center  justify-between gap-1 py-0 underline",
+                  )}
+                >
+                  <Link
+                    href={`${rootPath}/create`}
+                    className={cn(
+                      buttonVariants({
+                        variant: "ghost",
+                        size: "icon",
+                      }),
+                      " h-7 w-7",
+                    )}
+                  >
+                    <Icons.plusCircled className="h-4 w-4" />
+                  </Link>
+                  <span>Your Teams</span>
+                </AccordionTrigger>
+
+                <AccordionContent>
+                  <Accordion
+                    type="single"
+                    collapsible
+                    defaultValue={team_identity}
+                  >
+                    {teams.map((team, index) => {
+                      const { image_url, initials } = generateAvatar({
+                        name: team.name,
+                      });
+                      return (
+                        <AccordionItem key={team.id} value={team.identity}>
+                          <AccordionTrigger
+                            className={cn(
+                              buttonVariants({
+                                variant: "ghost",
+                                size: "sm",
+                              }),
+                              "justify-between",
+                            )}
+                            key={index}
+                            draggable
+                          >
+                            <Avatar className="h-5 w-5 rounded-md">
+                              <AvatarImage
+                                src={team.image_uri || image_url}
+                                alt={team.name ?? ""}
+                              />
+                              <AvatarFallback>{initials}</AvatarFallback>
+                            </Avatar>
+                            <span className="cursor-pointer overflow-x-auto overflow-ellipsis whitespace-nowrap">
+                              {team.name}
+                            </span>
+                          </AccordionTrigger>
+                          <AccordionContent key={team.id} className="pt-1">
+                            <div className="flex w-full gap-1 pl-2">
+                              <Separator
+                                orientation="vertical"
+                                className="h-auto "
+                              />
+                              <Nav
+                                rootPath={`${rootPath}/${team.identity}`}
+                                items={items}
+                              />
+                            </div>
+                          </AccordionContent>
+                        </AccordionItem>
+                      );
+                    })}
+                  </Accordion>
+                </AccordionContent>
+              </AccordionItem>
+            ) : (
+              <Link
+                href={`${rootPath}/create`}
+                className={cn(
+                  buttonVariants({
+                    variant: "ghost",
+                    size: "sm",
+                  }),
+                  "flex w-full justify-between gap-2 py-0",
+                )}
+              >
+                Start new team
+                <Icons.plusCircled className="h-4 w-4" />
+              </Link>
+            )}
+          </Accordion>
+        )}
+      </div>
+    </nav>
   );
 }
