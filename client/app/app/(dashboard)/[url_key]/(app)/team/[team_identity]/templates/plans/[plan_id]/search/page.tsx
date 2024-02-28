4841f6bab3e2f53b6274876dbe93efc4bb363811
Erkan Turut <erkamturut@gmail.com>
2024-02-29 00:11:13 +0100

    Add search page component

---
+++ client/app/app/(dashboard)/[url_key]/(app)/team/[team_identity]/templates/plans/[plan_id]/search/page.tsx
@@ -0,0 +1,35 @@
+import { searchSteps } from "@/lib/service/step/fetch";
+import { SearchStep } from "../_components/search-step";
+import { cookies } from "next/headers";
+import { createClient } from "@/lib/supabase/server";
+import { headers as dynamic } from "next/headers";
+
+interface pageProps {
+  searchParams: {
+    q: string;
+  };
+  params: {
+    url_key: string;
+    team_identity: string;
+    plan_id: string;
+  };
+}
+
+export default async function Page({ searchParams, params }: pageProps) {
+  dynamic();
+
+  const cookieStore = cookies();
+  const supabase = createClient(cookieStore);
+  const { data: steps } = await searchSteps({
+    client: supabase,
+    q: searchParams.q,
+  });
+
+  return (
+    <SearchStep
+      plan_id={params.plan_id}
+      steps={steps || []}
+      searchParams={searchParams}
+    />
+  );
+}
