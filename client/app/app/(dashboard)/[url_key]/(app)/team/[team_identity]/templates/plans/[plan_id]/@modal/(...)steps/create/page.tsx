863a54c9d441ca59e839a7e754b630f78bdc4176
Erkan Turut <erkamturut@gmail.com>
2024-03-03 22:31:35 +0100

    Delete unused modal and data table components

--- client/app/app/(dashboard)/[url_key]/(app)/team/[team_identity]/templates/plans/[plan_id]/@modal/(...)steps/create/page.tsx
+++
@@ -1,9 +0,0 @@
-import { FC } from "react";
-import { Modal } from "../../_components/modal";
-interface pageProps {}
-
-const page: FC<pageProps> = ({}) => {
-  return <Modal>page</Modal>;
-};
-
-export default page;
