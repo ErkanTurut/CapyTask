2278c3b616195147c33e10470d03a290c8a1e2b0
Erkan Turut <erkamturut@gmail.com>
2024-03-18 00:12:01 +0100

    Update routes in _app.ts

--- client/trpc/routes/_app.ts
+++ client/trpc/routes/_app.ts
@@ -3,20 +3,22 @@ import { user } from "./user/_router";
 import { team } from "./team/_router";
 import { workspace } from "./workspace/_router";
 import { step } from "./step/_router";
-import { plan } from "./plan/_router";
+import { inspection } from "./inspection/_router";
 import { work_order } from "./work_order/_router";
-import { publicProcedure, router } from "@/trpc/trpc";
+import { router } from "@/trpc/trpc";
+import { template } from "./template/_router";
 
 export const appRouter = router({
   auth: authMutationRouter,
-  db: {
+  db: router({
     user,
     team,
     workspace,
     step,
-    plan,
+    inspection,
     work_order,
-  },
+    template,
+  }),
 });
 
 export type AppRouter = typeof appRouter;
