0a9f26738e0c222385961eb31e6df845877ca95d
Erkan Turut <erkamturut@gmail.com>
2024-03-24 00:53:04 +0100

    Add get and create routes for inspections

--- client/trpc/routes/inspection/_router.ts
+++ client/trpc/routes/inspection/_router.ts
@@ -1,7 +1,29 @@
 import { protectedProcedure, router } from "@/trpc/trpc";
+import { getInspectionsByIdentityHandler } from "./get.handler";
+import { ZGetInspcetionSchema } from "./get.schema";
+import { ZCreateInspcetionSchema } from "./create.schema";
+import { createInspectionHandler } from "./create.handler";
 
 export const inspection = router({
   test: protectedProcedure.query(async ({ ctx }) => {
     return { data: "Hello" };
   }),
+  get: router({
+    byTeamIdentity: protectedProcedure
+      .input(ZGetInspcetionSchema.pick({ team_identity: true, range: true }))
+      .query(async ({ ctx, input }) => {
+        return await getInspectionsByIdentityHandler({
+          input,
+          db: ctx.db,
+        });
+      }),
+  }),
+  create: protectedProcedure
+    .input(ZCreateInspcetionSchema)
+    .mutation(async ({ ctx, input }) => {
+      return await createInspectionHandler({
+        input,
+        db: ctx.db,
+      });
+    }),
 });
