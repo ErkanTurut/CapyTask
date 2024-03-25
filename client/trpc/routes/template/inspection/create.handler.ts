b4c54a3db3a2c9c7087bcc2717ab8b91f9ba3798
Erkan Turut <erkamturut@gmail.com>
2024-03-25 23:28:00 +0100

    Add error handling and snapshot creation in createStepHandler

--- client/trpc/routes/template/inspection/create.handler.ts
+++ client/trpc/routes/template/inspection/create.handler.ts
@@ -2,6 +2,7 @@ import "server-only";
 
 import { SupabaseClient } from "@/lib/supabase/server";
 import { TCreateInspectionSchema } from "./create.schema";
+import { TRPCError } from "@trpc/server";
 // import { cache } from "react";
 
 type opts = {
@@ -10,5 +11,30 @@ type opts = {
 };
 
 export const createStepHandler = async ({ input, db }: opts) => {
-  return await db.from("inspection_template").insert(input).select("*");
+  const { data, error } = await db
+    .from("inspection_template")
+    .insert(input)
+    .select("*")
+    .single()
+    .throwOnError();
+
+  if (!data || error) {
+    throw new TRPCError({
+      code: "INTERNAL_SERVER_ERROR",
+      message: "Failed to create inspection template",
+      cause: {
+        error,
+      },
+    });
+  }
+  await db
+    .from("inspection_template_snapshot")
+    .insert({
+      name: data.name,
+      description: data.description,
+      inspection_template_id: data.id,
+    })
+    .throwOnError();
+
+  return data;
 };
