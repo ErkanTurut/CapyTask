ccef3eeb38bca893b54e43e1744a034e119dd52c
Erkan Turut <erkamturut@gmail.com>
2024-04-01 01:27:41 +0200

    Fix event listener options and add missing commas

--- client/lib/hooks/use-on-click-outside.ts
+++ client/lib/hooks/use-on-click-outside.ts
@@ -6,7 +6,7 @@ type Handler = (event: MouseEvent) => void;
 export function useOnClickOutside<T extends HTMLElement = HTMLElement>(
   ref: RefObject<T>,
   handler: Handler,
-  mouseEvent: "mousedown" | "mouseup" = "mousedown"
+  mouseEvent: "mousedown" | "mouseup" = "mousedown",
 ): void {
   useEventListener(mouseEvent, (event) => {
     const el = ref?.current;
