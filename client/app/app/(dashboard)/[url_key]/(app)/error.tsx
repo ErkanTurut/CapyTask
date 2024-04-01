b790f04eb29a37999cdbc250f152435a5d80bdde
Erkan Turut <erkamturut@gmail.com>
2024-04-02 00:10:02 +0200

    Remove console.error and add a button for error recovery

--- client/app/app/(dashboard)/[url_key]/(app)/error.tsx
+++ client/app/app/(dashboard)/[url_key]/(app)/error.tsx
@@ -9,11 +9,10 @@ export default function Error({
   error: Error & { digest?: string };
   reset: () => void;
 }) {
-  console.error(error);
-
   return (
     <div>
       <h2>Something went wrong!</h2>
+
       <button
         onClick={
           // Attempt to recover by trying to re-render the segment
