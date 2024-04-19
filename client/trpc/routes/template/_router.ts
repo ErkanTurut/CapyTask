c0a6a06ab1d6333669b5a2f731dc42d0d637220a
Erkan Turut <erkamturut@gmail.com>
2024-04-19 18:38:58 +0200

    Update work plan template components and pages

--- client/trpc/routes/template/_router.ts
+++
@@ -1,8 +0,0 @@
-import { router } from "@/trpc/trpc";
-import { inspection } from "./inspection/_router";
-import { step } from "./step/_router";
-
-export const template = router({
-  inspection,
-  step,
-});
