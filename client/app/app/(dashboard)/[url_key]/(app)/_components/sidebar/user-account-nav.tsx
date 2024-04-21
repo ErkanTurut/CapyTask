0a436248141bfa6b65a6897cdd7ca1356a5f7978
Erkan Turut <erkamturut@gmail.com>
2024-04-22 01:16:34 +0200

    Add new sidebar components for team navigation, user account navigation, and workspace selector

---
+++ client/app/app/(dashboard)/[url_key]/(app)/_components/sidebar/user-account-nav.tsx
@@ -0,0 +1,13 @@
+import UserAccountNav from "@/components/user/user-account-nav";
+import { trpc } from "@/trpc/server";
+import { redirect } from "next/navigation";
+
+interface UserNavProps {}
+
+export default async function UserNav({}: UserNavProps) {
+  const { data: user } = await trpc.db.user.getCurrentUser.query();
+  if (!user) {
+    redirect("/login");
+  }
+  return <UserAccountNav className="w-full" user={user} />;
+}
