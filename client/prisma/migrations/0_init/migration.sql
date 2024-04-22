1fe329d10042e85f586605954ea6cc93abae95b6
Erkan Turut <erkamturut@gmail.com>
2024-04-22 17:03:24 +0200

    Update work order components and templates in dashboard app

--- client/prisma/migrations/0_init/migration.sql
+++ client/prisma/migrations/0_init/migration.sql
@@ -1,3 +1,9 @@
+-- CreateExtension
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
+-- CreateExtension
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA "extensions";
+
 -- CreateEnum
 CREATE TYPE "Status" AS ENUM ('OPEN', 'IN_PROGRESS', 'COMPLETED', 'ON_HOLD', 'CANCELED');
 
@@ -30,7 +36,6 @@ CREATE TABLE "user" (
 -- CreateTable
 CREATE TABLE "team" (
     "id" TEXT NOT NULL DEFAULT nanoid(10),
-    "public_id" UUID NOT NULL DEFAULT gen_random_uuid(),
     "name" TEXT NOT NULL,
     "description" TEXT,
     "image_uri" TEXT,
@@ -38,6 +43,7 @@ CREATE TABLE "team" (
     "updated_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "workspace_id" TEXT NOT NULL,
     "identity" TEXT NOT NULL,
+    "public_id" UUID NOT NULL DEFAULT gen_random_uuid(),
 
     CONSTRAINT "team_pkey" PRIMARY KEY ("id")
 );
@@ -45,7 +51,6 @@ CREATE TABLE "team" (
 -- CreateTable
 CREATE TABLE "workspace" (
     "id" TEXT NOT NULL DEFAULT nanoid(10),
-    "public_id" UUID NOT NULL DEFAULT gen_random_uuid(),
     "name" TEXT NOT NULL,
     "description" TEXT,
     "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -53,6 +58,7 @@ CREATE TABLE "workspace" (
     "url_key" TEXT NOT NULL,
     "created_by" UUID NOT NULL,
     "image_uri" TEXT,
+    "public_id" UUID NOT NULL DEFAULT gen_random_uuid(),
 
     CONSTRAINT "workspace_pkey" PRIMARY KEY ("id")
 );
@@ -86,7 +92,7 @@ CREATE TABLE "role" (
 );
 
 -- CreateTable
-CREATE TABLE "inspection_template" (
+CREATE TABLE "work_plan_template" (
     "id" TEXT NOT NULL DEFAULT nanoid(10),
     "public_id" UUID NOT NULL DEFAULT gen_random_uuid(),
     "name" TEXT NOT NULL,
@@ -95,11 +101,11 @@ CREATE TABLE "inspection_template" (
     "updated_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "team_id" TEXT NOT NULL,
 
-    CONSTRAINT "inspection_template_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "work_plan_template_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "step_template" (
+CREATE TABLE "work_step_template" (
     "id" TEXT NOT NULL DEFAULT nanoid(17),
     "public_id" UUID NOT NULL DEFAULT gen_random_uuid(),
     "name" TEXT NOT NULL,
@@ -109,27 +115,27 @@ CREATE TABLE "step_template" (
     "parent_step_id" TEXT,
     "created_by_id" UUID,
     "step_order" INTEGER,
-    "inspection_template_id" TEXT NOT NULL,
+    "work_plan_template_id" TEXT NOT NULL,
 
-    CONSTRAINT "step_template_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "work_step_template_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "inspection_template_snapshot" (
+CREATE TABLE "work_plan" (
     "id" TEXT NOT NULL DEFAULT nanoid(10),
     "public_id" UUID NOT NULL DEFAULT gen_random_uuid(),
     "name" TEXT NOT NULL,
     "description" TEXT,
     "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "inspection_template_id" TEXT,
+    "work_plan_template_id" TEXT,
     "team_id" TEXT NOT NULL,
 
-    CONSTRAINT "inspection_template_snapshot_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "work_plan_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "step_template_snapshot" (
+CREATE TABLE "work_step" (
     "id" TEXT NOT NULL DEFAULT nanoid(17),
     "public_id" UUID NOT NULL DEFAULT gen_random_uuid(),
     "name" TEXT NOT NULL,
@@ -139,13 +145,13 @@ CREATE TABLE "step_template_snapshot" (
     "parent_step_id" TEXT,
     "created_by_id" UUID,
     "step_order" INTEGER,
-    "inspection_template_snapshot_id" TEXT NOT NULL,
+    "work_plan_id" TEXT NOT NULL,
 
-    CONSTRAINT "step_template_snapshot_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "work_step_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "inspection" (
+CREATE TABLE "work_order" (
     "id" TEXT NOT NULL DEFAULT nanoid(10),
     "public_id" UUID NOT NULL DEFAULT gen_random_uuid(),
     "name" TEXT NOT NULL,
@@ -154,22 +160,23 @@ CREATE TABLE "inspection" (
     "updated_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "status" "Status" NOT NULL DEFAULT 'OPEN',
     "team_id" TEXT NOT NULL,
-    "inspection_snapshot_id" TEXT,
+    "work_plan_id" TEXT,
 
-    CONSTRAINT "inspection_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "work_order_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "step" (
+CREATE TABLE "work_step_status" (
     "id" TEXT NOT NULL DEFAULT nanoid(17),
     "public_id" UUID NOT NULL DEFAULT gen_random_uuid(),
     "status" "Status" NOT NULL DEFAULT 'OPEN',
     "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "inspection_id" TEXT NOT NULL,
-    "step_template_snapshot_id" TEXT,
+    "work_order_id" TEXT NOT NULL,
+    "work_step_id" TEXT NOT NULL,
+    "step_order" INTEGER,
 
-    CONSTRAINT "step_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "work_step_status_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
@@ -177,6 +184,9 @@ CREATE UNIQUE INDEX "user_email_key" ON "user"("email");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "user_external_id_key" ON "user"("external_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "team_public_id_key" ON "team"("public_id");
 
 -- CreateIndex
 CREATE INDEX "team_workspace_id_idx" ON "team"("workspace_id");
@@ -188,16 +198,13 @@ CREATE UNIQUE INDEX "team_identity_workspace_id_key" ON "team"("identity", "work
 CREATE UNIQUE INDEX "team_name_workspace_id_key" ON "team"("name", "workspace_id");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "team_public_id_key" ON "team"("public_id");
-
--- CreateIndex
 CREATE UNIQUE INDEX "workspace_url_key_key" ON "workspace"("url_key");
 
 -- CreateIndex
-CREATE INDEX "workspace_url_key_idx" ON "workspace"("url_key");
+CREATE UNIQUE INDEX "workspace_public_id_key" ON "workspace"("public_id");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "workspace_public_id_key" ON "workspace"("public_id");
+CREATE INDEX "workspace_url_key_idx" ON "workspace"("url_key");
 
 -- CreateIndex
 CREATE INDEX "user_team_team_id_idx" ON "user_team"("team_id");
@@ -218,55 +225,55 @@ CREATE INDEX "user_workspace_workspace_id_idx" ON "user_workspace"("workspace_id
 CREATE INDEX "user_workspace_role_id_idx" ON "user_workspace"("role_id");
 
 -- CreateIndex
-CREATE INDEX "inspection_template_team_id_idx" ON "inspection_template"("team_id");
+CREATE UNIQUE INDEX "work_plan_template_public_id_key" ON "work_plan_template"("public_id");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "inspection_template_public_id_key" ON "inspection_template"("public_id");
+CREATE INDEX "work_plan_template_team_id_idx" ON "work_plan_template"("team_id");
 
 -- CreateIndex
-CREATE INDEX "step_template_parent_step_id_idx" ON "step_template"("parent_step_id");
+CREATE UNIQUE INDEX "work_step_template_public_id_key" ON "work_step_template"("public_id");
 
 -- CreateIndex
-CREATE INDEX "step_template_created_by_id_idx" ON "step_template"("created_by_id");
+CREATE INDEX "work_step_template_parent_step_id_idx" ON "work_step_template"("parent_step_id");
 
 -- CreateIndex
-CREATE INDEX "step_template_inspection_template_id_idx" ON "step_template"("inspection_template_id");
+CREATE INDEX "work_step_template_created_by_id_idx" ON "work_step_template"("created_by_id");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "step_template_public_id_key" ON "step_template"("public_id");
+CREATE INDEX "work_step_template_work_plan_template_id_idx" ON "work_step_template"("work_plan_template_id");
 
 -- CreateIndex
-CREATE INDEX "inspection_template_snapshot_inspection_template_id_idx" ON "inspection_template_snapshot"("inspection_template_id");
+CREATE UNIQUE INDEX "work_plan_public_id_key" ON "work_plan"("public_id");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "inspection_template_snapshot_inspection_template_id_created_key" ON "inspection_template_snapshot"("inspection_template_id", "created_at");
+CREATE INDEX "work_plan_work_plan_template_id_idx" ON "work_plan"("work_plan_template_id");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "inspection_template_snapshot_public_id_key" ON "inspection_template_snapshot"("public_id");
+CREATE UNIQUE INDEX "work_plan_work_plan_template_id_created_at_key" ON "work_plan"("work_plan_template_id", "created_at");
 
 -- CreateIndex
-CREATE INDEX "step_template_snapshot_parent_step_id_idx" ON "step_template_snapshot"("parent_step_id");
+CREATE UNIQUE INDEX "work_step_public_id_key" ON "work_step"("public_id");
 
 -- CreateIndex
-CREATE INDEX "step_template_snapshot_created_by_id_idx" ON "step_template_snapshot"("created_by_id");
+CREATE INDEX "work_step_parent_step_id_idx" ON "work_step"("parent_step_id");
 
 -- CreateIndex
-CREATE INDEX "step_template_snapshot_inspection_template_snapshot_id_idx" ON "step_template_snapshot"("inspection_template_snapshot_id");
+CREATE INDEX "work_step_created_by_id_idx" ON "work_step"("created_by_id");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "step_template_snapshot_public_id_key" ON "step_template_snapshot"("public_id");
+CREATE INDEX "work_step_work_plan_id_idx" ON "work_step"("work_plan_id");
 
 -- CreateIndex
-CREATE INDEX "inspection_team_id_idx" ON "inspection"("team_id");
+CREATE UNIQUE INDEX "work_order_public_id_key" ON "work_order"("public_id");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "inspection_public_id_key" ON "inspection"("public_id");
+CREATE INDEX "work_order_team_id_idx" ON "work_order"("team_id");
 
 -- CreateIndex
-CREATE INDEX "step_inspection_id_idx" ON "step"("inspection_id");
+CREATE UNIQUE INDEX "work_step_status_public_id_key" ON "work_step_status"("public_id");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "step_public_id_key" ON "step"("public_id");
+CREATE INDEX "work_step_status_work_order_id_idx" ON "work_step_status"("work_order_id");
 
 -- AddForeignKey
 ALTER TABLE "team" ADD CONSTRAINT "team_workspace_id_fkey" FOREIGN KEY ("workspace_id") REFERENCES "workspace"("id") ON DELETE CASCADE ON UPDATE CASCADE;
@@ -293,41 +300,35 @@ ALTER TABLE "user_workspace" ADD CONSTRAINT "user_workspace_user_id_fkey" FOREIG
 ALTER TABLE "user_workspace" ADD CONSTRAINT "user_workspace_workspace_id_fkey" FOREIGN KEY ("workspace_id") REFERENCES "workspace"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "inspection_template" ADD CONSTRAINT "inspection_template_team_id_fkey" FOREIGN KEY ("team_id") REFERENCES "team"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "work_plan_template" ADD CONSTRAINT "work_plan_template_team_id_fkey" FOREIGN KEY ("team_id") REFERENCES "team"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "step_template" ADD CONSTRAINT "step_template_inspection_template_id_fkey" FOREIGN KEY ("inspection_template_id") REFERENCES "inspection_template"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "work_step_template" ADD CONSTRAINT "work_step_template_parent_step_id_fkey" FOREIGN KEY ("parent_step_id") REFERENCES "work_step_template"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "step_template" ADD CONSTRAINT "step_template_created_by_id_fkey" FOREIGN KEY ("created_by_id") REFERENCES "user"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "work_step_template" ADD CONSTRAINT "work_step_template_work_plan_template_id_fkey" FOREIGN KEY ("work_plan_template_id") REFERENCES "work_plan_template"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "step_template" ADD CONSTRAINT "step_template_parent_step_id_fkey" FOREIGN KEY ("parent_step_id") REFERENCES "step_template"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "work_plan" ADD CONSTRAINT "work_plan_team_id_fkey" FOREIGN KEY ("team_id") REFERENCES "team"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "inspection_template_snapshot" ADD CONSTRAINT "inspection_template_snapshot_inspection_template_id_fkey" FOREIGN KEY ("inspection_template_id") REFERENCES "inspection_template"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "work_plan" ADD CONSTRAINT "work_plan_work_plan_template_id_fkey" FOREIGN KEY ("work_plan_template_id") REFERENCES "work_plan_template"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "inspection_template_snapshot" ADD CONSTRAINT "inspection_template_snapshot_team_id_fkey" FOREIGN KEY ("team_id") REFERENCES "team"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "work_step" ADD CONSTRAINT "work_step_parent_step_id_fkey" FOREIGN KEY ("parent_step_id") REFERENCES "work_step"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "step_template_snapshot" ADD CONSTRAINT "step_template_snapshot_inspection_template_snapshot_id_fkey" FOREIGN KEY ("inspection_template_snapshot_id") REFERENCES "inspection_template_snapshot"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "work_step" ADD CONSTRAINT "work_step_work_plan_id_fkey" FOREIGN KEY ("work_plan_id") REFERENCES "work_plan"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "step_template_snapshot" ADD CONSTRAINT "step_template_snapshot_created_by_id_fkey" FOREIGN KEY ("created_by_id") REFERENCES "user"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "work_order" ADD CONSTRAINT "work_order_team_id_fkey" FOREIGN KEY ("team_id") REFERENCES "team"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "step_template_snapshot" ADD CONSTRAINT "step_template_snapshot_parent_step_id_fkey" FOREIGN KEY ("parent_step_id") REFERENCES "step_template_snapshot"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "work_order" ADD CONSTRAINT "work_order_work_plan_id_fkey" FOREIGN KEY ("work_plan_id") REFERENCES "work_plan"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "inspection" ADD CONSTRAINT "inspection_team_id_fkey" FOREIGN KEY ("team_id") REFERENCES "team"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "work_step_status" ADD CONSTRAINT "work_step_status_work_order_id_fkey" FOREIGN KEY ("work_order_id") REFERENCES "work_order"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "inspection" ADD CONSTRAINT "inspection_inspection_snapshot_id_fkey" FOREIGN KEY ("inspection_snapshot_id") REFERENCES "inspection_template_snapshot"("id") ON DELETE SET NULL ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "step" ADD CONSTRAINT "step_inspection_id_fkey" FOREIGN KEY ("inspection_id") REFERENCES "inspection"("id") ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "step" ADD CONSTRAINT "step_step_template_snapshot_id_fkey" FOREIGN KEY ("step_template_snapshot_id") REFERENCES "step_template_snapshot"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "work_step_status" ADD CONSTRAINT "work_step_status_work_step_id_fkey" FOREIGN KEY ("work_step_id") REFERENCES "work_step"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
