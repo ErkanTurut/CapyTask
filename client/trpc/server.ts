0a8c4dd0265029f64699ae2c35ecd32cd74baa18
Erkan Turut <erkamturut@gmail.com>
2024-04-23 22:46:30 +0200

    Update trpc server configuration and links in client/trpc/server.ts, call without http request + cache 5s

--- client/trpc/server.ts
+++ client/trpc/server.ts
@@ -1,48 +1,61 @@
 // src/trpc/server.ts
-import type { AppRouter } from "./routes/_app";
+import { AppRouter } from "./routes/_app";
 import { createTRPCClient, httpBatchLink, loggerLink } from "@trpc/client";
-import { headers } from "next/headers";
+import { cookies, headers } from "next/headers";
+import { appRouter } from "@/trpc";
 
 import superjson from "superjson";
 
-// export const trpc = experimental_createTRPCNextAppDirServer<AppRouter>({
-//   config() {
-//     return {
-//       links: [
-//         loggerLink({
-//           enabled: (op) => true,
-//         }),
-//         experimental_nextHttpLink({
-//           batch: true,
-//           url: process.env.NEXT_PUBLIC_ROOT_DOMAIN + "/api/trpc",
-//           transformer: superjson,
-//           headers() {
-//             return {
-//               cookie: cookies().toString(),
-//               "x-trpc-source": "rsc-http",
-//             };
-//           },
-//         }),
-//       ],
-//     };
-//   },
-// });
-
-export const trpc = createTRPCClient<AppRouter>({
-  links: [
-    loggerLink({
-      enabled: (opts) =>
-        process.env.NODE_ENV === "development" ||
-        (opts.direction === "down" && opts.result instanceof Error),
-    }),
-    httpBatchLink({
-      transformer: superjson,
-      url: `${process.env.NEXT_PUBLIC_ROOT_DOMAIN}/api/trpc`,
-      headers: () => {
-        const h = new Map(headers());
-        h.set("x-trpc-source", "nextjs-react");
-        return Object.fromEntries(h);
-      },
-    }),
-  ],
+import { experimental_createTRPCNextAppDirServer } from "@trpc/next/app-dir/server";
+import { experimental_nextCacheLink } from "@trpc/next/app-dir/links/nextCache";
+import { createClient } from "@/lib/supabase/server";
+export const trpc = experimental_createTRPCNextAppDirServer<typeof appRouter>({
+  config() {
+    return {
+      links: [
+        loggerLink({
+          enabled: (op) => true,
+        }),
+        experimental_nextCacheLink({
+          transformer: superjson,
+          // requests are cached for 5 seconds
+          revalidate: 5,
+          router: appRouter,
+          async createContext() {
+            const supabase = createClient();
+            const {
+              data: { session },
+            } = await supabase.auth.getSession();
+            return {
+              session: session,
+              headers: {
+                cookie: cookies().toString(),
+                "x-trpc-source": "rsc-invoke",
+              },
+              db: supabase,
+            };
+          },
+        }),
+      ],
+    };
+  },
 });
+
+// export const trpc = createTRPCClient<AppRouter>({
+//   links: [
+//     loggerLink({
+//       enabled: (opts) =>
+//         process.env.NODE_ENV === "development" ||
+//         (opts.direction === "down" && opts.result instanceof Error),
+//     }),
+//     httpBatchLink({
+//       transformer: superjson,
+//       url: `${process.env.NEXT_PUBLIC_ROOT_DOMAIN}/api/trpc`,
+//       headers: () => {
+//         const h = new Map(headers());
+//         h.set("x-trpc-source", "nextjs-react");
+//         return Object.fromEntries(h);
+//       },
+//     }),
+//   ],
+// });
