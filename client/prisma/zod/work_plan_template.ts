5860739afacbadb67f921746f17f40a670d9a1e4
Erkan Turut <erkamturut@gmail.com>
2024-05-23 20:01:05 +0200

    Refactor location routes and handlers for fetching locations by workspace

---
+++ client/prisma/zod/work_plan_template.ts
@@ -0,0 +1,29 @@
+import * as z from "zod"
+import { Completework_plan, Relatedwork_planModel, Completeteam, RelatedteamModel, Completework_step_template, Relatedwork_step_templateModel } from "./index"
+
+export const work_plan_templateModel = z.object({
+  id: z.string(),
+  public_id: z.string(),
+  name: z.string(),
+  description: z.string().nullish(),
+  created_at: z.date(),
+  updated_at: z.date(),
+  team_id: z.string(),
+})
+
+export interface Completework_plan_template extends z.infer<typeof work_plan_templateModel> {
+  work_plan: Completework_plan[]
+  team: Completeteam
+  work_step_template: Completework_step_template[]
+}
+
+/**
+ * Relatedwork_plan_templateModel contains all relations on your model in addition to the scalars
+ *
+ * NOTE: Lazy required in case of potential circular dependencies within schema
+ */
+export const Relatedwork_plan_templateModel: z.ZodSchema<Completework_plan_template> = z.lazy(() => work_plan_templateModel.extend({
+  work_plan: Relatedwork_planModel.array(),
+  team: RelatedteamModel,
+  work_step_template: Relatedwork_step_templateModel.array(),
+}))
