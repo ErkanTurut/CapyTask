d63d41d82a554dabb97f8534c71e3f33e43b2393
Erkan Turut <erkamturut@gmail.com>
2024-05-31 23:15:44 +0200

    chore: Update npm dependencies to latest versions

--- client/lib/utils.ts
+++ client/lib/utils.ts
@@ -1,5 +1,6 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+import * as React from "react";
 
 import { formatDistanceToNowStrict } from "date-fns";
 import locale from "date-fns/locale/en-US";
@@ -202,4 +203,35 @@ export function generateAvatar({
   }${size ? `&size=${size}` : ""}`;
 
   return { image_url, initials };
+}
+
+type PossibleRef<T> = React.Ref<T> | undefined;
+
+/**
+ * Set a given ref to a given value
+ * This utility takes care of different types of refs: callback refs and RefObject(s)
+ */
+function setRef<T>(ref: PossibleRef<T>, value: T) {
+  if (typeof ref === "function") {
+    ref(value);
+  } else if (ref !== null && ref !== undefined) {
+    (ref as React.MutableRefObject<T>).current = value;
+  }
+}
+
+/**
+ * A utility to compose multiple refs together
+ * Accepts callback refs and RefObject(s)
+ */
+export function composeRefs<T>(...refs: PossibleRef<T>[]) {
+  return (node: T) => refs.forEach((ref) => setRef(ref, node));
+}
+
+/**
+ * A custom hook that composes multiple refs
+ * Accepts callback refs and RefObject(s)
+ */
+export function useComposedRefs<T>(...refs: PossibleRef<T>[]) {
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  return React.useCallback(composeRefs(...refs), refs);
 }
