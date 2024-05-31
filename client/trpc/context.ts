edbdb270c25152b385abeae7e5a0f8b3bb7a34b0
Erkan Turut <erkamturut@gmail.com>
2024-05-31 23:15:47 +0200

    Refactor createContext function to use cache for getUser

--- client/trpc/context.ts
+++ client/trpc/context.ts
@@ -1,11 +1,14 @@
 import { createClient } from "@/lib/supabase/server";
 import { FetchCreateContextFnOptions } from "@trpc/server/adapters/fetch";
 import { cookies } from "next/headers";
+import { cache } from "react";
+
+const getUser = cache(async () => {
+  return await createClient(cookies()).auth.getUser();
+});
 
 export async function createContext(opts?: FetchCreateContextFnOptions) {
-  const {
-    data: { user },
-  } = await createClient(cookies()).auth.getUser();
+  const { data: user } = await getUser();
 
   return {
     user,
