bc8352a968d9a147fbd4cc3951e392c7d0739f62
Erkan Turut <erkamturut@gmail.com>
2024-07-03 00:58:30 +0200

    refactor: Update layout component with resizable sidebar and AI assistant modal

--- client/app/app/(dashboard)/[url_key]/(app)/layout.tsx
+++ client/app/app/(dashboard)/[url_key]/(app)/layout.tsx
@@ -5,9 +5,12 @@ import Resizable from "./_components/rezisable";
 import Toolbar from "@/components/layouts/toolbar/top-toolbar";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { AssistantModal } from "./_components/ai/assistant-modal";
+import ResizableGroup from "./_components/resizableGroup";
 import Sidebar from "./_components/sidebar";
-import { Providers } from "./providers";
-import { trpc } from "@/trpc/server";
+
+import { SidebarProvider } from "@/lib/store";
+import { AI } from "./_components/ai/actions";
+import { LivekitRoomProvider } from "./_components/ai/livekitRoomProvider";
 interface DashboardLayoutProps {
   children: React.ReactNode;
   params: {
@@ -20,25 +23,36 @@ export default async function DashboardLayout({
   params,
 }: DashboardLayoutProps) {
   const layout = cookies().get("react-resizable-panels:layout");
+  const collapsed = cookies().get("react-resizable-panels:collapsed");
+
+  const defaultCollapsed = collapsed
+    ? (JSON.parse(collapsed.value) as boolean)
+    : undefined;
   const defaultLayout = layout
     ? (JSON.parse(layout.value) as number[])
     : undefined;
 
   return (
-    <Providers>
-      <Sidebar params={params} />
-      {/* <DotPattern className="absolute top-0 z-[-2] h-screen w-screen bg-background  "> */}
-      <Resizable defaultLayout={defaultLayout}>
-        <AssistantModal />
-        <ScrollArea className="h-screen">
-          <Toolbar className="sticky top-0 z-40 hidden p-2 pb-4 sm:block" />
+    <AI>
+      <LivekitRoomProvider>
+        <SidebarProvider isCollapsed={defaultCollapsed || false}>
+          <ResizableGroup>
+            <Sidebar params={params} />
+            {/* <DotPattern className="absolute top-0 z-[-2] h-screen w-screen bg-background  "> */}
+            <Resizable defaultLayout={defaultLayout}>
+              <AssistantModal />
+              <ScrollArea className="h-screen">
+                <Toolbar className="sticky top-0 z-40 hidden p-2 pb-4 sm:block" />
 
-          {children}
+                {children}
 
-          <Toolbar className="sticky bottom-0 z-40 mt-4 block rounded-md border bg-muted p-2 pt-4 sm:hidden" />
-        </ScrollArea>
-      </Resizable>
-      {/* </DotPattern> */}
-    </Providers>
+                <Toolbar className="sticky bottom-0 z-40 mt-4 block rounded-md border bg-muted p-2 pt-4 sm:hidden" />
+              </ScrollArea>
+            </Resizable>
+            {/* </DotPattern> */}
+          </ResizableGroup>
+        </SidebarProvider>
+      </LivekitRoomProvider>
+    </AI>
   );
 }
