99d8c97ab997dac7349a69f5b429bbed6a712822
Erkan Turut <erkamturut@gmail.com>
2024-07-22 00:08:29 +0200

    refactor: Update AI assistant route to use local API endpoint for testing

--- client/middleware.ts
+++ client/middleware.ts
@@ -1,31 +1,19 @@
 import { NextRequest, NextResponse } from "next/server";
-
 import { createClient } from "./lib/supabase/middleware";
+import { Session } from "@supabase/supabase-js";
+import { SupabaseClient } from "./lib/supabase/server";
 
 export const config = {
-  matcher: [
-    /*
-     * Match all paths except for:
-     * 1. /api routes
-     * 2. /_next (Next.js internals)
-     * 3. /_static (inside /public)
-     * 4. all root files inside /public (e.g. /favicon.ico)
-     */
-    "/((?!api|_next/static|_next/image|home|public|favicon.ico).*)",
-  ],
+  matcher: ["/((?!api|_next/static|_next/image|home|public|favicon.ico).*)"],
 };
 
-export default async function middleware(req: NextRequest) {
-  const url = req.nextUrl;
+const publicAppRoutes = ["/login", "/signup", "/logout"];
 
-  const { supabase, response } = createClient(req);
-
-  // Get hostname of request (e.g. demo.vercel.pub, demo.localhost:3000)
+async function getHostname(req: NextRequest): Promise<string> {
   let hostname = req.headers
     .get("host")!
     .replace(".localhost:3000", `.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`);
 
-  // special case for Vercel preview deployment URLs
   if (
     hostname.includes("---") &&
     hostname.endsWith(`.${process.env.NEXT_PUBLIC_VERCEL_DEPLOYMENT_SUFFIX}`)
@@ -35,71 +23,87 @@ export default async function middleware(req: NextRequest) {
     }`;
   }
 
-  const searchParams = req.nextUrl.searchParams.toString();
-  const path = `${url.pathname}${
-    searchParams.length > 0 ? `?${searchParams}` : ""
-  }`;
+  return hostname;
+}
 
-  // rewrites for app pages
-  if (hostname == `app.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`) {
-    const {
-      data: { user },
-      error,
-    } = await supabase.auth.getUser();
+function getPathWithSearchParams(url: URL): string {
+  const searchParams = url.searchParams.toString();
+  return `${url.pathname}${searchParams ? `?${searchParams}` : ""}`;
+}
 
-    if (
-      (!user &&
-        path !== "/login" &&
-        path !== "/signup" &&
-        path !== "/logout") ||
-      (user && path === "/signup")
-    ) {
-      return NextResponse.redirect(new URL("/login", req.url), {
-        headers: response.headers,
-      });
-    } else if (user && path == "/login") {
-      const { data: workspaces, error } = await supabase
-        .from("workspace")
-        .select("*, user_workspace!inner(*)")
-        .eq("user_workspace.user_id", user.id);
+async function handleAppHostname(
+  req: NextRequest,
+  supabase: SupabaseClient,
+  path: string,
+  response: NextResponse,
+): Promise<NextResponse> {
+  const {
+    data: { session },
+    error,
+  } = await supabase.auth.getSession();
 
-      if (!workspaces || workspaces.length < 1 || error) {
-        return NextResponse.redirect(new URL("/create", req.url), {
-          headers: response.headers,
-        });
-      }
-      let stored_workspace: { url_key: string } | null;
-      try {
-        stored_workspace = JSON.parse(
-          req.cookies.get("workspace_url_key")?.value as string,
-        );
-      } catch (error) {
-        stored_workspace = null;
-      }
-      const workspace =
-        workspaces.find(
-          (workspace) => workspace.url_key === stored_workspace?.url_key,
-        ) || workspaces[0];
+  if (
+    (!session && !publicAppRoutes.includes(path)) ||
+    (session && path === "/signup")
+  ) {
+    return NextResponse.redirect(new URL("/login", req.url), {
+      headers: response.headers,
+    });
+  } else if (session && path === "/login") {
+    const { data: workspaces, error } = await supabase
+      .from("workspace")
+      .select("*, user_workspace(user_id, workspace_id)")
+      .eq("user_workspace.user_id", session.user.id);
 
-      if (!workspace) {
-        return NextResponse.redirect(new URL("/create", req.url), {
-          headers: response.headers,
-        });
-      }
-
-      return NextResponse.redirect(new URL(`/${workspace.url_key}`, req.url), {
+    if (!workspaces || workspaces.length < 1 || error) {
+      return NextResponse.redirect(new URL("/create", req.url), {
         headers: response.headers,
       });
     }
-    return NextResponse.rewrite(
-      new URL(`/app${path === "/" ? "" : path}`, req.url),
-      {
+
+    let storedWorkspace: { url_key: string } | null = null;
+    try {
+      storedWorkspace = JSON.parse(
+        req.cookies.get("workspace_url_key")?.value as string,
+      );
+    } catch (error) {
+      storedWorkspace = null;
+    }
+
+    const workspace =
+      workspaces.find(
+        (workspace) => workspace.url_key === storedWorkspace?.url_key,
+      ) || workspaces[0];
+
+    if (!workspace) {
+      return NextResponse.redirect(new URL("/create", req.url), {
         headers: response.headers,
-      },
-    );
+      });
+    }
+
+    return NextResponse.redirect(new URL(`/${workspace.url_key}`, req.url), {
+      headers: response.headers,
+    });
   }
 
-  // rewrite root application to `/home` folder
+  return NextResponse.rewrite(
+    new URL(`/app${path === "/" ? "" : path}`, req.url),
+    {
+      headers: response.headers,
+    },
+  );
+}
+
+export default async function middleware(req: NextRequest) {
+  const url = req.nextUrl;
+  const { supabase, response } = createClient(req);
+  const hostname = await getHostname(req);
+  const path = getPathWithSearchParams(url);
+
+  if (hostname === `app.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`) {
+    return handleAppHostname(req, supabase, path, response);
+  }
+
   if (
     hostname === "localhost:3000" ||
     hostname === process.env.NEXT_PUBLIC_ROOT_DOMAIN
@@ -109,6 +113,5 @@ export default async function middleware(req: NextRequest) {
     );
   }
 
-  // rewrite everything else to `/[domain]/[slug] dynamic route
   return NextResponse.rewrite(new URL(`/${hostname}${path}`, req.url));
 }
