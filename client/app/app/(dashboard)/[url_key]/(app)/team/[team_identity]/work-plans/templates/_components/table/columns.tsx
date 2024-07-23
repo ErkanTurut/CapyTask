6e3962ef7e5f3c31994de9a668ace1de9dc332ad
Erkan Turut <erkamturut@gmail.com>
2024-07-24 00:32:14 +0200

    refactor: Remove unused table data and toolbar components

--- client/app/app/(dashboard)/[url_key]/(app)/team/[team_identity]/work-plans/templates/_components/table/columns.tsx
+++ client/app/app/(dashboard)/[url_key]/(app)/team/[team_identity]/work-plans/templates/_components/table/columns.tsx
@@ -1,20 +1,20 @@
 "use client";
 
-import { buttonVariants } from "@/components/ui/button";
+import { DataTableColumnHeader } from "@/components/table/data-table-column-header";
+import { Button, buttonVariants } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
 import { cn } from "@/lib/utils";
 import { ColumnDef } from "@tanstack/react-table";
 import Link from "next/link";
-import { DataTableColumnHeader } from "@/components/table/data-table-column-header";
-import { DataTableRowActions } from "./data-table-row-actions";
-import { Checkbox } from "@/components/ui/checkbox";
-import type { trpc } from "@/trpc/server";
+import { DataTableRowActions } from "./DataTableRowActions";
+
+import { RouterOutput } from "@/trpc/client";
+import { DotsHorizontalIcon } from "@radix-ui/react-icons";
 
 export const columns: ColumnDef<
   NonNullable<
-    Awaited<
-      ReturnType<(typeof trpc)["db"]["work_plan_template"]["get"]["byId"]>
-    >["data"]
-  >
+    RouterOutput["db"]["work_plan_template"]["get"]["byTeamId"]["data"]
+  >[number]
 >[] = [
   {
     id: "select",
@@ -38,7 +38,6 @@ export const columns: ColumnDef<
     enableSorting: false,
     enableHiding: false,
   },
-
   {
     accessorKey: "name",
     header: ({ column }) => {
@@ -47,7 +46,7 @@ export const columns: ColumnDef<
     cell: ({ row }) => {
       return (
         <Link
-          href={`./templates/${row.original.id}`}
+          href={{ pathname: `assets/${row.original.id}` }}
           className={cn(
             buttonVariants({ variant: "link", size: "sm" }),
             "underline",
@@ -98,6 +97,22 @@ export const columns: ColumnDef<
   },
   {
     id: "actions",
-    cell: ({ row }) => <DataTableRowActions row={row} />,
+    header: ({ column, table }) => {
+      return (
+        (table.getIsSomePageRowsSelected() ||
+          table.getIsAllPageRowsSelected()) && (
+          <Button
+            variant="ghost"
+            className="flex h-7 w-7 p-0 data-[state=open]:bg-muted"
+          >
+            <DotsHorizontalIcon className="h-4 w-4" />
+            <span className="sr-only">Open menu</span>
+          </Button>
+        )
+      );
+    },
+    cell: ({ row, table }) => {
+      return <DataTableRowActions row={row} table={table} />;
+    },
   },
 ];
