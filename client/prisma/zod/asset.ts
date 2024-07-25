03fbd918eeb8e200ed4922242fcc79de8a2efff5
Erkan Turut <erkamturut@gmail.com>
2024-07-26 00:44:59 +0200

    refactor: Update zod imports and formatting in asset.ts and index.ts

--- client/prisma/zod/asset.ts
+++ client/prisma/zod/asset.ts
@@ -1,12 +1,5 @@
-import * as z from "zod";
-import {
-  Completelocation,
-  RelatedlocationModel,
-  Completeworkspace,
-  RelatedworkspaceModel,
-  Completework_order_asset,
-  Relatedwork_order_assetModel,
-} from "./index";
+import * as z from "zod"
+import { Completelocation, RelatedlocationModel, Completeworkspace, RelatedworkspaceModel, Completework_order_asset, Relatedwork_order_assetModel } from "./index"
 
 export const assetModel = z.object({
   id: z.string(),
@@ -17,12 +10,12 @@ export const assetModel = z.object({
   updated_at: z.date(),
   workspace_id: z.string(),
   location_id: z.string().nullish(),
-});
+})
 
 export interface Completeasset extends z.infer<typeof assetModel> {
-  location?: Completelocation | null;
-  workspace: Completeworkspace;
-  work_order_asset: Completework_order_asset[];
+  location?: Completelocation | null
+  workspace: Completeworkspace
+  work_order_asset: Completework_order_asset[]
 }
 
 /**
@@ -30,10 +23,8 @@ export interface Completeasset extends z.infer<typeof assetModel> {
  *
  * NOTE: Lazy required in case of potential circular dependencies within schema
  */
-export const RelatedassetModel: z.ZodSchema<Completeasset> = z.lazy(() =>
-  assetModel.extend({
-    location: RelatedlocationModel.nullish(),
-    workspace: RelatedworkspaceModel,
-    work_order_asset: Relatedwork_order_assetModel.array(),
-  }),
-);
+export const RelatedassetModel: z.ZodSchema<Completeasset> = z.lazy(() => assetModel.extend({
+  location: RelatedlocationModel.nullish(),
+  workspace: RelatedworkspaceModel,
+  work_order_asset: Relatedwork_order_assetModel.array(),
+}))
