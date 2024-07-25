ee7ca99e3c24b66117be1ebe810fc1f4c864a324
Erkan Turut <erkamturut@gmail.com>
2024-07-26 00:44:51 +0200

    refactor: Update work_plan_template router to include update functionality

--- client/trpc/server/routes/work_plan_template/_router.ts
+++ client/trpc/server/routes/work_plan_template/_router.ts
@@ -12,16 +12,19 @@ import {
   searchWorkPlanTemplateHandler,
 } from "./get.handler";
 import { ZGetWorkPlanTemplateSchema } from "./get.schema";
+import { ZUpdateWorkPlanTemplateSchema } from "./update.schema";
+import { updateWorkPlanTemplateHandler } from "./update.handler";
 
 export const work_plan_template = router({
   get: router({
     byId: protectedProcedure
       .input(ZGetWorkPlanTemplateSchema.pick({ id: true }))
       .query(async ({ ctx, input }) => {
-        return await getWorkPlanTemplateHandler({
+        const { data } = await getWorkPlanTemplateHandler({
           input,
           db: ctx.db,
         });
+        return data;
       }),
     withSteps: protectedProcedure
       .input(ZGetWorkPlanTemplateSchema.pick({ id: true }))
@@ -43,28 +46,6 @@ export const work_plan_template = router({
         return { data, count };
       }),
   }),
-
-  getWorkPlanTemplatesByTeamId: protectedProcedure
-    .input(ZGetWorkPlanTemplateSchema.pick({ team_id: true, range: true }))
-    .query(async ({ ctx, input }) => {
-      return await getWorkPlanTemplatesByTeamIdHandler({
-        input,
-        db: ctx.db,
-      });
-    }),
-
-  getWorkPlanTemplatesByIdentity: protectedProcedure
-    .input(
-      ZGetWorkPlanTemplateSchema.pick({ team_identity: true, range: true }),
-    )
-    .query(async ({ ctx, input }) => {
-      const { data, count } = await getWorkPlanTemplatesByIdentityHandler({
-        input,
-        db: ctx.db,
-      });
-      return { data, count };
-    }),
-
   search: protectedProcedure
     .input(ZGetWorkPlanTemplateSchema.pick({ q: true, team_identity: true }))
     .query(async ({ ctx, input }) => {
@@ -84,6 +65,14 @@ export const work_plan_template = router({
     .input(ZDeleteWorkPlanTemplateSchema)
     .mutation(async ({ ctx, input }) => {
       return await deleteWorkPlanTemplateHandler({
+        input,
+        db: ctx.db,
+      });
+    }),
+  update: protectedProcedure
+    .input(ZUpdateWorkPlanTemplateSchema)
+    .mutation(async ({ ctx, input }) => {
+      return await updateWorkPlanTemplateHandler({
         input,
         db: ctx.db,
       });
