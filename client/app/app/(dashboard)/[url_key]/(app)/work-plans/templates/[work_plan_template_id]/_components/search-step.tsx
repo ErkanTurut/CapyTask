f07d05fca2b79d4505bf3e03fa5cf7cbbf5a824a
Erkan Turut <erkamturut@gmail.com>
2024-07-28 18:02:17 +0200

    change workspace template link logic from team to workspace and permission based

---
+++ client/app/app/(dashboard)/[url_key]/(app)/work-plans/templates/[work_plan_template_id]/_components/search-step.tsx
@@ -0,0 +1,137 @@
+"use client";
+
+import { useState } from "react";
+
+import {
+  Command,
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+} from "@/components/ui/command";
+import { Drawer, DrawerContent } from "@/components/ui/drawer";
+import { useMediaQuery } from "@/lib/hooks/use-media-query";
+import Link from "next/link";
+
+import { Icons } from "@/components/icons";
+import { trpc } from "@/trpc/server";
+import { useRouter } from "next/navigation";
+import { useDebouncedCallback } from "use-debounce";
+
+interface SearchStepProps {
+  searchParams: {
+    q: string;
+  };
+  work_plan_template_id: string;
+  initialData: NonNullable<
+    Awaited<
+      ReturnType<
+        (typeof trpc)["db"]["work_step_template"]["getStepsByWorkPlanTemplate"]
+      >
+    >
+  >;
+}
+
+export function SearchStep({
+  searchParams,
+  work_plan_template_id,
+  initialData,
+}: SearchStepProps) {
+  const router = useRouter();
+  const isDesktop = useMediaQuery("(min-width: 768px)");
+
+  const debouncedValue = useDebouncedCallback((value: string) => {
+    router.replace(`?q=${encodeURIComponent(value)}`);
+  }, 1000);
+
+  if (isDesktop) {
+    return (
+      <CommandDialog open={true} onOpenChange={() => router.back()}>
+        <StepCommand
+          onSearch={debouncedValue}
+          query={searchParams.q}
+          initialData={initialData}
+          work_plan_template_id={work_plan_template_id}
+        />
+      </CommandDialog>
+    );
+  }
+
+  return (
+    <Drawer open={true} onClose={() => router.back()}>
+      <DrawerContent>
+        <div className="mt-4 border-t">
+          <StepCommand
+            onSearch={debouncedValue}
+            query={searchParams.q}
+            initialData={initialData}
+            work_plan_template_id={work_plan_template_id}
+          />
+        </div>
+      </DrawerContent>
+    </Drawer>
+  );
+}
+
+export function StepCommand({
+  query,
+  onSearch,
+  initialData,
+  work_plan_template_id,
+}: {
+  query?: string;
+  onSearch: (value: string) => void;
+  work_plan_template_id: string;
+  initialData: NonNullable<
+    Awaited<
+      ReturnType<
+        (typeof trpc)["db"]["work_step_template"]["getStepsByWorkPlanTemplate"]
+      >
+    >
+  >;
+}) {
+  const [searchValue, setSearchValue] = useState(query || "");
+
+  return (
+    <Command shouldFilter={false}>
+      <CommandInput
+        value={searchValue}
+        onValueChange={(e) => {
+          setSearchValue(e);
+          onSearch(e);
+        }}
+        placeholder="Search for a step..."
+      />
+      <CommandList>
+        <CommandGroup>
+          <Link href={"./create"}>
+            <CommandItem className="cursor-pointer">
+              <Icons.plusCircled className="mr-2 h-4 w-4" />
+              <span>Create a new step</span>
+            </CommandItem>
+          </Link>
+        </CommandGroup>
+        <CommandSeparator alwaysRender className="mb-1" />
+        <CommandEmpty>no found</CommandEmpty>
+        <CommandGroup>
+          {initialData.map((step) => (
+            <CommandItem
+              className="cursor-pointer"
+              key={step.id}
+              value={step.id}
+              onSelect={(e) => {}}
+            >
+              {step.work_plan_template_id === work_plan_template_id && (
+                <Icons.checkCircled className="mr-2 h-4 w-4" />
+              )}
+              <span>{step.name}</span>
+            </CommandItem>
+          ))}
+        </CommandGroup>
+      </CommandList>
+    </Command>
+  );
+}
