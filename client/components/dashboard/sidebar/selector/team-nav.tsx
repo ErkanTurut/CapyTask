76d29db15b47f32df9071c4f31fbffcfbb31cdfb
Erkan Turut <erkamturut@gmail.com>
2024-08-01 00:25:38 +0200

    feat: Add sidebar navigation components for user account, workspace, and team

---
+++ client/components/dashboard/sidebar/selector/team-nav.tsx
@@ -0,0 +1,34 @@
+import { Nav } from "@/components/layouts/side-navigation/nav";
+import { appNavItems } from "@/config/dashboard.config";
+import { generateAvatar } from "@/lib/utils";
+import { trpc } from "@/trpc/server";
+
+interface TeamNavProps {
+  params: {
+    url_key: string;
+  };
+}
+
+export default async function TeamNav({ params }: TeamNavProps) {
+  const { data: teams } = await trpc.db.team.getByWorkspaceUrlKey({
+    url_key: params.url_key,
+  });
+  return (
+    <Nav
+      size={"sm"}
+      items={
+        teams?.map((team) => ({
+          image_url: generateAvatar({
+            name: team.name,
+          }).image_url,
+          title: team.name,
+          href: `/${team.identity}`,
+          items: appNavItems.teamNav,
+          id: team.identity,
+        })) || []
+      }
+      defaultValue="teams"
+      rootPath={`/${params.url_key}/team`}
+    />
+  );
+}
---
+++ client/components/dashboard/sidebar/selector/user-account-nav.tsx
@@ -0,0 +1,14 @@
+import UserAccountNav from "@/components/dashboard/navigation/user-account-nav";
+import { trpc } from "@/trpc/server";
+import { redirect } from "next/navigation";
+
+interface UserNavProps {}
+
+export default async function UserNav({}: UserNavProps) {
+  const { data: user } = await trpc.db.user.getCurrentUser();
+
+  if (!user) {
+    redirect("/login");
+  }
+  return <UserAccountNav className="w-full" user={user} />;
+}
