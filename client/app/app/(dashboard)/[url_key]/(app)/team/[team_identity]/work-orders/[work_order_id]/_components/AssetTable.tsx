4b1d9831436165951e2b570fbf3de2fc7b938f00
Erkan Turut <erkamturut@gmail.com>
2024-08-06 22:12:39 +0200

    refactor: Update work order create form to use TCreateWorkOrderSchema
    Refactor the work order create form to use the `TCreateWorkOrderSchema` instead of `TCreateWorkOrderWithStepsSchema`. This ensures correct validation of form data and improves codebase consistency.

--- client/app/app/(dashboard)/[url_key]/(app)/team/[team_identity]/work-orders/[work_order_id]/_components/AssetTable.tsx
+++ client/app/app/(dashboard)/[url_key]/(app)/team/[team_identity]/work-orders/[work_order_id]/_components/AssetTable.tsx
@@ -18,7 +18,6 @@ import {
 import {
   Tooltip,
   TooltipContent,
-  TooltipProvider,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
 import { Database } from "@/lib/supabase/server";
@@ -38,63 +37,61 @@ interface AssetTableProps {
 
 export default async function AssetTable({ params, assets }: AssetTableProps) {
   return (
-    <TooltipProvider>
-      <Card x-chunk="dashboard-05-chunk-3">
-        <CardHeader className="px-7">
-          <CardTitle>Assets</CardTitle>
-          <CardDescription>Related assets to this work order.</CardDescription>
-        </CardHeader>
-        <CardContent>
-          {assets.length > 0 ? (
-            <Table>
-              <TableHeader>
-                <TableRow>
-                  <TableHead>Order</TableHead>
-                  <TableHead className="table-cell">Title</TableHead>
-                  <TableHead className="hidden md:table-cell">
-                    Last update
-                  </TableHead>
-                </TableRow>
-              </TableHeader>
-              <TableBody>
-                {assets.map((asset, index) => {
-                  return (
-                    <TableRow key={asset.id}>
-                      <TableCell className="">{index + 1}</TableCell>
-                      <TableCell className="table-cell max-w-40 overflow-hidden overflow-ellipsis whitespace-nowrap">
-                        <div className="font-normal sm:font-medium">
-                          {asset.name}
-                        </div>
-                        <div className="hidden text-sm text-muted-foreground md:inline">
-                          {asset.description} {asset.id}
-                        </div>
-                      </TableCell>
+    <Card x-chunk="dashboard-05-chunk-3">
+      <CardHeader className="px-7">
+        <CardTitle>Assets</CardTitle>
+        <CardDescription>Related assets to this work order.</CardDescription>
+      </CardHeader>
+      <CardContent>
+        {assets.length > 0 ? (
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead>Order</TableHead>
+                <TableHead className="table-cell">Title</TableHead>
+                <TableHead className="hidden md:table-cell">
+                  Last update
+                </TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {assets.map((asset, index) => {
+                return (
+                  <TableRow key={asset.id}>
+                    <TableCell className="">{index + 1}</TableCell>
+                    <TableCell className="table-cell max-w-40 overflow-hidden overflow-ellipsis whitespace-nowrap">
+                      <div className="font-normal sm:font-medium">
+                        {asset.name}
+                      </div>
+                      <div className="hidden text-sm text-muted-foreground md:inline">
+                        {asset.description} {asset.id}
+                      </div>
+                    </TableCell>
 
-                      <TableCell className="hidden sm:table-cell">
-                        <Tooltip>
-                          <TooltipTrigger>
-                            {formatTimeToNow(new Date(asset.updated_at))}
-                          </TooltipTrigger>
-                          <TooltipContent>
-                            {formatDate({
-                              date: asset.updated_at,
-                              format: "LLL",
-                            })}
-                          </TooltipContent>
-                        </Tooltip>
-                      </TableCell>
-                    </TableRow>
-                  );
-                })}
-              </TableBody>
-            </Table>
-          ) : (
-            <p className="p-6 text-center text-muted-foreground">
-              No orders found.
-            </p>
-          )}
-        </CardContent>
-      </Card>
-    </TooltipProvider>
+                    <TableCell className="hidden sm:table-cell">
+                      <Tooltip>
+                        <TooltipTrigger>
+                          {formatTimeToNow(new Date(asset.updated_at))}
+                        </TooltipTrigger>
+                        <TooltipContent>
+                          {formatDate({
+                            date: asset.updated_at,
+                            format: "LLL",
+                          })}
+                        </TooltipContent>
+                      </Tooltip>
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        ) : (
+          <p className="p-6 text-center text-muted-foreground">
+            No orders found.
+          </p>
+        )}
+      </CardContent>
+    </Card>
   );
 }
