27e677064b01ccceb1eb60d3c98140e81bf3d62a
Erkan Turut <erkamturut@gmail.com>
2024-08-08 00:54:06 +0200

    refactor : work order creation form

--- client/components/forms/work-order/work-order-create-form.tsx
+++ client/components/forms/work-order/work-order-create-form.tsx
@@ -6,44 +6,53 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
 import { Form } from "@/components/ui/form";
 
-import { catchError, cn } from "@/lib/utils";
+import { catchError, cn, formatDate } from "@/lib/utils";
 
 import { Button } from "@/components/ui/button";
 
+import { Separator } from "@/components/ui/separator";
 import { api } from "@/trpc/client";
 import {
-  TCreateWorkOrderSchema,
-  TCreateWorkOrderWithStepsSchema,
-  ZCreateWorkOrderSchema,
-  ZCreateWorkOrderWithStepsSchema,
+  TCreateWorkOrderWithItemsSchema,
+  ZCreateWorkOrderWithItemsSchema,
 } from "@/trpc/server/routes/work_order/create.schema";
-import { useRouter } from "next/navigation";
+import { notFound, useParams, useRouter } from "next/navigation";
 import { General } from "./general";
 import { WorkSteps } from "./work-steps";
-import { Separator } from "@/components/ui/separator";
 
 interface WorkOrderCreateFormProps
-  extends React.HTMLAttributes<HTMLFormElement> {
-  workspace_id: string;
-}
+  extends React.HTMLAttributes<HTMLFormElement> {}
 
 export function WorkOrderCreateForm({ className }: WorkOrderCreateFormProps) {
   const router = useRouter();
+  const { team_identity, url_key } = useParams() as {
+    team_identity: string;
+    url_key: string;
+  };
+
+  const [team, {}] = api.db.team.getByIdentity.useSuspenseQuery({
+    identity: team_identity,
+  });
+  const [{ data: workspace }, {}] =
+    api.db.workspace.getByUrlKey.useSuspenseQuery({
+      url_key,
+    });
+
+  if (!team || !workspace) {
+    throw notFound();
+  }
+
+  if (!team_identity || !url_key) {
+    throw new Error("Missing team_identity or url_key");
+  }
 
   const { mutate, isPending } = api.db.work_order.create.useMutation({
     onSuccess(data) {
-      form.reset();
       router.push(`./${data?.work_order.id}`);
+      form.reset();
     },
     onError(err) {
       catchError(new Error(err.message));
@@ -51,20 +60,23 @@ export function WorkOrderCreateForm({ className }: WorkOrderCreateFormProps) {
   });
 
   // react-hook-form
-  const form = useForm<TCreateWorkOrderSchema>({
-    resolver: zodResolver(ZCreateWorkOrderSchema),
+  const form = useForm<TCreateWorkOrderWithItemsSchema>({
+    resolver: zodResolver(ZCreateWorkOrderWithItemsSchema),
     defaultValues: {
       company_id: "4doRuGC8pE",
       name: undefined,
       description: undefined,
       location_id: "xc9zBwVtbm",
       source: "MANUAL_ENTRY",
-      team_id: "4pGki9KGYX",
+      team_id: team.id,
       type: "MAINTENANCE",
       work_step: [],
       asset: [],
       priority: "LOW",
       status: "OPEN",
+      workspace_id: workspace.id,
+      sheduled_start: undefined,
+      sheduled_end: undefined,
     },
   });
 
@@ -79,17 +91,12 @@ export function WorkOrderCreateForm({ className }: WorkOrderCreateFormProps) {
         }
       >
         <Card>
-          <CardHeader>
-            <CardTitle>General</CardTitle>
-            <CardDescription>
-              Fill in the general information about the work order
-            </CardDescription>
-            <Separator />
-          </CardHeader>
-          <CardContent>
-            <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
+          <CardContent className="pt-6">
+            <div className="grid grid-cols-1 gap-4 md:grid-cols-[1fr,0.1rem,1fr]">
               <General form={form} />
-              <Tabs defaultValue="work-steps" className="w-full">
+              <Separator orientation="vertical" className="hidden md:block" />
+              <Separator orientation="horizontal" className="block md:hidden" />
+              <Tabs defaultValue="work-steps" className="w-full grid-cols-2">
                 <TabsList className="grid w-full grid-cols-3">
                   <TabsTrigger value="work-steps">Work steps</TabsTrigger>
                   <TabsTrigger value="assets">Assets</TabsTrigger>
