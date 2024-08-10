ff1bf388ca45e43f7d61546b87e4358e7a62f4a5
Erkan Turut <erkamturut@gmail.com>
2024-08-11 00:49:12 +0200

    feat: Add DataTableViewOptions and DataTableRowActions components
    Add new components DataTableViewOptions and DataTableRowActions to provide additional functionality to the data table. DataTableViewOptions component adds a dropdown menu for toggling column visibility in the table. DataTableRowActions component adds a dropdown menu for performing actions on individual rows in the table. These components enhance the user experience and improve the usability of the data table.

---
+++ client/components/tables/custom/data-table.tsx
@@ -0,0 +1,87 @@
+"use client";
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { cn } from "@/lib/utils";
+import {
+  ColumnDef,
+  flexRender,
+  type Table as TanstackTable,
+} from "@tanstack/react-table";
+
+interface DataTableProps<TData, TValue>
+  extends React.HTMLAttributes<HTMLDivElement> {
+  table: TanstackTable<TData>;
+}
+
+export function DataTable<TData, TValue>({
+  className,
+  children,
+  table,
+  ...props
+}: DataTableProps<TData, TValue>) {
+  return (
+    <div
+      className={cn("w-full space-y-2.5 overflow-auto", className)}
+      {...props}
+    >
+      {children}
+      <div className="overflow-hidden rounded-md border">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext(),
+                          )}
+                    </TableHead>
+                  );
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext(),
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={table.getAllColumns().length}
+                  className="h-24 text-center"
+                >
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+    </div>
+  );
+}
